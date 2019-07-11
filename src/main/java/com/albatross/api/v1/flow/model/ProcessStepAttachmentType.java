62fa0f2a5c3e290f7eb87b19902feb35e3d18cb6
Randa Nunn <radnanunn@gmail.com>
2019-07-11 17:01:57 -0600

    ancillary stuff

---
+++ src/main/java/com/albatross/api/v1/flow/model/ProcessStepAttachmentType.java
@@ -0,0 +1,25 @@
+package com.albatross.api.v1.flow.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.util.Date;
+import java.util.List;
+
+/**
+ * Created by randanunn on 2019-05-20.
+ * !Describe Purpose!
+ */
+@Getter
+@Setter
+public class ProcessStep {
+
+  private Long id, processId, companyId, orgId, createdById, modifiedById;
+  // originalProcessStepName used for frontend validation (without having to loop to populate it on frontend)
+  private String orgName, processStepName, originalProcessStepName;
+  private Boolean archived;
+  private Date dateCreated, dateModified;
+
+  private List<CustomFieldGroupType> customFieldGroupTypes;
+}
+
