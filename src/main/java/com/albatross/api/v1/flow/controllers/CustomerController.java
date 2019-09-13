54b5c527d745d10875c8bb39fd71298c24b27b6c
Randa Nunn <radnanunn@gmail.com>
2019-09-13 16:11:07 -0600

    missed change

--- src/main/java/com/albatross/api/v1/flow/controllers/CustomerController.java
+++ src/main/java/com/albatross/api/v1/flow/controllers/CustomerController.java
@@ -1,29 +1,21 @@
 package com.albatross.api.v1.flow.controllers;
 
 
-import java.util.List;
-
 import com.albatross.api.v1.flow.model.Customer;
 import com.albatross.api.v1.flow.model.Project;
 import com.albatross.api.v1.flow.model.User;
 import com.albatross.api.v1.flow.services.CustomerService;
-
+import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
-import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
+import java.util.List;
 
 @Slf4j
 @RestController
@@ -39,9 +31,8 @@ public class CustomerController {
     }
 
     @GetMapping(value = "/exportCustomers", produces = "text/csv")
-    public ResponseEntity exportCustomerList(@PathVariable Long companyId,
-                                             @RequestParam String query) {
-        return customerService.exportCustomers(companyId, query);
+    public ResponseEntity exportCustomerList(@RequestParam String query) {
+        return customerService.exportCustomers(query);
     }
 
     @GetMapping(value = "/{customerId}", produces = MediaType.APPLICATION_JSON_VALUE)
