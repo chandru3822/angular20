b075303686f5f432514fba4430e772a3bf317fa2
Randa Nunn <radnanunn@gmail.com>
2019-09-17 21:01:35 -0600

    test something

--- client/src/vue.config.js
+++ client/src/vue.config.js
@@ -1,5 +1,13 @@
 module.exports = {
   configureWebpack: {
     devtool: 'source-map'
+  },
+  devServer: {
+    proxy: process.env.VUE_APP_ENV === 'dev' ?
+        'https://dev-albatross-api.myblueraven.com' :
+        process.env.VUE_APP_ENV === 'uat' ?
+          'https://uat-albatross-api.myblueraven.com' :
+          process.env.VUE_APP_ENV === 'prod' ?
+            'https://prod-albatross-api.myblueraven.com' : 'http://localhost:8080'
   }
 }
