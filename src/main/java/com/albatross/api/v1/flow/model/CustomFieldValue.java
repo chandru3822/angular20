a4bc3dd67e178ce77945bd40952f6152718c357a
Scott Humes <scotthumes@gmail.com>
2020-01-07 12:08:17 -0700

    Add work type to process steps

--- src/main/java/com/albatross/api/v1/flow/model/CustomFieldValue.java
+++ src/main/java/com/albatross/api/v1/flow/model/CustomFieldValue.java
@@ -15,7 +15,7 @@ public class CustomFieldValue {
 
   private Long id, customerId, customFieldGroupAssignmentId, customFieldGroupId,
       customFieldId, fieldOrder, listOfValueId, companyDataTypeId, dataTypeId,
-      customFieldSqlKeyId, companySystemListId, projectProcessStepId, scheduleFieldTypeId;
+      customFieldSqlKeyId, companySystemListId, projectProcessStepId, scheduleFieldTypeId, ancillaryCustomFieldGroupAssignmentId;
 
   private String fieldName, fieldValue, textValue, customFieldSqlKey;
   private Long intValue;
--- src/main/java/com/albatross/api/v1/flow/model/ProjectProcessStep.java
+++ src/main/java/com/albatross/api/v1/flow/model/ProjectProcessStep.java
@@ -8,7 +8,9 @@ import java.util.List;
 @Data
 public class ProjectProcessStep {
 
-  private Long projectProcessStepId, companyStatusId, statusId, processStepId, userPositionId, projectId;
+  //@TODO: This repeats a lot of the stuff the ProcessStep model does and should probably inherit from it (aka line 16)
+
+  private Long projectProcessStepId, companyStatusId, statusId, processStepId, userPositionId, projectId, workTypeId;
 
   private String owner, name, status;
   // @humes, didn't want to break anything else you had so i put these 2 here to show some data on the screen
