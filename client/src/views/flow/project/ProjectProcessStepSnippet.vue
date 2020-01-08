4a1edba2ce2ccc4a3ecfd807cc0aa1510c7ea5a3
Scott Humes <scotthumes@gmail.com>
2020-01-08 09:42:29 -0700

    Project Screen: Animate showing/hiding all process steps section

--- client/src/views/flow/project/ProjectProcessStepSnippet.vue
+++ client/src/views/flow/project/ProjectProcessStepSnippet.vue
@@ -9,7 +9,7 @@
             style="border-bottom: 1px solid gray;"
           >
             <v-col cols="1">ID</v-col>
-            <v-col cols="3">Type</v-col>
+            <v-col cols="3">Name</v-col>
             <v-col cols="3">Owner</v-col>
             <v-col cols="3">Last Activity</v-col>
             <v-col cols="2">Status</v-col>
@@ -19,7 +19,7 @@
             <v-col cols="1" >
               <router-link :to="`/project/${projectId}/processStep/${step.projectProcessStepId}?processStepId=${step.processStepId}`">{{ step.projectProcessStepId }}</router-link>
             </v-col>
-            <v-col cols="3">{{ step.workType }}</v-col>
+            <v-col cols="3">{{ step.processStepName }}</v-col>
             <v-col cols="3">{{ step.owner }}</v-col>
             <v-col cols="3">{{ step.lastUpdated }}</v-col>
             <v-col cols="2">{{ step.processStepStatusType }}</v-col>
