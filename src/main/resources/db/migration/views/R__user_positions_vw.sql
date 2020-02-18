52412b86b02f8e30bd042b4eb78c6bc1358b136d
Scott Keller <kellersk88@gmail.com>
2020-02-17 20:12:36 -0700

    new views

--- src/main/resources/db/migration/views/R__user_positions_vw.sql
+++ src/main/resources/db/migration/views/R__user_positions_vw.sql
@@ -1,25 +1,46 @@
 DROP MATERIALIZED VIEW if EXISTS flow.user_positions_vw;
 CREATE MATERIALIZED VIEW flow.user_positions_vw AS
-select u.id as user_id,u.first_name,u.last_name,
-       org_hierarchy.org_id,org_hierarchy.org_name,org_hierarchy.org_level_id,org_hierarchy.position_level,
-       up.primary_flag,up.start_date,up.end_date,
-       o.state_id, u.archived as user_archived, p.schedulable as position_schedulable,
-       p.position,p.id as position_id, up.id as user_position_id,
-       p.company_id, u.email, u.phone_number,u.user_status_type_id, ust.user_status_type,
+select u.id                                      as user_id,
+       u.first_name,
+       u.last_name,
+       org_hierarchy.org_id,
+       org_hierarchy.org_name,
+       org_hierarchy.org_level_id,
+       org_hierarchy.position_level,
+       up.primary_flag,
+       up.start_date,
+       up.end_date,
+       o.state_id,
+       u.archived                                as user_archived,
+       p.schedulable                             as position_schedulable,
+       p.position,
+       p.id                                      as position_id,
+       up.id                                     as user_position_id,
+       p.company_id as company_id,
+       u.email,
+       u.phone_number,
        p.available_to_children,
-       (select json_agg( json_build_object(
-               'orgId', h.org_id,
-               'orgName', h.org_name,
-               'positionLevel',h.position_level,
-               'orgLevelId',h.org_level_id
-           )order by h.org_level_id)::jsonb
-        from flow.user_org_hierarchy(o.id) as h) as hierarchy
+       (select  ust.id
+        from flow.user_status_type ust
+                 inner join flow.company_user_status_type cust on cust.id = ust.company_user_status_type_id
+        where cust.company_id = p.company_id
+          and ust.user_id = u.id) as user_status
 from flow."user" u
-         inner join flow.user_status_type ust on ust.id = u.user_status_type_id
          left join flow.user_position up on up.user_id = u.id
          left join flow.position p on p.id = up.position_id
-         left join  flow.org o on o.id = up.org_id and o.company_id = p.company_id
+         left join flow.org o on o.id = up.org_id and o.company_id = p.company_id
          cross join flow.user_org_hierarchy(o.id) org_hierarchy
-    WITH DATA;
+WITH DATA;
 
 
+CREATE INDEX upv_user_position_id_idx
+    ON flow.user_positions_vw (user_position_id);
+
+CREATE INDEX upv_user_id_idx
+    ON flow.user_positions_vw (user_id);
+
+CREATE INDEX upv_position_id_idx
+    ON flow.user_positions_vw (position_id);
+
+CREATE INDEX upv_company_id_idx
+    ON flow.user_positions_vw (company_id);
