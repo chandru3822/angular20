c0fa1ccca02bce2f92329d9f21609c6394908993
jberns003 <54857272+jberns003@users.noreply.github.com>
2020-06-08 12:39:14 -0400

    Fix for Rebate view

--- src/main/resources/db/migration/V1_38__add_rebate_view.sql
+++ src/main/resources/db/migration/V1_38__add_rebate_view.sql
@@ -19,10 +19,7 @@ SELECT p.id AS project_id,
         FROM brs.project_rebate_payment
         WHERE project_rebate_payment.project_id = p.id
           AND project_rebate_payment.project_rebate_payment_state_id = 3)   AS paid_to_date,
-       flow.get_value_for_custom_field(4,
-                                       (select id from flow.custom_field where field_name='Total Promotion Amount'),
-                                       p.id,
-                                       4) as total_promotion_amount,
+       totalPrmAmount.numeric_value as totalPromotionAmount,
        c.first_name,
        c.last_name,
        c.first_name || ' ' || c.last_name as full_name,
@@ -44,4 +41,6 @@ FROM flow.project p
          LEFT JOIN brs.project_rebate_batch prb ON prb.id = prp.project_rebate_batch_id
          JOIN flow.contact c ON c.id = p.contact_id
          JOIN flow.state state on c.state_id = state.id
+         left join flow.project_process_step pps0 on pps0.project_id = p.id and pps0.process_step_id  = 4
+         left join flow.project_process_step_custom_field_value totalPrmAmount on totalPrmAmount.project_process_step_id = pps0.id and totalPrmAmount.custom_field_group_assignment_id = 51
 WHERE prp.project_rebate_payment_state_id = 2;
