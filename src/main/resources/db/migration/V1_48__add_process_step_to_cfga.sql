55474d0bf7741126c15980d2d6d0b9129908e290
Randa Nunn <radnanunn@gmail.com>
2020-07-13 17:24:12 -0600

    what a noob

--- src/main/resources/db/migration/V1_48__add_process_step_to_cfga.sql
+++ src/main/resources/db/migration/V1_48__add_process_step_to_cfga.sql
@@ -5,4 +5,4 @@ alter table if exists flow.custom_field_group_assignment
 drop constraint if exists cfga_ancillary_process_step_id_fk;
 
 alter table if exists flow.custom_field_group_assignment
-add constraint cfga_ancillary_process_step_id_fk foreign key (ancillary_process_step_id) references flow.process_step;w
+add constraint cfga_ancillary_process_step_id_fk foreign key (ancillary_process_step_id) references flow.process_step;
