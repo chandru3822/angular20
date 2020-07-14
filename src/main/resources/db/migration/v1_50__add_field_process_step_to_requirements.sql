38e6ae3d86c2a38386c5792f47299c5808169575
Scott Humes <scotthumes@gmail.com>
2020-07-14 15:30:28 -0600

    Revert "Process Step Requirement: Add the process step id for ancillary fields on process step requirements. Make ancillary fields work in the process step action logic."
    This reverts commit 9f8dca277e95e6a64a10289586192873510accad.

--- src/main/resources/db/migration/v1_50__add_field_process_step_to_requirements.sql
+++
@@ -1,8 +0,0 @@
-alter table if exists flow.process_step_requirement
-add if not exists custom_field_process_step_id int;
-
-alter table if exists flow.process_step_requirement
-drop constraint if exists psr_custom_field_process_step_id;
-
-alter table if exists flow.process_step_requirement
-add constraint psr_custom_field_process_step_id foreign key (custom_field_process_step_id) references flow.process_step;
