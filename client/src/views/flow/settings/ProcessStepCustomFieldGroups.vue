41cb22660e9141e1ffe10a271a986d594af921de
Randa Nunn <radnanunn@gmail.com>
2020-07-30 16:40:48 -0600

    remove dummy text

--- client/src/views/flow/settings/ProcessStepCustomFieldGroups.vue
+++ client/src/views/flow/settings/ProcessStepCustomFieldGroups.vue
@@ -149,7 +149,6 @@
                 <td :colspan="headers.length" class="pb-2 px-0"  :class="{'shaded-row': selectedIndex % 2}">
                   <v-col cols="12" justify="center" class="pl-3 pr-3" v-if="addField">
                     <h3 class="text-left">Add New Field</h3>
-                    {{ newFieldType }}
                     <v-radio-group v-model="newFieldType" @change="fetchAvailableCustomFields(item.companyObjectTypeId, item.id)">
                       <v-radio label="Native Field"
                                value="native"></v-radio>
