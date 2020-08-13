07af16d22e8af9890376637ad015d83930435a7c
Randa Nunn <radnanunn@gmail.com>
2020-08-12 22:17:27 -0600

    process step work queue type project status type ... yes really

--- src/main/java/com/albatross/api/v1/flow/model/ProcessStepWorkQueueType.java
+++ src/main/java/com/albatross/api/v1/flow/model/ProcessStepWorkQueueType.java
@@ -2,6 +2,8 @@ package com.albatross.api.v1.flow.model;
 
 import lombok.Data;
 
+import java.util.List;
+
 /**
  * Created by Joseph Canto on 2019-08-01.
  */
@@ -11,4 +13,5 @@ public class ProcessStepWorkQueueType {
     private Long id, processStepId, workQueueTypeId;
     private String workQueueType;
     private Boolean archived;
+    private List<WorkQueueTypeProjectStatus> projectStatuses;
 }
