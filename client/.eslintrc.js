dd38de41ada3e98e095af7e9b057570f38d28bca
Kaleb Scholes <kscholes@gmail.com>
2020-08-24 23:39:18 -0600

    Attempt to update outdated client dependencies

--- client/.eslintrc.js
+++ client/.eslintrc.js
@@ -1,23 +1,28 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  'extends': [
-    'plugin:vue/essential',
-    'eslint:recommended'
-  ],
-  plugins: [
-    'vuetify'
-  ],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'vuetify/no-deprecated-classes': 'error',
-    'vuetify/grid-unknown-attributes': 'error',
-    'no-unused-vars': 'off',
-  },
-  parserOptions: {
-    parser: 'babel-eslint'
-  }
+    root: true,
+    env: {
+        node: true
+    },
+    'extends': [
+        'plugin:vue/essential',
+        'eslint:recommended'
+    ],
+    plugins: [
+        'vuetify'
+    ],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+        'vuetify/no-deprecated-classes': 'error',
+        'vuetify/grid-unknown-attributes': 'error',
+        'vue/require-v-for-key': 'warn',
+        'vue/no-unused-vars': 'warn',
+        'vue/valid-v-for': 'warn',
+        'no-unused-vars': 'warn',
+        'no-undef': 'warn',
+        'no-useless-catch': 'warn'
+    },
+    parserOptions: {
+        parser: 'babel-eslint'
+    }
 }
