ea9e4d7f04baffd920e1136eb75f9d37f95171b9
Scott Humes <scotthumes@gmail.com>
2020-08-27 11:53:48 -0600

    Action logic: Allow actions to be performed only once per project process step

---
+++ src/main/java/com/albatross/api/v1/flow/model/ProjectProcessStepAction.java
@@ -0,0 +1,11 @@
+package com.albatross.api.v1.flow.model;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class ProjectProcessStepAction extends ProcessStepAction {
+
+    private Boolean alreadyTriggered;
+}
