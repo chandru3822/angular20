b23c582bcfe9a1d1e34925fd78b0da3eeeae75a4
Randa Nunn <radnanunn@gmail.com>
2020-08-31 16:56:05 -0600

    fix NaN for link to pps

--- client/src/views/flow/schedule/components/Calendar.vue
+++ client/src/views/flow/schedule/components/Calendar.vue
@@ -720,7 +720,10 @@
       handleEventClick (info) {
         if(info.event.title) {
           let props = info.event.extendedProps
-          this.$router.push({name: 'projectProcessStep', params: {projectId: props.projectId, processStepId: props.projectProcessStepId}})
+          console.log('randaLogger', props)
+          debugger
+          this.$router.push({path: `/project/${props.projectId}/processStep/${props.projectProcessStepId}?processStepId=${props.processStepId}&contactId=${props.contactId}`})
+          // this.$router.push({name: 'projectProcessStep', params: {projectId: props.projectId, processStepId: props.projectProcessStepId}})
         }
       },
       handleEventRender (info) {
