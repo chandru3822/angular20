fd0914177b7bd6ff68f3950430d6f8f058fa21fb
Randa Nunn <radnanunn@gmail.com>
2020-08-31 16:42:54 -0600

    change color of stage and uat header

--- client/src/components/AccountMenu.vue
+++ client/src/components/AccountMenu.vue
@@ -93,7 +93,9 @@
         attachmentTypeId: 9,
         userId: this.$store.state.user.details.id,
         userFirstName: this.getFirstName(),
-        headerColor: VUE_APP_ENV === 'local' ? 'pink' : VUE_APP_ENV === 'uat' || VUE_APP_ENV === 'dev' ? 'orange' : 'primaryCustom',
+        headerColor: VUE_APP_ENV === 'local' ? 'blue' :
+                     VUE_APP_ENV === 'dev' || VUE_APP_ENV === 'stage' ? 'orange' :
+                     VUE_APP_ENV === 'uat' ? 'blue' : 'primaryCustom',
         menuOpen: false,
         timezone: null,
         highestCompanyId: this.$store.state.user.details.highestCompanyId,
