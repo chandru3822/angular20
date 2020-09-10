76caebe1ce5890bc630dc23ef88b53767c7317f7
Randa Nunn <radnanunn@gmail.com>
2020-09-10 14:07:06 -0600

    oopsies

--- src/main/resources/db/migration/V1_68__flow_add_pss_with_actions_and_reqs_function.sql
+++ src/main/resources/db/migration/V1_68__flow_add_pss_with_actions_and_reqs_function.sql
@@ -41,7 +41,7 @@ FROM (
                 u.first_name as "firstName",
                 u.last_name as "lastName",
                 up.id as "userPositionId",
-                concat(u.first_name, ' ', u.last_name) AS "fullName",
+                u.first_name || ' ' || u.last_name AS "fullName",
                 p.position
             from blueraven.user u
                      inner join flow.user_position up on up.user_id = u.id
