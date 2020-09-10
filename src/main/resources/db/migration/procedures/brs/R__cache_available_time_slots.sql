43a44cab96ac2349f911e66a96c02338c4e7f54f
Scott Keller <kellersk88@gmail.com>
2020-09-10 13:46:02 -0600

    changed the name of user_status_type and company_user_status_type

--- src/main/resources/db/migration/procedures/brs/R__cache_available_time_slots.sql
+++ src/main/resources/db/migration/procedures/brs/R__cache_available_time_slots.sql
@@ -18,13 +18,13 @@ BEGIN
                           inner join flow.user_position up on up.user_id = u.id
                           inner join flow.org o on o.id = up.org_id
                           inner join flow.state s on s.id = o.state_id
-                          inner join flow.user_status_type ust on ust.user_id = u.id
-                          inner join flow.company_user_status_type cust on cust.id = ust.company_user_status_type_id and cust.company_id = o.company_id
+                          inner join flow.company_user_status cus on cus.user_id = u.id
+                          inner join flow.user_status_type ust on ust.id = cus.user_status_type_id and ust.company_id = o.company_id
                      and up.position_id in (1,2,3)
                      and up.end_date is null
                      and up.primary_flag is true
                      and up.archived is false
-                     and cust.has_access is true
+                     and ust.has_access is true
              )
              select unnest(users) as user, scheduled_start_time
              from active_projects ap cross join generate_series(ap.start_time - interval '21 days',
--- src/main/resources/db/migration/procedures/brs/R__get_setter_to_beat.sql
+++ src/main/resources/db/migration/procedures/brs/R__get_setter_to_beat.sql
@@ -39,11 +39,11 @@ BEGIN
                       inner join flow.contact c on c.id = p.contact_id
                       inner join flow.user_positions_vw upv on upv.user_position_id = c.owner_user_position_id
                       inner join flow.user u on u.id = upv.user_id
-                      inner join flow.user_status_type ust on ust.user_id = u.id
-                      inner join flow.company_user_status_type cust on cust.id = ust.company_user_status_type_id
+                      inner join flow.company_user_status cus on cus.user_id = u.id
+                      inner join flow.user_status_type ust on ust.id = cus.user_status_type_id
                       left join flow.project_process_step pps on pps.project_id = p.id and pps.process_step_id = 2
                       left join flow.project_process_step_custom_field_value closer_appointment_outcome on closer_appointment_outcome.project_process_step_id = pps.id and closer_appointment_outcome.custom_field_group_assignment_id = 4
-                  where cust.user_status_type = 'Active'
+                  where ust.user_status_type = 'Active'
                       and pd.source in (6,493)
                       and pd.closer_appointment_start between p_start_date and p_end_date
                       and closer_appointment_outcome.text_value in ('Pitched', 'Missed')
