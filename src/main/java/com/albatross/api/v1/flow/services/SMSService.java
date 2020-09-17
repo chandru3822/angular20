dd2e0956e40f2ca2fd9329da5d58e5e21e44fc62
jberns003 <54857272+jberns003@users.noreply.github.com>
2020-09-17 16:24:23 -0400

    SMS change, removed logging message

--- src/main/java/com/albatross/api/v1/flow/services/SMSService.java
+++ src/main/java/com/albatross/api/v1/flow/services/SMSService.java
@@ -380,7 +380,6 @@ public class SMSService {
 
     public void saveReply(TwilioMessageRequest sms) {
         log.info("saving Twilio SMS reply: {}", sms.getMessageSid());
-        log.info("saving Twilio SMS reply: {}", sms.toHashMap().toString());
 
         RecordType type = getRecordTypeByMessagingServiceSID(sms.getMessagingServiceSid());
 
