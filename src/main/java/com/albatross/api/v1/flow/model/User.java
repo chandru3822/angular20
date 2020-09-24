e48c7b0a93622663775779ad12507cda37424de4
Randa Nunn <randanunn@randa-64.lan>
2020-09-24 16:57:59 -0600

    admin save user password

--- src/main/java/com/albatross/api/v1/flow/model/User.java
+++ src/main/java/com/albatross/api/v1/flow/model/User.java
@@ -19,7 +19,7 @@ public class User {
     // parentCompanyId = true parent of ^^ that company (can be null, can point to albatross)
     // highestParentCompanyId = highest company id within company_id's hierarchy
     // highestCompanyId = highest company a user has access to (regardless of context and hierarchies) <- mostly used to determine if user is system admin/has access to albatross
-    private String email, phoneNumber, username, firstName, lastName, fullName,
+    private String email, phoneNumber, username, firstName, lastName, fullName, newPassword,
         userStatusType, timezone, awsBucket, companyName, companyAbbreviation, position, title; //title used for scheduling tool resource name
     private Long companyUserStatusId, userStatusTypeId;
     private List<Company> companies;
