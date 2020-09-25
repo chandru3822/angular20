105eefe4a93ee032bbaac60005aa0c1667d43718
Randa Nunn <randanunn@randa-64.lan>
2020-09-24 22:55:44 -0600

    picky picky

--- src/main/resources/db/migration/V1_81__multi_line.sql
+++ src/main/resources/db/migration/V1_81__multi_line.sql
@@ -1,5 +1,5 @@
 --added some return values, need to drop
-drop function flow.set_closer_appointment(integer, integer, timestamp, int[])
+drop function flow.set_closer_appointment(integer, integer, timestamp, int[]);
 
 alter table if exists flow.custom_field
     add column if not exists multi_line boolean not null default false;
