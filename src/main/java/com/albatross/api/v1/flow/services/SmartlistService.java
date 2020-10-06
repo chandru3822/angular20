f66e0d8d42f8eab470aa16fd7c2c461158e590e3
Scott Humes <scotthumes@gmail.com>
2020-10-06 14:45:03 -0600

    Smartlists: Return correct list of smartlists based on the viewing object type

--- src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
+++ src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
@@ -197,7 +197,7 @@ public class SmartlistService {
 
   public List<Smartlist> getSharedByType(Long objectTypeId) {
       User user = securityService.getCurrentUser();
-      return sqlCache.query("project.getSharedByObjectType", Map.of("companyId", user.getCompanyId(), "objectTypeId", objectTypeId, "userId", user.getId()), Smartlist.class);
+      return sqlCache.query("smartlist.getSharedByObjectType", Map.of("companyId", user.getCompanyId(), "objectTypeId", objectTypeId, "userId", user.getId()), Smartlist.class);
   }
 
   public SmartlistResult getSmartlistResults(Long smartlistId) {
