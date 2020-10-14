146ec8739a3716da27d4cd6c2db8d06203a32b35
Scott Humes <scotthumes@gmail.com>
2020-10-14 14:40:27 -0600

    Smartlists: Add smartlist system lists to smartlists so smartlists can have system lists that aren't really system lists but are instead smartlist system lists... Randa's idea

--- src/main/java/com/albatross/api/v1/flow/model/SmartlistRequirement.java
+++ src/main/java/com/albatross/api/v1/flow/model/SmartlistRequirement.java
@@ -14,7 +14,8 @@ public class SmartlistRequirement {
   //@TODO humes: should probably make this have a field of type SmartlistField since it uses most of those fields anyways
 
   private Long id, smartlistId, smartlistFieldId, processStepId, customFieldGroupAssignmentId, operatorTypeId, objectTypeId,
-               dataTypeId, dataTypeRequirementId, displayOrder, createdById, modifiedById, companySystemListId, listOfValueId, systemListOptionId, customSqlOptionId;
+               dataTypeId, dataTypeRequirementId, displayOrder, createdById, modifiedById, companySystemListId, listOfValueId, systemListOptionId, customSqlOptionId,
+               smartlistSystemListId;
 
   private String requirementValue, secondaryRequirementValue, processStepName, objectType, operatorType, name, customFieldSqlKey;
 
