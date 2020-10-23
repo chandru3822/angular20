fa5dcaf4db7a3e6266bd426ebcae25b173babf7a
jberns003 <54857272+jberns003@users.noreply.github.com>
2020-10-23 17:13:06 -0400

    Added Birdeye support

---
+++ src/main/java/com/albatross/api/v1/company/blueraven/controllers/BirdeyeReviewController.java
@@ -0,0 +1,60 @@
+package com.albatross.api.v1.company.blueraven.controllers;
+
+import com.albatross.api.v1.company.blueraven.models.birdeye.BirdeyeLocation;
+import com.albatross.api.v1.company.blueraven.models.birdeye.BirdeyeReviewInvitation;
+import com.albatross.api.v1.company.blueraven.models.birdeye.BirdeyeService;
+import com.google.common.base.Supplier;
+import com.google.common.base.Suppliers;
+import org.apache.commons.lang3.StringUtils;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.concurrent.TimeUnit;
+
+import static com.google.common.base.Preconditions.checkArgument;
+
+@RestController
+@RequestMapping(value = "/api/v1/company/blueraven/birdeye")
+public class BirdeyeReviewController {
+    @Autowired
+    BirdeyeService birdeye;
+
+    private Supplier<List<BirdeyeLocation>> locations;
+
+    public BirdeyeReviewController(BirdeyeService birdeye) {
+        checkArgument(birdeye != null);
+        this.birdeye = birdeye;
+        this.locations = Suppliers.memoizeWithExpiration(birdeye::getLocations, 1, TimeUnit.HOURS);
+    }
+
+    /**
+     * Send (or re-send) an invite to a customer to review Blue Raven.
+     *
+     * @param invitation
+     * @return
+     */
+    @RequestMapping(method = RequestMethod.POST, value = "/invite")
+    public ResponseEntity<String> invite(@RequestBody BirdeyeReviewInvitation invitation) {
+        JSONObject result = new JSONObject();
+        if (invitation.getSendSms() != null && invitation.getSendSms()) {
+            checkArgument(StringUtils.isNotBlank(invitation.getCustomerPhone()),
+                    "Customer phone number must be specified if sendSms is set.");
+        } else {
+            checkArgument(StringUtils.isNotBlank(invitation.getCustomerEmail()),
+                    "Customer email must be specified if sendSms is not set.");
+        }
+        try {
+            birdeye.sendInvitation(invitation);
+            result.put("message", "Invite sent.");
+        } catch (Exception e) {
+            result.put("message", e.getMessage());
+        }
+        return ResponseEntity.badRequest().body(result.toString());
+    }
+}
