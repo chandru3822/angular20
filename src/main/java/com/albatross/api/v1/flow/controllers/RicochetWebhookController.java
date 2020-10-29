43c12923400935548b9a00bdc2441a2b5b62e4d8
Joseph Canto <joseph@7oaksgroup.com>
2020-10-29 16:03:21 -0400

    more ricochet stuff

--- src/main/java/com/albatross/api/v1/flow/controllers/RicochetWebhookController.java
+++ src/main/java/com/albatross/api/v1/flow/controllers/RicochetWebhookController.java
@@ -10,6 +10,8 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import static org.apache.commons.lang3.StringUtils.isBlank;
+
 @RestController
 @RequestMapping(value = "/webhook/ricochet")
 @Slf4j
@@ -43,18 +45,18 @@ public class RicochetWebhookController {
                 "lead_source_detail: {}, " +
                 "hubspotId: {}",
                 lead.getUniqueIdentifier() != null ? lead.getUniqueIdentifier() : "null",
-                lead.getStatus() != null ? lead.getStatus() : "null",
-                lead.getLeadOwner() != null ? lead.getLeadOwner() : "null",
-                lead.getCustomer().getFirstName() != null ? lead.getCustomer().getFirstName() : "null",
-                lead.getCustomer().getLastName() != null ? lead.getCustomer().getLastName() : "null",
-                lead.getCustomer().getPhone1() != null ? lead.getCustomer().getPhone1() : "null",
-                lead.getCustomer().getEmail() != null ? lead.getCustomer().getEmail() : "null",
-                lead.getCustomer().getAddress().getAddress1() != null ? lead.getCustomer().getAddress().getAddress1() : "null",
-                lead.getCustomer().getAddress().getCity() != null ? lead.getCustomer().getAddress().getCity() : "null",
-                lead.getCustomer().getAddress().getZip() != null ? lead.getCustomer().getAddress().getZip() : "null",
-                lead.getCustomer().getAddress().getState() != null ? lead.getCustomer().getAddress().getState() : "null",
-                lead.getLead_source() != null ? lead.getLead_source() : "null",
-                lead.getLead_source_detail() != null ? lead.getLead_source_detail() : "null",
+                !isBlank(lead.getStatus()) ? lead.getStatus() : "null",
+                !isBlank(lead.getLeadOwner()) ? lead.getLeadOwner() : "null",
+                !isBlank(lead.getCustomer().getFirstName()) ? lead.getCustomer().getFirstName() : "null",
+                !isBlank(lead.getCustomer().getLastName()) ? lead.getCustomer().getLastName() : "null",
+                !isBlank(lead.getCustomer().getPhone1()) ? lead.getCustomer().getPhone1() : "null",
+                !isBlank(lead.getCustomer().getEmail()) ? lead.getCustomer().getEmail() : "null",
+                !isBlank(lead.getCustomer().getAddress().getAddress1()) ? lead.getCustomer().getAddress().getAddress1() : "null",
+                !isBlank(lead.getCustomer().getAddress().getCity()) ? lead.getCustomer().getAddress().getCity() : "null",
+                !isBlank(lead.getCustomer().getAddress().getZip()) ? lead.getCustomer().getAddress().getZip() : "null",
+                !isBlank(lead.getCustomer().getAddress().getState()) ? lead.getCustomer().getAddress().getState() : "null",
+                !isBlank(lead.getLead_source()) ? lead.getLead_source() : "null",
+                !isBlank(lead.getLead_source_detail()) ? lead.getLead_source_detail() : "null",
                 lead.getHubspotId() != null ? lead.getHubspotId() : "null"
         );
 
