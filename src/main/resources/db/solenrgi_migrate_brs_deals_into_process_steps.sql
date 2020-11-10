479c785d03d79b148dc068ec62e5531881da8922
Scott Keller <kellersk88@gmail.com>
2020-11-09 21:05:51 -0700

    Mandy is even less awesome

--- src/main/resources/db/solenrgi_migrate_brs_deals_into_process_steps.sql
+++ src/main/resources/db/solenrgi_migrate_brs_deals_into_process_steps.sql
@@ -17884,7 +17884,7 @@ with process_step1 as (
                 2350555 as created_by_id,
                 now(),
                 d1.complete_date
-         ,now(),
+         ,date_trunc('second', coalesce(migrated_created_date,now())::timestamp)+ ((random() * 1000 ) + 1) * interval '1 milliseconds',
                 d1.org_id,
                 d1.start_time,
                 d1.end_time
