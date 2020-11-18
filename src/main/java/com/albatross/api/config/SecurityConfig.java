6734deb9950b6ea5968bc4bd6e586884281b1d05
Randa Nunn <randanunn@randa-64.lan>
2020-11-18 15:43:02 -0700

    open mobile endpoint

--- src/main/java/com/albatross/api/config/SecurityConfig.java
+++ src/main/java/com/albatross/api/config/SecurityConfig.java
@@ -72,7 +72,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
         .antMatchers("/webhook/hubspot/**").permitAll()
         .antMatchers("/webhook/ricochet/**").permitAll()
           // export the endpoint for automating s3 uploads of mobile builds from fast lane
-          .antMatchers("/api/v1/apps/addAttachmentRecord").permitAll()
+          .antMatchers("/api/v1/flow/app/addAttachmentRecord").permitAll()
         .anyRequest().authenticated()
         .and()
         .exceptionHandling()
