d8348dfeee1744310059b331886fa2bd2d746984
Randa Nunn <randanunn@randa-64.lan>
2020-11-27 11:39:31 -0700

    fix link

--- client/src/views/flow/project/ProjectAdmin.vue
+++ client/src/views/flow/project/ProjectAdmin.vue
@@ -4,7 +4,7 @@
     <v-row class="project-header">
       <v-col cols="8" class="text-left pl-5">
         <div class="project-title">
-          <router-link :to="`/projectId/${projectId}/details`">{{ contact.fullName}}</router-link>
+          <router-link :to="`/project/${projectId}/details`">{{ contact.fullName}}</router-link>
         </div>
         <div class="project-subtitle">
           {{ contact.street1 }} - {{ contact.city }}, {{ contact.state }}
