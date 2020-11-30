69e7eb0b2eacfcedf2069ac82b80da1c5cc95e88
Randa Nunn <randanunn@randa-64.lan>
2020-11-29 21:28:08 -0700

    fix inserting app build record

--- src/main/java/com/albatross/api/v1/flow/model/AppAttachment.java
+++ src/main/java/com/albatross/api/v1/flow/model/AppAttachment.java
@@ -7,7 +7,7 @@ import java.util.Date;
 @Data
 public class AppAttachment {
     private Long id, size, appTypeId, attachmentTypeId, companyId;
-    private String filename, contentType, s3Key, url, presignedUrl, appType;
+    private String filename, contentType, s3Key, url, presignedUrl, appType, keyPattern;
     private Date dateCreated, dateModified;
     private Boolean archived, show;
 }
