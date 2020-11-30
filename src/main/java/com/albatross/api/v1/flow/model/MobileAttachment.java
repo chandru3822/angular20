57e6e7addf79717bc8bb354bcd6f3765e789e892
Randa Nunn <randanunn@randa-64.lan>
2020-11-29 18:12:51 -0700

    fix bad endpoint

--- src/main/java/com/albatross/api/v1/flow/model/MobileAttachment.java
+++ src/main/java/com/albatross/api/v1/flow/model/MobileAttachment.java
@@ -4,7 +4,7 @@ import lombok.Data;
 
 @Data
 public class MobileAttachment {
-    private Attachment attachment;
+    private AppAttachment attachment;
     private String keyPattern;
     private Long appTypeId, attachmentTypeId;
 }
