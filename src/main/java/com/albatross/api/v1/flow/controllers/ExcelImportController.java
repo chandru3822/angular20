13ed239c1bcccc40bf0ecadb36a2234c83e9e23c
Randa Nunn <randanunn@Randa-64.local>
2020-11-30 22:09:36 -0700

    last one

--- src/main/java/com/albatross/api/v1/flow/controllers/ExcelImportController.java
+++ src/main/java/com/albatross/api/v1/flow/controllers/ExcelImportController.java
@@ -135,7 +135,7 @@ public class ExcelImportController {
     params.put("proposalId", propId);
     params.put("projectId", projectId);
 
-    log.info("EXCEL_IMPORT: lets figure this out PROP_ID: {} PROJECT_ID: {} SOURCE: {} PROPOSAL: {}", propId, projectId, proposal.getSource(), proposal.getProposal());
+    log.info("EXCEL_IMPORT: lets figure this out PROP_ID: {} PROJECT_ID: {} SOURCE: {}", propId, projectId, proposal.getSource());
     Optional<ProposalResponse> created = cache.get("excel.import.insert", params,
         (rs, rowNum) -> {
             try {
