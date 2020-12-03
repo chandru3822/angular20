24edf144fa4ee3bf17ee0ef7d8cf72afb771cd8b
Joseph Canto <joseph@7oaksgroup.com>
2020-12-03 18:46:35 -0500

    fix label & length of undefined error

--- client/src/views/flow/contacts/Contact.vue
+++ client/src/views/flow/contacts/Contact.vue
@@ -268,7 +268,7 @@ export default {
       this.$store.commit(AppMutations.SET_LOADING, true)
       try {
         let phoneRegex = '^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$'
-        if (this.contact.phone.length > 0 && (!this.contact.phone.match(phoneRegex) || this.contact.phone.length > 20)) {
+        if (this.contact?.phone?.length > 0 && (!this.contact?.phone?.match(phoneRegex) || this.contact?.phone?.length > 20)) {
           this.snackbar = getSnackbar('ERROR', 'Error Saving Contact: Please reformat the Phone field with a valid phone number')
           this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
           this.$store.commit(AppMutations.SET_LOADING, false)
@@ -276,7 +276,7 @@ export default {
           return;
         }
 
-        if (this.contact.mobile.length > 0 && (!this.contact.mobile.match(phoneRegex) || this.contact.mobile.length > 20)) {
+        if (this.contact?.mobile?.length > 0 && (!this.contact?.mobile?.match(phoneRegex) || this.contact?.mobile?.length > 20)) {
           this.snackbar = getSnackbar('ERROR', 'Error Saving Contact: Please reformat the Mobile field with a valid phone number')
           this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
           this.$store.commit(AppMutations.SET_LOADING, false)
