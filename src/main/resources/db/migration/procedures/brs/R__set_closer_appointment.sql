50a558dc49dbc8608a10a8286e276a678091fd6a
Scott Keller <kellersk88@gmail.com>
2020-12-11 13:22:24 -0700

    changed formula

--- src/main/resources/db/migration/procedures/brs/R__set_closer_appointment.sql
+++ src/main/resources/db/migration/procedures/brs/R__set_closer_appointment.sql
@@ -114,7 +114,7 @@ BEGIN
                  select rru.user_id, count(pd2.id) as appointment_count
                  from round_robin_users rru
                       left join brs.project_details pd2 on rru.user_id = pd2.closer_user_id
-                 and  closer_appointment_start between now() - interval '90 days' and now() + interval '100 days'
+                 and  closer_appointment_start between now() - interval '21 days' and now() + interval '100 days'
                  group by rru.user_id),
              appointment_count_with_interval as (
                  select rru.user_id, count(pd2.id) as appointment_count_with_interval
@@ -166,7 +166,7 @@ BEGIN
                                                        self_gen +
                                                        ((appointment_count + avail) / 3) + ((lead_gen_num + self_gen) * 15)
                                           else
-                                            ((lead_gen_num / lead_gen_den) * 10000) + self_gen + avail + ((lead_gen_num + self_gen) * 15)  end  as score,
+                                            ((lead_gen_num / lead_gen_den) * 10000) + self_gen + ((appointment_count + avail) / 3) + ((lead_gen_num + self_gen) * 15)  end  as score,
                                           case
                                               when sum(appointment_count_with_interval) over () = 0 then
                                                   0
