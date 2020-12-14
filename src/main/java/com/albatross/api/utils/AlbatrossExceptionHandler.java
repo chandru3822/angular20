9f279b502ef0dbc18a32d1b398d69c549eb3492e
Scott Humes <scotthumes@gmail.com>
2020-12-14 12:12:32 -0700

    Uploads: Properly handle/display file upload errors. Improve file upload logging.

--- src/main/java/com/albatross/api/utils/AlbatrossExceptionHandler.java
+++ src/main/java/com/albatross/api/utils/AlbatrossExceptionHandler.java
@@ -1,10 +1,13 @@
 package com.albatross.api.utils;
 
+import com.albatross.api.model.RequestError;
 import com.albatross.api.security.SecurityService;
 import com.albatross.api.v1.flow.model.User;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.context.request.ServletWebRequest;
@@ -23,18 +26,18 @@ public class AlbatrossExceptionHandler extends ResponseEntityExceptionHandler {
   final private SecurityService securityService;
 
   @ExceptionHandler(value = MultipartException.class)
-  protected void multiExHandler(MultipartException e, WebRequest request) throws MultipartException {
-    logger(request);
-    throw e;
+  protected ResponseEntity<RequestError> multiExHandler(MultipartException e, WebRequest request) {
+      logFileUploadException(request);
+    return new ResponseEntity<>(new RequestError(e.getMessage()), HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
   @ExceptionHandler(value = IOException.class)
-  protected void ioExHandler(IOException e, WebRequest request) throws IOException {
-    logger(request);
-    throw e;
+  protected ResponseEntity<RequestError> ioExHandler(IOException e, WebRequest request) {
+    logFileUploadException(request);
+    return new ResponseEntity<>(new RequestError(e.getMessage()), HttpStatus.UNPROCESSABLE_ENTITY);
   }
 
-  private void logger(WebRequest request) {
+  private void logFileUploadException(WebRequest request) {
 
     String info = request.getDescription(false);
 
@@ -57,6 +60,6 @@ public class AlbatrossExceptionHandler extends ResponseEntityExceptionHandler {
       // noop
     }
 
-    log.error("ALBA LOGGER: {}", info);
+    log.error("FILE UPLOAD: {}", info);
   }
 }
