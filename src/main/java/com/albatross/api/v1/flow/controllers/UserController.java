33d33e9425ccdecd0e3d04bb70fa354c47083708
jberns003 <54857272+jberns003@users.noreply.github.com>
2021-02-10 13:28:50 -0500

    Made changes for specifying Email sent by name Removed unused code

--- src/main/java/com/albatross/api/v1/flow/controllers/UserController.java
+++ src/main/java/com/albatross/api/v1/flow/controllers/UserController.java
@@ -159,7 +159,7 @@ public class UserController {
                 context.put("from", "Blue Raven Solar Sales HR");
                 context.put("mailTo", "saleshr@blueravensolar.com");
 
-                communicationService.sendEmail("Click on link to reset your password", StringUtils.trimWhitespace(passwordResetRequest.getUsernameOrEmail()), template, context, "SalesOps@blueravensolar.com");
+                communicationService.sendEmail("Click on link to reset your password", StringUtils.trimWhitespace(passwordResetRequest.getUsernameOrEmail()), template, context, "SalesOps@blueravensolar.com", "Blue Raven Sales Operation");
                 log.info("AUTH: Password reset email has been sent to {}", passwordResetRequest.getUsernameOrEmail());
             } else {
                 log.info("AUTH: Password reset attempted for unknown user email {}.", passwordResetRequest.getUsernameOrEmail());
