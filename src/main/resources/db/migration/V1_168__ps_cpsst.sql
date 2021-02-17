b474c650bec64934a7e80e0d6085c9bc50ccdc5d
Randa Nunn <randanunn@gmail.com>
2021-02-17 13:04:22 -0700

    pscpsst script changes

--- src/main/resources/db/migration/V1_168__ps_cpsst.sql
+++ src/main/resources/db/migration/V1_168__ps_cpsst.sql
@@ -22,3 +22,27 @@ CREATE TABLE if not exists flow.process_step_company_process_step_status_type
         REFERENCES flow.user (id) MATCH SIMPLE
         ON UPDATE NO ACTION ON DELETE NO ACTION
 );
+
+-- delete all of them so this won't break stage (i already manually added them
+delete from flow.process_step_company_process_step_status_type where id > 0;
+
+-- inserts the actives
+insert into flow.process_step_company_process_step_status_type(process_step_id, company_process_step_status_type_id, created_by_id)
+select ps.id as process_step_id,
+       (select id from flow.company_process_step_status_type cpsst where cpsst.company_id = ps.company_id and cpsst.process_step_status_type_id = 1 and cpsst.process_step_status_type = 'Active'),
+       2350555
+from flow.process_step ps;
+
+-- inserts the completes
+insert into flow.process_step_company_process_step_status_type(process_step_id, company_process_step_status_type_id, created_by_id)
+select ps.id as process_step_id,
+       (select id from flow.company_process_step_status_type cpsst where cpsst.company_id = ps.company_id and cpsst.process_step_status_type_id = 2 and cpsst.process_step_status_type = 'Complete'),
+       2350555
+from flow.process_step ps;
+
+-- inserts the cancelled
+insert into flow.process_step_company_process_step_status_type(process_step_id, company_process_step_status_type_id, created_by_id)
+select ps.id as process_step_id,
+       (select id from flow.company_process_step_status_type cpsst where cpsst.company_id = ps.company_id and cpsst.process_step_status_type_id = 3 and cpsst.process_step_status_type = 'Cancelled'),
+       2350555
+from flow.process_step ps;
