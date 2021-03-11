b87c138558a3be1f5d213cc32e1cc73df80fb6a4
Scott Humes <scotthumes@gmail.com>
2021-03-11 13:16:59 -0700

    Smartlist: Verify all data types (except system list) on project and contact object types for PS smartlists

--- src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
+++ src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
@@ -1152,7 +1152,26 @@ public class SmartlistService {
           if (f.getCustomFieldSqlKey() != null) {
             selectFields.append(String.format("\"%s\".name as \"%s\", ", f.getValueReferenceTable(), f.getId()));
           } else {
-            selectFields.append(String.format("%s.%s as \"%s\", ", f.getReferenceTable(), f.getReferenceColumn(), f.getId()));
+            if (f.getCustomFieldGroupAssignmentId() != null) {
+              if (f.getDataTypeId() == 1) {
+                selectFields.append(String.format(" to_char(\"%s\".%s, 'YYYY-MM-DD') as \"%s\", ", f.getValueReferenceTable(), getReferenceColumn(f.getDataTypeId()), f.getId()));
+              } else if(f.getDataTypeId() == 2) {
+                selectFields.append(String.format(" to_char(\"%s\".%s, 'YYYY-MM-DD HH:MI am') as \"%s\", ", f.getValueReferenceTable(), getReferenceColumn(f.getDataTypeId()), f.getId()));
+              } else if (f.getDataTypeId() == 6 && f.getHasListValues()) {
+                 selectFields.append(String.format(" (select name from flow.list_of_value where id = \"%s\".%s) as \"%s\", ", f.getValueReferenceTable(), getReferenceColumn(f.getDataTypeId()), f.getId()));
+              } else if (f.getDataTypeId() == 7) {
+                selectFields.append(String.format(" (select array_to_string(array(select \"name\" from flow.list_of_value where id = any(\"%s\".%s)), ',')) as \"%s\", ", f.getValueReferenceTable(), getReferenceColumn(f.getDataTypeId()), f.getId()));
+              } else if (f.getDataTypeId() == 9) {
+//                final long systemListNumber = (f.getSystemListId() == 1 || f.getSystemListId() == 2) ? 1 : f.getSystemListId();
+//
+//                final String sql = String.format(" (select name from \"%s\" where \"%s\".id = \"%s\".int_value) as \"%s\", ", "systemList_" + systemListNumber, "systemList_" + systemListNumber, f.getValueReferenceTable(), f.getId());
+//                query.append(sql);
+              } else {
+                selectFields.append(String.format(" \"%s\".%s as \"%s\", ", f.getValueReferenceTable(), getReferenceColumn(f.getDataTypeId()), f.getId()));
+              }
+            } else {
+              selectFields.append(String.format("%s.%s as \"%s\", ", f.getReferenceTable(), f.getReferenceColumn(), f.getId()));
+            }
           }
         }
       });
