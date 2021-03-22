be5a681c2ee3e1c29afbbae770b45093a409df82
Scott Keller <kellersk88@gmail.com>
2021-03-22 13:16:40 -0600

    added stuff to trigger

--- src/main/resources/db/migration/procedures/flow/R__project_details_function_trigger.sql
+++ src/main/resources/db/migration/procedures/flow/R__project_details_function_trigger.sql
@@ -219,6 +219,8 @@ declare
     v_field_name                     varchar;
     v_parent_custom_field_id         integer;
     v_project_id2                    integer;
+    v_parent_process_step_id                integer;
+    v_project_process_step_completed_date timestamp;
 BEGIN
 
     select pps.project_id
@@ -227,10 +229,23 @@ BEGIN
     where pps.id = new.project_process_step_id
       and pps.main is true;
 
-    select pps.project_id
-    into v_project_id1
+    select pps.project_id,ps.parent_process_step_id,pps.process_step_complete_date
+    into v_project_id1,v_parent_process_step_id,v_project_process_step_completed_date
     from flow.project_process_step pps
+    inner join flow.process_step ps on pps.process_step_id = ps.id
     where pps.id = new.project_process_step_id;
+
+    if v_parent_process_step_id = 3166 and v_project_process_step_completed_date is not null then
+        update brs.project_details
+        set complete_date_booking = v_project_process_step_completed_date
+        where project_id = v_project_id1 and
+            complete_date_booking is null;
+    elsif v_parent_process_step_id = 3241 and v_project_process_step_completed_date is not null then
+        update brs.project_details
+        set complete_date_final_design_completion = v_project_process_step_completed_date
+        where project_id = v_project_id1
+        and complete_date_final_design_completion is null;
+    end if;
 
     select cf.field_name,cf.parent_custom_field_id
     into v_field_name,v_parent_custom_field_id
