525990f7533f5d5c3e31a5c7c7275ceeba329652
Randa Nunn <randanunn@gmail.com>
2021-03-23 09:59:03 -0600

    coalesce some things

--- src/main/resources/db/migration/procedures/brs/R__get_tournament_brackets.sql
+++ src/main/resources/db/migration/procedures/brs/R__get_tournament_brackets.sql
@@ -4,14 +4,14 @@ AS
 $BODY$
 declare
 BEGIN
-    RETURN QUERY select array_to_json(array_agg(row_to_json(brackets)))
+    RETURN QUERY select coalesce((select array_to_json(array_agg(row_to_json(brackets)))
                  from (
                           select tb.id                         as "tournamentBracketId",
                                  tb.tournament_id              as "tournamentId",
                                  tb.matches_generated          as "matchesGenerated",
                                  tb.archived,
                                  tb.number_of_users            as "numberOfUsers",
-                                 (select array_to_json(array_agg(row_to_json(tournament_rounds)))
+                                 coalesce((select array_to_json(array_agg(row_to_json(tournament_rounds)))
                                   from (
                                            select tr.id,
                                                   tr.tournament_bracket_id                   as "tournamentBracketId",
@@ -20,7 +20,7 @@ BEGIN
                                                   tr.advanced,
                                                   tr.archived,
                                                   row_number() over (order by tr.start_date) as "roundNumber",
-                                                  (select array_to_json(array_agg(row_to_json(tournament_matches)))
+                                                  coalesce((select array_to_json(array_agg(row_to_json(tournament_matches)))
                                                    from (
                                                             select tm.id,
                                                                    tm.parent_match_id                                             as "parentMatchId",
@@ -48,19 +48,19 @@ BEGIN
                                                             where tm.tournament_round_id = tr.id
                                                               and tm.archived is false
                                                             order by tm.id
-                                                        ) as tournament_matches)                matches
+                                                        ) as tournament_matches), '[]')                matches
                                            from brs.tournament_round tr
                                            where tr.tournament_bracket_id = tb.id
                                              and tr.archived is false
                                            order by tr.start_date
-                                       ) as tournament_rounds) as rounds
+                                       ) as tournament_rounds), '[]') as rounds
                           from brs.tournament t
                                    inner join brs.tournament_formula tf
                                               on tf.id = t.tournament_formula_id and tf.archived is false
                                    inner join brs.tournament_bracket tb
                                               on t.id = tb.tournament_id and tb.archived is false
                           where t.id = p_tournament_id
-                            and t.archived is false) as brackets;
+                            and t.archived is false) as brackets), '[]') as brackets;
 END
 $BODY$
     LANGUAGE plpgsql VOLATILE;
