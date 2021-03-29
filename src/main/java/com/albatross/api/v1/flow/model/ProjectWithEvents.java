6c29c48bd9a42e7706f2fc052473fb87385cfb6c
Randa Nunn <randanunn@gmail.com>
2021-03-29 14:27:14 -0600

    mobile is the worst

--- src/main/java/com/albatross/api/v1/flow/model/ProjectWithEvents.java
+++ src/main/java/com/albatross/api/v1/flow/model/ProjectWithEvents.java
@@ -19,7 +19,7 @@ public class ProjectWithEvents {
   @Data
   public static class ProjectEvent {
     private Long projectProcessStepId, processStepId, eventTypeId;
-    private String processStepName, resourceName;
+    private String processStepName, resourceName, eventType;
     private Timestamp start, end;
   }
 }
