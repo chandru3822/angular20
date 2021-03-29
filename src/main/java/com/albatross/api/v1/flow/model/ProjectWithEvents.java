72e0415381203b2e0fc97c0e67e430827cf2d5d9
Randa Nunn <randanunn@gmail.com>
2021-03-29 13:49:28 -0600

    mobile is so needy

--- src/main/java/com/albatross/api/v1/flow/model/ProjectWithEvents.java
+++ src/main/java/com/albatross/api/v1/flow/model/ProjectWithEvents.java
@@ -18,7 +18,7 @@ public class ProjectWithEvents {
 
   @Data
   public static class ProjectEvent {
-    private Long projectProcessStepId, eventTypeId;
+    private Long projectProcessStepId, processStepId, eventTypeId;
     private String processStepName, resourceName;
     private Timestamp start, end;
   }
