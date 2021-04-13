43940711e6107267108f7c5889b9df4f39a3492a
Scott Keller <kellersk88@gmail.com>
2021-04-13 10:58:43 -0600

    fixed tournamnet stuff

--- src/main/resources/db/migration/procedures/brs/R__get_tournament_user_score.sql
+++ src/main/resources/db/migration/procedures/brs/R__get_tournament_user_score.sql
@@ -61,11 +61,6 @@ BEGIN
                                   from brs.project_details pd
                                   where ((pd.first_appointment at time zone 'UTC') at time zone v_timezone)::date between p_start_date and p_end_date
                                     and pd.first_appointment_missed_id is not null
-                                    and pd.setter_user_id = p_user_id) +
-                                 (select count(1) * -1
-                                  from brs.project_details pd
-                                  where ((pd.first_appointment at time zone 'UTC') at time zone v_timezone)::date between p_start_date and p_end_date
-                                    and pd.first_appointment_not_pitched_or_missed_id is not null
                                     and pd.setter_user_id = p_user_id))) as cnt;
 
             else
--- src/main/resources/db/migration/procedures/flow/R__project_details_function_trigger.sql
+++ src/main/resources/db/migration/procedures/flow/R__project_details_function_trigger.sql
@@ -219,8 +219,6 @@ declare
     v_field_name                     varchar;
     v_parent_custom_field_id         integer;
     v_project_id2                    integer;
-    v_parent_process_step_id                integer;
-    v_project_process_step_completed_date timestamp;
 BEGIN
 
     select pps.project_id
@@ -229,23 +227,12 @@ BEGIN
     where pps.id = new.project_process_step_id
       and pps.main is true;
 
-    select pps.project_id,ps.parent_process_step_id,pps.process_step_complete_date
-    into v_project_id1,v_parent_process_step_id,v_project_process_step_completed_date
+    select pps.project_id
+    into v_project_id1
     from flow.project_process_step pps
     inner join flow.process_step ps on pps.process_step_id = ps.id
     where pps.id = new.project_process_step_id;
 
-    if v_parent_process_step_id = 3166 and v_project_process_step_completed_date is not null then
-        update brs.project_details
-        set complete_date_booking = v_project_process_step_completed_date
-        where project_id = v_project_id1 and
-            complete_date_booking is null;
-    elsif v_parent_process_step_id = 3241 and v_project_process_step_completed_date is not null then
-        update brs.project_details
-        set complete_date_final_design_completion = v_project_process_step_completed_date
-        where project_id = v_project_id1
-        and complete_date_final_design_completion is null;
-    end if;
 
     select cf.field_name,cf.parent_custom_field_id
     into v_field_name,v_parent_custom_field_id
@@ -458,6 +445,42 @@ CREATE TRIGGER update_project_details_trg
     ON flow.project_process_step_custom_field_value
     FOR EACH ROW
 EXECUTE PROCEDURE flow.update_project_details_process_steps();
+
+CREATE OR REPLACE FUNCTION flow.pps_update_project_details()
+    RETURNS TRIGGER AS
+$body$
+declare
+    v_parent_process_step_id integer;
+BEGIN
+
+    select ps.parent_process_step_id
+    into v_parent_process_step_id
+    from  flow.process_step ps
+    where new.process_step_id = ps.id;
+
+    if v_parent_process_step_id = 3166 and new.process_step_complete_date is not null then
+        update brs.project_details
+        set complete_date_booking = new.process_step_complete_date
+        where project_id = new.project_id and
+            complete_date_booking is null;
+    elsif v_parent_process_step_id = 3241 and new.process_step_complete_date is not null then
+        update brs.project_details
+        set complete_date_final_design_completion = new.process_step_complete_date
+        where project_id = new.project_id
+          and complete_date_final_design_completion is null;
+    end if;
+
+    RETURN NULL;
+END
+$body$
+    LANGUAGE plpgsql;
+
+drop trigger if exists pps_update_project_details_trg on flow.project_process_step;
+CREATE TRIGGER pps_update_project_details_trg
+    after INSERT or update
+    ON flow.project_process_step
+    FOR EACH ROW
+EXECUTE PROCEDURE flow.pps_update_project_details();
 
 
 CREATE OR REPLACE FUNCTION flow.update_project_details_project()
