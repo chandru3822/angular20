ff992b72cb49f865220bf506e2f86a79877a5af7
Scott Keller <kellersk88@gmail.com>
2021-05-10 10:15:47 -0600

    fixed date for setter commissions

--- src/main/resources/db/migration/procedures/brs/R__get_commission_account_details_for_setters.sql
+++ src/main/resources/db/migration/procedures/brs/R__get_commission_account_details_for_setters.sql
@@ -189,7 +189,7 @@ BEGIN
                           inner join flow.company_user_status cus  on cus.user_id = u.id
                           inner join flow.user_status_type ust on ust.id = cus.user_status_type_id and ust.company_id = 3
                           left join flow.list_of_value lov_source on lov_source.id = pd.source
-                 WHERE pd.setter_milestone_pay::date > '2021-03-31' and
+                 WHERE pd.setter_milestone_pay::date >= '2021-05-03' and
                      CASE WHEN p_project_ids IS NOT NULL
                               THEN p.id = ANY(p_project_ids) ELSE
                          pd.setter_milestone_pay::date <= v_period_end_date  END
