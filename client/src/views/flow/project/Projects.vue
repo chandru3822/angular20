7fc04b0d772c474dc589b9766a5216e2f1e0bed2
Randa Nunn <randanunn@gmail.com>
2021-05-17 10:48:30 -0600

    right click project stuff

--- client/src/views/flow/project/Projects.vue
+++ client/src/views/flow/project/Projects.vue
@@ -67,19 +67,40 @@
           </template>
 
           <template #item="{item: project}">
-            <tr class="clickable"
-                @click="$router.push({name: 'projectDetails', params: {projectId: project.id}})">
-              <td class="text-left">{{project.id}}</td>
-              <td class="text-left">{{project.projectName}}</td>
-              <td class="text-left">{{project.stateAbbreviation}}</td>
-              <td class="text-left">{{project.projectStatusType}}</td>
-              <td class="text-left">{{project.dateCreated | formatDate('date')}}</td>
+            <tr class="clickable">
+              <td class="text-left">
+                <router-link class="no-text-decoration" :to="`/project/${project.id}/details`">
+                  {{project.id}}
+                </router-link>
+              </td>
+              <td class="text-left">
+                <router-link class="no-text-decoration" :to="`/project/${project.id}/details`">
+                  {{project.projectName}}
+                </router-link>
+              </td>
+              <td class="text-left">
+                <router-link class="no-text-decoration" :to="`/project/${project.id}/details`">
+                  {{project.stateAbbreviation}}
+                </router-link>
+              </td>
+              <td class="text-left">
+                <router-link class="no-text-decoration" :to="`/project/${project.id}/details`">
+                  {{project.projectStatusType}}
+                </router-link>
+              </td>
+              <td class="text-left">
+                <router-link class="no-text-decoration" :to="`/project/${project.id}/details`">
+                  {{project.dateCreated | formatDate('date')}}
+                </router-link>
+
+              </td>
             </tr>
           </template>
         </v-data-table>
 
         <SmartlistTable
           class="mt-3"
+          :type="'PROJECT'"
           v-else
           :smartlistId="selectedSmartlistId"
           @row-selected="goToSelectedProject"
@@ -168,6 +189,9 @@
       }
     },
     methods: {
+      goToRoute(projectId) {
+        this.$router.push({name: 'projectDetails', params: {projectId: projectId}})
+      },
       async getProjects() {
         const {page, itemsPerPage} = this.options
         try {
