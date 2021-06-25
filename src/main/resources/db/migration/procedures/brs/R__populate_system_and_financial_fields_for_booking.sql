fc4e0319e0bd86994748b1773d2e5b09ca97c4a9
Scott Keller <kellersk88@gmail.com>
2021-06-25 14:20:33 -0600

    added more stuff

--- src/main/resources/db/migration/procedures/brs/R__populate_system_and_financial_fields_for_booking.sql
+++ src/main/resources/db/migration/procedures/brs/R__populate_system_and_financial_fields_for_booking.sql
@@ -168,10 +168,7 @@ BEGIN
                              where cf.parent_custom_field_id = 10324
                                and cfga.archived is false and cf.archived is false and cfg.archived is false
                                and cf.company_id = v_company_id
-                               and cfg.process_step_id = p_process_step_id), case when coalesce(promotion_eighteen_months_free::numeric,0)::numeric > 0 and
-                                                                                       pd.proposal_complete_date::date between '2020-03-25'::date and '2020-04-30'::date then 1
-                                                                                  when coalesce(promotion_eighteen_months_free::numeric,0)::numeric > 0 and
-                                                                                       (pd.proposal_complete_date::date < '2020-03-25'::date or  pd.proposal_complete_date::date > '2020-04-30'::date) then 18 else 0 end,
+                               and cfg.process_step_id = p_process_step_id), plh.number_of_promotion_payments,
                             (select cfga.id
                              from flow.custom_field cf
                                       inner join flow.custom_field_group_assignment cfga on cfga.custom_field_id = cf.id
