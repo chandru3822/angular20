4a2f37a0fb17466c2661aa249e0ba989382966ed
Scott Humes <scotthumes@gmail.com>
2021-07-12 14:24:43 -0600

    Java action functions: Add functionality to for action logic to call the correct java functions with required params

--- src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
@@ -4,11 +4,13 @@ import com.albatross.api.v1.flow.model.ProcessStepActionChildFunction;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
+import java.util.Map;
+
 @Slf4j
 @Service
 public class BrsProcessStepActionFunctionService {
 
-  public static void updateLoanDocumentStatus(ProcessStepActionChildFunction func) {
-
+  public static void updateLoanDocumentStatus(ProcessStepActionChildFunction func, Map<String, Object> systemValues) {
+    // @TODO: implement this sucker
   }
 }
--- src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
+++ src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
@@ -4,6 +4,7 @@ import com.albatross.api.convert.JsonCollectionDeserializer;
 import com.albatross.api.security.SecurityService;
 import com.albatross.api.utils.CleanString;
 import com.albatross.api.utils.SqlCache;
+import com.albatross.api.v1.company.blueraven.services.BrsProcessStepActionFunctionService;
 import com.albatross.api.v1.flow.enums.SystemSettings;
 import com.albatross.api.v1.flow.model.*;
 import com.amazonaws.services.s3.AmazonS3;
@@ -33,6 +34,7 @@ import org.springframework.web.server.ResponseStatusException;
 
 import java.io.ByteArrayInputStream;
 import java.io.IOException;
+import java.lang.reflect.Method;
 import java.math.BigDecimal;
 import java.math.RoundingMode;
 import java.sql.Timestamp;
@@ -463,7 +465,7 @@ public class ProjectProcessStepService {
       this.setProjectStatus(pps.getProjectId(), action.getCompanyProjectStatusTypeId(), false);
     }
 
-    performChildFunctions(action.getId(), pps.getProjectProcessStepId(), pps.getProcessStepId());
+    performChildFunctions(action.getId(), pps.getProjectProcessStepId(), pps.getProcessStepId(), pps.getProjectId());
 
     ArrayList<Long> createdPpsIds = new ArrayList<>();
 
@@ -943,14 +945,32 @@ public class ProjectProcessStepService {
     return passed;
   }
 
-    public void performChildFunctions(Long actionId, Long ppsId, Long processStepId) {
+    public void performChildFunctions(Long actionId, Long ppsId, Long processStepId, Long projectId) {
 
         List<ProcessStepActionChildFunction> childFunctions = processStepActionService.getChildFunctionsWithParamValues(actionId, ppsId);
         childFunctions.forEach(childFunction -> {
             try {
                 if (childFunction.getRunInBackend()) {
-                  // @TODO:
-                  log.info("run in java");
+                  User user = securityService.getCurrentUser();
+
+                  final String originalFuncName = childFunction.getFunctionName();
+                  final int dot = originalFuncName.indexOf('.');
+                  final String functionAbbreviation = originalFuncName.substring(0, dot);
+                  final String functionName = originalFuncName.substring(dot + 1);
+
+                  Map<String, Object> systemValues = new HashMap<>();
+                  systemValues.put("processStepId", processStepId);
+                  systemValues.put("ppsId", ppsId);
+                  systemValues.put("projectId", projectId);
+                  systemValues.put("userId", user.getId());
+
+                  if (functionAbbreviation.equals("brs")) {
+                    Method method = BrsProcessStepActionFunctionService.class.getMethod(functionName, ProcessStepActionChildFunction.class, Map.class);
+                    method.invoke(null, childFunction, systemValues);
+                  } else {
+                    // @TODO: Add company IDs here during onboarding
+                  }
+
                 } else {
                   String params = String.join(", ", prepareFunctionParams(childFunction.getCompanyFunctionParams(), childFunction.getProjectId(), processStepId, ppsId));
                   String query = String.format("select * from %s(%s)", childFunction.getFunctionName(), params);
