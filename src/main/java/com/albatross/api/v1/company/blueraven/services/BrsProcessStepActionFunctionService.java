7f282cfd069c45e4c0cb2b83d80c967b718e9859
Scott Humes <scotthumes@gmail.com>
2021-07-12 15:50:49 -0600

    java sucks

--- src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
@@ -1,16 +1,35 @@
 package com.albatross.api.v1.company.blueraven.services;
 
 import com.albatross.api.v1.flow.model.ProcessStepActionChildFunction;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.json.JSONObject;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import javax.annotation.PostConstruct;
 import java.util.Map;
 
 @Slf4j
+@RequiredArgsConstructor(onConstructor = @__(@Autowired))
 @Service
 public class BrsProcessStepActionFunctionService {
 
+  private final LoanPalService loanPalServiceAutowire;
+
+  private static LoanPalService loanPalService;
+
+  @PostConstruct
+  public void init() {
+    BrsProcessStepActionFunctionService.loanPalService = loanPalServiceAutowire;
+  }
+
   public static void updateLoanDocumentStatus(ProcessStepActionChildFunction func, Map<String, Object> systemValues) {
-    // @TODO: implement this sucker
+
+    try {
+      JSONObject application = loanPalService.getApplicationByProjectId(systemValues.get("projectId").toString());
+    } catch (Exception e) {
+      // @TODO: implement this sucker
+    }
   }
 }
