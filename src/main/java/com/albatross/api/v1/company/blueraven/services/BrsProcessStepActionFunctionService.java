3f865ab8b98eb7c94fe3b18870c60cc95382b4f3
Scott Humes <scotthumes@gmail.com>
2021-07-20 09:19:57 -0600

    Java Action Functions: Put guts in the function to grab the loanpal application status and update the given CFGA ID

--- src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
@@ -1,5 +1,6 @@
 package com.albatross.api.v1.company.blueraven.services;
 
+import com.albatross.api.utils.SqlCache;
 import com.albatross.api.v1.flow.model.ProcessStepActionChildFunction;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
@@ -8,6 +9,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import javax.annotation.PostConstruct;
+import java.util.HashMap;
 import java.util.Map;
 
 @Slf4j
@@ -17,19 +19,40 @@ public class BrsProcessStepActionFunctionService {
 
   private final LoanPalService loanPalServiceAutowire;
 
+  private final SqlCache sqlCacheAutowire;
+
   private static LoanPalService loanPalService;
+
+  private static SqlCache sqlCache;
 
   @PostConstruct
   public void init() {
     BrsProcessStepActionFunctionService.loanPalService = loanPalServiceAutowire;
+    BrsProcessStepActionFunctionService.sqlCache = sqlCacheAutowire;
   }
 
-  public static void updateLoanDocumentStatus(ProcessStepActionChildFunction func, Map<String, Object> systemValues) {
+  public static void updateLoanDocumentStatus(ProcessStepActionChildFunction func, Map<String, Object> systemValues) throws Exception {
 
     try {
       JSONObject application = loanPalService.getApplicationByProjectId(systemValues.get("projectId").toString());
+
+      HashMap<String, Object> params = new HashMap<>();
+      params.put("userId", Long.parseLong(systemValues.get("userId").toString()));
+      params.put("sourceId", Long.parseLong(systemValues.get("ppsId").toString()));
+      params.put("customFieldGroupAssignmentId", Long.parseLong(func.getActionParamDynamicValues().get(0).getDynamicValue()));
+      params.put("textValue", application.get("status").toString());
+
+      //default values
+      params.put("dateValue", null);
+      params.put("timestampValue", null);
+      params.put("booleanValue", false);
+      params.put("numericValue", null);
+      params.put("intValue", null);
+      params.put("intArrayValue", null);
+
+      sqlCache.update("customFieldValues.process_step.upsertCustomFieldValue", params);
     } catch (Exception e) {
-      // @TODO: implement this sucker
+      throw e;
     }
   }
 }
