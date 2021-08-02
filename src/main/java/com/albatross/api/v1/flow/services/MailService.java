457d3923dc8f2076c4cbd1235126e76c6b1185ce
Kaleb Scholes <kscholes@gmail.com>
2021-08-02 12:05:01 -0600

    Upgrade dependencies and update to Java 16 (#8)
    * Updating dependencies
    
    * Updating some npm dependencies
    
    * Update to java 16
    
    * Clean up imports

--- src/main/java/com/albatross/api/v1/flow/services/MailService.java
+++ src/main/java/com/albatross/api/v1/flow/services/MailService.java
@@ -14,7 +14,6 @@ import javax.mail.internet.InternetAddress;
 import javax.mail.internet.MimeBodyPart;
 import javax.mail.internet.MimeMessage;
 import javax.mail.internet.MimeMultipart;
-import java.util.List;
 import java.util.Map;
 import java.util.Properties;
 
