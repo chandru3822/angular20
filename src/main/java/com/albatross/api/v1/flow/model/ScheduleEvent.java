b149c6f530b603c729184837c99243a558b37ea6
Randa Nunn <randanunn@gmail.com>
2021-08-02 11:30:48 -0600

    moar

--- src/main/java/com/albatross/api/v1/flow/model/ScheduleEvent.java
+++ src/main/java/com/albatross/api/v1/flow/model/ScheduleEvent.java
@@ -16,7 +16,7 @@ public class ScheduleEvent {
 
   private Long resourceId, companyId, systemListId, processStepId, systemListTypeId, projectId, projectProcessStepId, stateId, processStepStatusTypeId, companyProcessStepStatusTypeId,
       startCustomFieldValueId, startCustomFieldGroupAssignmentId, endCustomFieldValueId, endCustomFieldGroupAssignmentId,
-      resourceCustomFieldValueId, resourceCustomFieldGroupAssignmentId, contactId, eventId, projectProcessStepEventId,
+      resourceCustomFieldValueId, resourceCustomFieldGroupAssignmentId, contactId, eventId, projectProcessStepEventId, processStepEventId,
     //this is annoying but in order to make loading events work for multiple userPositions for the same user we need to pass back the userId. or i am dumb and cant figure it out otherwise.
     userId;
   private String resourceName, groupName, contactFirstName, contactLastName, contactFullName, projectName, processStepName,
