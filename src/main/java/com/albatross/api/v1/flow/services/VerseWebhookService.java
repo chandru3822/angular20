6517c2f431a8c12e00726f362662278ccb75254c
jberns003 <54857272+jberns003@users.noreply.github.com>
2021-08-18 16:37:40 -0400

    Verse integration changes

--- src/main/java/com/albatross/api/v1/flow/services/VerseWebhookService.java
+++ src/main/java/com/albatross/api/v1/flow/services/VerseWebhookService.java
@@ -29,8 +29,6 @@ public class VerseWebhookService {
 
   private final SqlCache sqlCache;
 
-  private final SecurityService securityService;
-
   public ResponseEntity updateContactLeadStatus(VersusLeadEvent versusLeadEvent) throws Exception {
     String msg = "";
 
@@ -51,7 +49,6 @@ public class VerseWebhookService {
         }
       }
 
-      User currentUser = securityService.getCurrentUser();
       String leadStatusId = checkIfCustomFieldDropdownValueExists(696, leadStatus);
       if (!leadStatusId.equals("null")) {
         HashMap<String, Object> params = new HashMap<>();
@@ -64,7 +61,8 @@ public class VerseWebhookService {
         params.put("intArrayValue", null);
         params.put("customFieldGroupAssignmentId", 399L);
         params.put("sourceId", Long.parseLong(versusLeadEvent.getExternalLeadId()));
-        params.put("userId", currentUser.getId());
+        // Sales Dev Lead's user ID
+        params.put("userId", 2371412L);
         sqlCache.update("customFieldValues.contact.upsertCustomFieldValue", params);
         msg = "VERSE: Contact Id " + versusLeadEvent.getExternalLeadId() + " lead status successfully updated";
       }
@@ -93,6 +91,7 @@ public class VerseWebhookService {
     params.put("state", contactMap.get("state"));
     params.put("postalCode", contactMap.get("postal_code"));
     params.put("zapierLeadId", contactMap.get("id"));
+    params.put("channelWebsite", contactMap.get("lead_source_detail"));
 
     Map<String, String> headers = new HashMap<>();
     headers.put("X-API-KEY", apiKey);
