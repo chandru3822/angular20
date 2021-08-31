fe327229373a5e8ffb3044d599c87b3b4b411a33
Scott Humes <scotthumes@gmail.com>
2021-08-30 21:18:07 -0600

    HOTFIX: Autotriggers - when changing status of a PPS, don't re-run autotriggers on parent PS

--- src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
+++ src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
@@ -165,12 +165,11 @@ public class ProjectProcessStepService {
     //run auto triggers for those actions
     List<ProjectProcessStep> steps = sqlCache.query("projectProcessStep.getUsingStatusByPpsId", params, ProjectProcessStep.class);
     for(ProjectProcessStep step : steps) {
-      //only run if the referring project process step is active
-      if(step.getProcessStepStatusTypeId() == 1) {
+      //only run if the referring project process step is active and not in the same process step we're currently running
+      if(step.getProcessStepStatusTypeId() == 1 && !step.getProcessStepId().equals(pps.getProcessStepId())) {
         performAutoTriggerActions(step.getProjectProcessStepId(), securityService.getCurrentUserDetails(), callingProcessStepActionId);
       }
     }
-
   }
 
   public void setMain(Long ppsId, CompanyProcessStepStatusType status) {
