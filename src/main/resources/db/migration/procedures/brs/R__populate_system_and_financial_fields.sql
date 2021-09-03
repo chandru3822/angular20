2455b2ff6117c29a59cce162d289aff0362b1611
Randa Nunn <randanunn@gmail.com>
2021-09-03 10:20:27 -0600

    empty string check for numeric

--- src/main/resources/db/migration/procedures/brs/R__populate_system_and_financial_fields.sql
+++ src/main/resources/db/migration/procedures/brs/R__populate_system_and_financial_fields.sql
@@ -78,7 +78,7 @@ BEGIN
                                             where cf.id = 11157
                                               and cfga.archived is false and cf.archived is false and cfg.archived is false
                                               and cf.company_id = v_company_id
-                                              and cfg.process_step_id = p_process_step_id), plh.storage_size_kwh::numeric,
+                                              and cfg.process_step_id = p_process_step_id), nullif(trim(plh.storage_size_kwh), '')::numeric,
                                            (select cfga.id
                                             from flow.custom_field cf
                                                    inner join flow.custom_field_group_assignment cfga on cfga.custom_field_id = cf.id
