85ec1ca72921dcb157b57a37c8d1d29ddbd0e55d
Scott Humes <scotthumes@gmail.com>
2021-09-08 13:01:32 -0600

    Autotriggers: Move running autotriggers for self (parent PPS) out of main autotrigger flow. Run autotriggers when manually creating new PPS.

--- src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
+++ src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
@@ -160,12 +160,12 @@ public class ProjectProcessStepService {
 
     sqlCache.query("projectProcessStep.setStatus", params, String.class);
 
-    //check for any actions using this PS - Status as a requirement - including SELF if active
+    //check for any actions using this PS - Status as a requirement - NOT including SELF (because that creates a potential infinite loop) if active
     //run auto triggers for those actions
     List<ProjectProcessStep> steps = sqlCache.query("projectProcessStep.getUsingStatusByPpsId", params, ProjectProcessStep.class);
     for(ProjectProcessStep step : steps) {
-      //only run if the referring project process step is active and not in the same process step we're currently running
-      if(step.getProcessStepStatusTypeId() == 1 && !step.getProcessStepId().equals(pps.getProcessStepId())) {
+      //only run if the referring project process step is active and we're in autotriggers
+      if(step.getProcessStepStatusTypeId() == 1 && callingProcessStepActionId != null) {
         performAutoTriggerActions(step.getProjectProcessStepId(), securityService.getCurrentUserDetails(), callingProcessStepActionId);
       }
     }
@@ -263,13 +263,13 @@ public class ProjectProcessStepService {
     if (performAutoTrigger) {
       this.performAutoTriggerActions(ppsId, securityService.getCurrentUserDetails(), null);
     }
-    //check for any actions using this PS - Status as a requirement - including SELF if active
+    //check for any actions using this PS - Status as a requirement - NOT including SELF (because that creates a potential infinite loop) if active
     //run auto triggers for those actions
     params.put("projectProcessStepId", ppsId);
     List<ProjectProcessStep> steps = sqlCache.query("projectProcessStep.getUsingStatusByPpsId", params, ProjectProcessStep.class);
     for(ProjectProcessStep step : steps) {
-      //only run if the referring project process step is active and not in the same process step we're currently running
-      if(step.getProcessStepStatusTypeId() == 1 && !step.getProcessStepId().equals(processStepId)) {
+      //only run if the referring project process step is active and we're in autotriggers
+      if(step.getProcessStepStatusTypeId() == 1 && callingProcessStepActionId != null) {
         performAutoTriggerActions(step.getProjectProcessStepId(), securityService.getCurrentUserDetails(), callingProcessStepActionId);
       }
     }
