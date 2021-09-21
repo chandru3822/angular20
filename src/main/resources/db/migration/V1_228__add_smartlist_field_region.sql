c9a3cc7c3812de25c27f4cb36bff1142752954e2
jberns003 <54857272+jberns003@users.noreply.github.com>
2021-09-21 15:29:25 -0400

    Code cleanup

--- src/main/resources/db/migration/V1_228__add_smartlist_field_region.sql
+++ src/main/resources/db/migration/V1_228__add_smartlist_field_region.sql
@@ -1,2 +1,2 @@
 insert into flow.smartlist_field (company_object_type_id, name, reference_table, reference_column, created_by_id, modified_by_id, archived, company_data_type_id, join_table, join_column, smartlist_system_list_id)
-values  (3, 'Region v3', 'obj', 'val ->> ''orgName''', 2350555, null, false, 1, null, null, null);
+values  (3, 'Region', 'obj', 'val ->> ''orgName''', 2350555, null, false, 1, null, null, null);
