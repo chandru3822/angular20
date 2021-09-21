46ab0499eff3bc54dfdb38938cef9e1c9962882e
jberns003 <54857272+jberns003@users.noreply.github.com>
2021-09-21 16:48:04 -0400

    Add new contact lead end points

--- src/main/java/com/albatross/api/v1/company/blueraven/controllers/ContactLeadController.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/controllers/ContactLeadController.java
@@ -213,4 +213,14 @@ public class ContactLeadController {
     contactLeadService.saveContactLead(contactLead);
   }
 
+  @PostMapping(value = "/allieddigitalmedia", produces = MediaType.APPLICATION_JSON_VALUE)
+  public void updateContactAlliedDigitalMedia(@RequestBody ContactLead contactLead) {
+    contactLeadService.saveContactLead(contactLead);
+  }
+
+  @PostMapping(value = "/solarmarketingexperts", produces = MediaType.APPLICATION_JSON_VALUE)
+  public void updateContactSolarMarketingExperts(@RequestBody ContactLead contactLead) {
+    contactLeadService.saveContactLead(contactLead);
+  }
+
 }
