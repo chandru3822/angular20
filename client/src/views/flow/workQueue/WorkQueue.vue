63845a13c849774f1ff82f6332735f3c043ab3e5
Randa Nunn <randanunn@gmail.com>
2021-09-22 11:51:13 -0600

    verbiage change

--- client/src/views/flow/workQueue/WorkQueue.vue
+++ client/src/views/flow/workQueue/WorkQueue.vue
@@ -54,7 +54,7 @@
                 color="primaryCustom"
                 v-model="hideFutureFollowUps"
                 class="mx-2 mt-5 wq-follow-up-switch"
-                label="Hide Records with a next follow-up date in the future"
+                label="Hide work with a next follow-up date in the future"
                 @change="getWorkQueues()"
               />
             </v-col>
