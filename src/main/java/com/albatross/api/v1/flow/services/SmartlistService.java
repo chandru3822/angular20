655bf3eaf278f9bf8b974a6c0fe055a7935007b8
Randa Nunn <randanunn@gmail.com>
2021-09-22 19:58:10 -0600

    include more projects for owners with multiple positions

--- src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
+++ src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
@@ -1418,8 +1418,10 @@ public class SmartlistService {
                 if (r.getSmartlistSystemListId() != null) {
                   whereClause.append(String.format(" %s %s %s and ", referenceLocation, operator, requirementValue));
                 } else {
-                  // If this field is process step owner, make sure we're getting past instances where this user had the same position and not just the current primary position
-                  if (r.getObjectTypeId() == 4 && Objects.equals(r.getReferenceTable(), "flow.user")) {
+                  // If this field is process step owner,
+                  // OR if this field is project owner,
+                  // make sure we're getting past instances where this user had the same position and not just the current primary position
+                  if ((r.getObjectTypeId() == 4 && Objects.equals(r.getReferenceTable(), "flow.user")) || (r.getObjectTypeId() == 1 && Objects.equals(r.getReferenceTable(), "flow.project_user")) ) {
                     final String positionSubquery = String.format("select id from flow.user_position where user_id = (select user_id from flow.user_position where id = %s)", requirementValue);
                     whereClause.append(String.format(" %s = any(%s) and ", referenceLocation, positionSubquery));
                   } else {
