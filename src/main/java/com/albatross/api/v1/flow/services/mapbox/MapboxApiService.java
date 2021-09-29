ec8eb376858905bdc55e7bfa8e6a3b486039599b
Scott Humes <scotthumes@gmail.com>
2021-09-29 13:51:19 -0600

    HOTFIX: Contacts -  Safety check for an empty feature list returned from Mapbox API

--- src/main/java/com/albatross/api/v1/flow/services/mapbox/MapboxApiService.java
+++ src/main/java/com/albatross/api/v1/flow/services/mapbox/MapboxApiService.java
@@ -66,7 +66,7 @@ public class MapboxApiService {
 
       FeatureCollection formattedData = FeatureCollection.fromJson(data.toString());
       String timezone = null;
-      if(null != formattedData && null != formattedData.features() && null != formattedData.features().get(0)) {
+      if(null != formattedData && null != formattedData.features() && formattedData.features().size() > 0 && null != formattedData.features().get(0)) {
         Feature firstFeature = formattedData.features().get(0);
         if(null != firstFeature.properties() && null != firstFeature.properties().get("TZID")) {
           timezone = firstFeature.properties().get("TZID").getAsString();
