e53b7b144344d4f343142e71df1a9d9e96f6889c
Randa Nunn <randanunn@gmail.com>
2021-09-30 08:57:32 -0600

    add some more contact lat/long stuff

--- src/main/java/com/albatross/api/v1/flow/services/HubspotWebhookService.java
+++ src/main/java/com/albatross/api/v1/flow/services/HubspotWebhookService.java
@@ -3,6 +3,7 @@ package com.albatross.api.v1.flow.services;
 import com.albatross.api.utils.SqlCache;
 import com.albatross.api.v1.company.blueraven.services.ContactLeadService;
 import com.albatross.api.v1.flow.model.RicochetLead;
+import com.albatross.api.v1.flow.services.mapbox.MapboxApiService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.io.IOUtils;
@@ -21,6 +22,8 @@ import org.springframework.stereotype.Service;
 
 import java.io.IOException;
 import java.util.HashMap;
+import java.util.List;
+import java.util.StringJoiner;
 
 import static java.nio.charset.StandardCharsets.UTF_8;
 
@@ -30,6 +33,7 @@ import static java.nio.charset.StandardCharsets.UTF_8;
 public class HubspotWebhookService {
     private final RequestConfig requestConfig = RequestConfig.custom().setCookieSpec(CookieSpecs.STANDARD).build();
     private final CloseableHttpClient client = HttpClients.custom().setDefaultRequestConfig(requestConfig).build();
+    private final MapboxApiService mapboxApiService;
 
     @Autowired
     private SqlCache sqlCache;
@@ -110,7 +114,28 @@ public class HubspotWebhookService {
         params.put("lastName", lead.getCustomer().getLastName());
         params.put("phoneNumber", lead.getCustomer().getPhone1());
         params.put("postalCode", lead.getCustomer().getAddress().getZip());
+        params.put("street1", lead.getCustomer().getAddress().getAddress1());
+        params.put("city", lead.getCustomer().getAddress().getCity());
         params.put("email", lead.getCustomer().getEmail());
+
+        Double latitude = null, longitude = null;
+        //even if the state value is null, try to get a valid lat/long if there is at least an address and a city
+        if(null != lead.getCustomer() && null != lead.getCustomer().getAddress()) {
+          try {
+            List<Double> coordinates = mapboxApiService.getLatLong(stringifyAddress(lead.getCustomer().getAddress().getAddress1(), lead.getCustomer().getAddress().getCity(), lead.getCustomer().getAddress().getState(), lead.getCustomer().getAddress().getZip()));
+            if (!coordinates.isEmpty() && null != coordinates.get(0) && null != coordinates.get(1)) {
+              //1 = lat, 0 = long
+              latitude = coordinates.get(1);
+              longitude = coordinates.get(0);
+            }
+          } catch (Exception ex) {
+            log.error("CONTACT: Exception when attempting to get geo location.");
+          }
+        }
+
+      //these will just insert as null unless a valid geo location was found from above
+      params.put("latitude", latitude);
+      params.put("longitude", longitude);
 
         Long contactId = sqlCache.updateReturningId("hubspotWebhook.saveLead", params, "id").longValue();
 
@@ -119,5 +144,14 @@ public class HubspotWebhookService {
 
         log.info("HUBSPOT: New HubSpot contact information was successfully saved to database for Contact ID " + contactId + " / Hubspot ID " + lead.getHubspot_id());
         return contactId;
+    }
+
+    public String stringifyAddress(String street1, String city, String state, String postalCode) {
+      StringJoiner sj = new StringJoiner(", ");
+      sj.add(street1);
+      sj.add(city);
+      sj.add(state + " " + postalCode);
+
+      return sj.toString();
     }
 }
