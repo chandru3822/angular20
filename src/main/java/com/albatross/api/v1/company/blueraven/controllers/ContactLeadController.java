76a4a95c606dbed4b4c3f8bd6405c073c61a334d
jberns003 <54857272+jberns003@users.noreply.github.com>
2021-10-08 12:48:37 -0400

    HOTFIX: Modify response for /cleanEnergyAuthority endpoint

--- src/main/java/com/albatross/api/v1/company/blueraven/controllers/ContactLeadController.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/controllers/ContactLeadController.java
@@ -6,6 +6,7 @@ import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.MediaType;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -49,8 +50,9 @@ public class ContactLeadController {
   }
 
   @PostMapping(value = "/cleanEnergyAuthority", produces = MediaType.APPLICATION_JSON_VALUE)
-  public void updateContactCleanEnergyAuthority(@RequestBody ContactLead contactLead) {
+  public ResponseEntity<Object> updateContactCleanEnergyAuthority(@RequestBody ContactLead contactLead) {
     contactLeadService.saveContactLead(contactLead);
+    return ResponseEntity.ok("");
   }
 
   @PostMapping(value = "/blueFireLeads", produces = MediaType.APPLICATION_JSON_VALUE)
