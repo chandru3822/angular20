3d420e34fd3b9b9f7f89509791707afa415acf1f
Randa Nunn <randanunn@gmail.com>
2021-10-21 11:01:19 -0600

    add root status for mobile

--- src/main/java/com/albatross/api/v1/flow/model/ProcessStepEventAction.java
+++ src/main/java/com/albatross/api/v1/flow/model/ProcessStepEventAction.java
@@ -13,7 +13,7 @@ import java.util.List;
 @Setter
 public class ProcessStepEventAction {
 
-  private Long id, companyEventStatusTypeId, companyProcessStepStatusTypeId, displayOrder, rootProcessStepStatusTypeId; //rootProcessStepStatusTypeId needed to determine if action can be run
+  private Long id, companyEventStatusTypeId, companyProcessStepStatusTypeId, displayOrder, rootEventStatusTypeId, rootProcessStepStatusTypeId; //rootProcessStepStatusTypeId needed to determine if action can be run, //rootEventStatusTypeId needed for mobile
   private String actionName, processStepStatusType, eventStatusType;
   private Boolean archived, requireStartTime, requireEndTime, requireResource,
     canPerform, logicListChanged, alwaysEnabled, alreadyTriggered, multipleUses;
