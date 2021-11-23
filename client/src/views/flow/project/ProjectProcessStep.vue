0f64e794b73d93d392e385e78c8546a91bfbae71
Randa Nunn <randanunn@gmail.com>
2021-11-23 13:54:19 -0700

    fix error showing when saving pps fields

--- client/src/views/flow/project/ProjectProcessStep.vue
+++ client/src/views/flow/project/ProjectProcessStep.vue
@@ -487,11 +487,12 @@
       },
       getProcessStep: async function () {
         try {
-          const {data} = await getRequest(`/projectProcessStep/${this.projectProcessStepId}`)
+          const {data, status} = await getRequest(`/projectProcessStep/${this.projectProcessStepId}`)
           this.processStep = {...data, newStatusToUse: {NEW_STATUS_TO_USE}}
           this.getAvailableStatuses()
           window.document.title = this.project?.id ? `${this.project.projectName} - ${this.processStep.processStepName}`
             : `${this.processStep.processStepName}`
+          return {data, status}
         } catch (e) {
           logError(e)
         } finally {
