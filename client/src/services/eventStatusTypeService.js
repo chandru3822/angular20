1f49757f8c7a9f102f2fca2a421ff765c69623a2
Randa Nunn <randanunn@gmail.com>
2021-12-09 14:31:44 -0700

    event admin screens

---
+++ client/src/services/eventStatusTypeService.js
@@ -0,0 +1,38 @@
+import {getRequest, getRequestWithParams} from '@/helpers/helpers'
+
+export async function getCompanyEventStatusTypes() {
+  try {
+    const {data} = await getRequest(`/event/companyStatus`)
+    return {data, status}
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function getEventStatusTypes() {
+  //these are at the root level
+  try {
+    const {data} = await getRequest(`/event/status`)
+    return {data, status}
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function getAvailableForEvent(eventId) {
+  try {
+    const {data} = await getRequest(`/event/status/company/availableForEvent/${eventId}`)
+    return {data, status}
+  } catch (e) {
+    throw e
+  }
+}
+
+export async function getAssignedToEvent(eventId) {
+  try {
+    const {data} = await getRequest(`/event/status/company/availableForEvent/${eventId}`)
+    return {data, status}
+  } catch (e) {
+    throw e
+  }
+}
