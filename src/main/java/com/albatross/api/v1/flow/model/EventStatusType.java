623dddba8c7581eb1015ec78aae0fb72086eaa33
Randa Nunn <randanunn@gmail.com>
2021-12-09 14:09:57 -0700

    event admin screens

--- src/main/java/com/albatross/api/v1/flow/model/EventStatusType.java
+++ src/main/java/com/albatross/api/v1/flow/model/EventStatusType.java
@@ -3,20 +3,16 @@ package com.albatross.api.v1.flow.model;
 import lombok.Getter;
 import lombok.Setter;
 
-import java.util.List;
-
-/**
- * Created by randanunn on 2019-05-20.
- * !Describe Purpose!
- */
 @Getter
 @Setter
 public class EventStatusType {
 
-  private Long id, companyId, resourceCustomFieldId;
-  private String eventName;
-  private Boolean archived;
-  private List<CustomFieldGroup> customFieldGroups;
-  private List<EventCompanyEventStatusType> companyEventStatusTypes;
-}
+  //oh boy. this is confusing af.  id = companyEventStatusTypeId.  EventStatusTypeId = rootEventStatusTypeId
+  private Long id, eventStatusTypeId, displayOrder;
 
+  private String eventStatusType, rootEventStatusType;
+  private Attachment icon;
+
+  private Boolean archived, isDefault;
+
+}
