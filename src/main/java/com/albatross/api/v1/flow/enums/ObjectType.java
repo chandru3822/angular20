a8cf3eb3d2b858a3e9398db540d57c90c2e2ca04
Randa Nunn <randanunn@gmail.com>
2021-12-09 14:13:01 -0700

    event admin screens

--- src/main/java/com/albatross/api/v1/flow/enums/ObjectType.java
+++ src/main/java/com/albatross/api/v1/flow/enums/ObjectType.java
@@ -1,33 +1,34 @@
 package com.albatross.api.v1.flow.enums;
 
 public enum ObjectType {
-    PROJECT(1L),
-    CONTACT(2L),
-    USER(3L),
-    PROCESS_STEP(4L),
-    ORGANIZATION(5L);
+  PROJECT(1L),
+  CONTACT(2L),
+  USER(3L),
+  PROCESS_STEP(4L),
+  ORGANIZATION(5L),
+  EVENT(6L);
 
-    public final Long id;
+  public final Long id;
 
-    ObjectType(Long id) {
-        this.id = id;
+  ObjectType(Long id) {
+    this.id = id;
+  }
+
+  public static ObjectType get(String name) {
+    name = name.toLowerCase();
+    for (ObjectType s : values()) {
+      if (s.toString().equals(name) || s.textValue().equals(name)) {
+        return s;
+      }
     }
+    throw new IllegalArgumentException();
+  }
 
-    public static ObjectType get(String name) {
-        name = name.toLowerCase();
-        for (ObjectType s : values()) {
-            if (s.toString().equals(name) || s.textValue().equals(name)) {
-                return s;
-            }
-        }
-        throw new IllegalArgumentException();
-    }
+  public String toString() {
+    return name().toLowerCase().replaceAll("_", " ");
+  }
 
-    public String toString() {
-        return name().toLowerCase().replaceAll("_", " ");
-    }
-
-    public String textValue() {
-        return name().toLowerCase();
-    }
+  public String textValue() {
+    return name().toLowerCase();
+  }
 }
