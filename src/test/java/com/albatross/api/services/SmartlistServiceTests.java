3e74d477be642f45153d815ddb1397fdf06d479b
Scott Humes <scotthumes@gmail.com>
2021-12-10 15:01:11 -0700

    Smartlists: Some makeshift unit tests for process step smartlists

--- src/test/java/com/albatross/api/services/SmartlistServiceTests.java
+++ src/test/java/com/albatross/api/services/SmartlistServiceTests.java
@@ -22,6 +22,7 @@ import javax.annotation.PostConstruct;
 import java.util.List;
 
 import static org.mockito.Mockito.*;
+import static org.springframework.test.util.AssertionErrors.fail;
 
 @Slf4j
 @ExtendWith(SpringExtension.class)
@@ -42,8 +43,6 @@ public class SmartlistServiceTests {
 
   private SmartlistService smartlistService;
 
-  private final List<Long> currentlyFailing = List.of(43L,71L,134L,112L,253L,255L,278L,378L,322L,384L,443L,595L,2385L,589L,598L,678L,2985L,661L,775L,2341L,755L,1171L,802L,2342L,842L,932L,943L,944L,983L,987L,1019L,1018L,1076L,1027L,1072L,2559L,1084L,1128L,1115L,1193L,1203L,1233L,1246L,646L,1998L,1272L,2513L,1237L,1273L,36L,377L,1296L,1999L,1312L,1360L,1341L,2001L,2426L,1428L,1430L,1972L,1469L,1591L,1568L,1065L,1740L,1759L,2309L,2357L,2384L,1788L,1838L,1850L,2500L,1918L,1880L,113L,127L,70L,155L,226L,147L,302L,393L,475L,489L,503L,518L,462L,529L,751L,823L,826L,853L,942L,1052L,1070L,1051L,1374L,1429L,1640L,1938L,444L,21L,59L,76L,84L,87L,123L,97L,201L,245L,294L,283L,323L,275L,367L,421L,426L,416L,429L,502L,483L,672L,720L,791L,794L,803L,830L,914L,1006L,947L,1064L,1126L,1144L,1098L,1155L,1554L,1271L,1293L,1332L,1441L,1629L,1644L,1321L,2410L,1682L,1678L,1792L,1814L,1904L,1940L,2502L,2969L,2986L,541L,2911L,3039L,2663L,2713L,2866L,2757L,2761L,2768L,2762L,2758L,2763L,2759L,2760L,2802L,2825L);
-
   @PostConstruct
   public void init() {
 
@@ -61,7 +60,7 @@ public class SmartlistServiceTests {
 
   @Test
   public void projectDetails() {
-    // The array in this query needs to keep update to date with currentlyFailing
+    final List<Long> currentlyFailingProjectDetails = List.of(43L,71L,134L,112L,253L,255L,278L,378L,322L,384L,443L,595L,2385L,589L,598L,678L,2985L,661L,775L,2341L,755L,1171L,802L,2342L,842L,932L,943L,944L,983L,987L,1019L,1018L,1076L,1027L,1072L,2559L,1084L,1128L,1115L,1193L,1203L,1233L,1246L,646L,1998L,1272L,2513L,1237L,1273L,36L,377L,1296L,1999L,1312L,1360L,1341L,2001L,2426L,1428L,1430L,1972L,1469L,1591L,1568L,1065L,1740L,1759L,2309L,2357L,2384L,1788L,1838L,1850L,2500L,1918L,1880L,113L,127L,70L,155L,226L,147L,302L,393L,475L,489L,503L,518L,462L,529L,751L,823L,826L,853L,942L,1052L,1070L,1051L,1374L,1429L,1640L,1938L,444L,21L,59L,76L,84L,87L,123L,97L,201L,245L,294L,283L,323L,275L,367L,421L,426L,416L,429L,502L,483L,672L,720L,791L,794L,803L,830L,914L,1006L,947L,1064L,1126L,1144L,1098L,1155L,1554L,1271L,1293L,1332L,1441L,1629L,1644L,1321L,2410L,1682L,1678L,1792L,1814L,1904L,1940L,2502L,2969L,2986L,541L,2911L,3039L,2663L,2713L,2866L,2757L,2761L,2768L,2762L,2758L,2763L,2759L,2760L,2802L,2825L);
     final String query = """
       select s.id
       from flow.smartlist s
@@ -78,10 +77,69 @@ public class SmartlistServiceTests {
         log.info("Running smartlist ID: " + id);
         smartlistService.getSmartlistResults(id);
       } catch (Exception e) {
-        if (!currentlyFailing.contains(id)) {
+        if (!currentlyFailingProjectDetails.contains(id)) {
           log.error(String.format("Failed on smartlist ID: %s", id));
           log.error(e.getMessage());
         }
+      }
+    });
+  }
+
+  @Test
+  public void processStepNonMain() {
+    final List<Long> currentlyFailingProcessStepNonMain = List.of(1359L,1361L,1370L,1395L,1427L,1474L,1964L,2390L,2392L,2413L,2416L,2441L,2568L,2914L);
+
+    final String query = """
+      select s.id
+      from flow.smartlist s
+             inner join flow.company_object_type cot on cot.id = s.company_object_type_id
+      where s.archived is false and
+            cot.company_id = 3 and
+            s.project_details = false and
+            cot.object_type_id = 4 and
+            s.main_process_steps = false
+      order by s.id;
+    """;
+    List<Long> smartlistIds = sqlCacheRO.queryBySql(query, null, new SingleColumnRowMapper<>(Long.class));
+    smartlistIds.forEach(id -> {
+      try {
+        log.info("Running smartlist ID: " + id);
+        smartlistService.getSmartlistResults(id);
+      } catch (Exception e) {
+        if (!currentlyFailingProcessStepNonMain.contains(id)) {
+          log.error(String.format("Failed on smartlist ID: %s", id));
+          fail(e.getMessage());
+        }
+      }
+    });
+  }
+
+  @Test
+  public void processStepMain() {
+//    final List<Long> currentlyFailingProcessStepMain = List.of(1359L,1361L,1370L,1395L,1427L,1474L,1964L,2390L,2392L,2413L,2416L,2441L,2568L,2914L);
+
+    final String query = """
+      select s.id
+      from flow.smartlist s
+             inner join flow.company_object_type cot on cot.id = s.company_object_type_id
+      where s.archived is false and
+            cot.company_id = 3 and
+            s.project_details = false and
+            cot.object_type_id = 4 and
+            s.main_process_steps = true
+      order by s.id;
+    """;
+    List<Long> smartlistIds = sqlCacheRO.queryBySql(query, null, new SingleColumnRowMapper<>(Long.class));
+    smartlistIds.forEach(id -> {
+      try {
+        log.info("Running smartlist ID: " + id);
+        smartlistService.getSmartlistResults(id);
+      } catch (Exception e) {
+//        if (!currentlyFailingProcessStepNonMain.contains(id)) {
+          log.error(String.format("Failed on smartlist ID: %s", id));
+          log.info(e.getMessage());
+//          fail(e.getMessage());
+//        }
       }
     });
   }
