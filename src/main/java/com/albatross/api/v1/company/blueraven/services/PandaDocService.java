6d50387a4eed78d6c7eb1c573769ad796e4b5f20
jberns003 <54857272+jberns003@users.noreply.github.com>
2021-12-16 14:01:32 -0500

    PandaDocs modify template selection code to use contains

--- src/main/java/com/albatross/api/v1/company/blueraven/services/PandaDocService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/PandaDocService.java
@@ -131,7 +131,7 @@ public class PandaDocService {
         JSONObject tpl = templates.getJSONObject(i);
         String tplName = tpl.getString("name");
         String version = tpl.getString("version");
-        if (name.equals(tplName) && version.equals("2")) {
+        if (tplName.contains(name) && version.equals("2")) {
           tplId = tpl.getString("id");
           log.debug("PANDADOC: selected template named {}", tplName);
           break;
