08ac88669b852a2674857bcb49f5252fa2102aaf
turekj <turekj@ldschurch.org>
2022-01-13 11:06:19 -0600

    tweak scrolling project.vue

--- client/src/views/flow/project/Project.vue
+++ client/src/views/flow/project/Project.vue
@@ -138,7 +138,7 @@
       </v-toolbar-items>
     </v-toolbar>
     <v-row class="project-split-container">
-      <div class="white-bg project-section"
+      <div class="white-bg project-section overflow-y-auto"
            :class="{'col-3': !collapseLeftSidebar, 'collapse-left text-center': collapseLeftSidebar}">
         <v-btn small text @click="collapseLeftSidebar = !collapseLeftSidebar">
           <v-icon>mdi-menu</v-icon>
@@ -194,7 +194,7 @@
           <UpcomingEvents v-if="userHasEventsFeature" :projectId="projectId"/>
         </div>
       </div>
-      <div class="router-view-column project-section" :class="{'col-5': !collapseLeftSidebar && !collapseRightSidebar,
+      <div class="overflow-y-auto project-section" :class="{'col-5': !collapseLeftSidebar && !collapseRightSidebar,
                                                                  'center-width-left-side-collapse': collapseLeftSidebar && !collapseRightSidebar,
                                                                  'center-width-right-side-collapse': !collapseLeftSidebar && collapseRightSidebar,
                                                                  'center-width-both-collapse': collapseLeftSidebar && collapseRightSidebar}">
@@ -460,12 +460,11 @@ export default {
   margin-left: 0 !important;
 }
 
-.router-view-column {
+.overflow-y-auto {
   overflow: auto;
 }
 
 .project-section {
-  overflow: auto;
   max-height: calc(100% - 15px);
 }
 
