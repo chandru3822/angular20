0aaa113fd5215dced708b976d8e99ca5d903b63d
Kaleb Scholes <kscholes@gmail.com>
2022-01-20 11:31:34 -0700

    Cleanup

--- src/main/java/com/albatross/api/v1/flow/model/Project.java
+++ src/main/java/com/albatross/api/v1/flow/model/Project.java
@@ -1,10 +1,13 @@
 package com.albatross.api.v1.flow.model;
 
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 
 import java.util.List;
 
+@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 @NoArgsConstructor
 public class Project {
