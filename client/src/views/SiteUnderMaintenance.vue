98385595d0a937f9ffc5218cc56aca446f2e8de3
Randa Nunn <randanunn@gmail.com>
2022-02-03 16:19:44 -0700

    more maintenance mode stuff

--- client/src/views/SiteUnderMaintenance.vue
+++ client/src/views/SiteUnderMaintenance.vue
@@ -7,8 +7,28 @@
             <v-toolbar dark color="primaryCustom">
               <v-toolbar-title>Site is Under Maintenance</v-toolbar-title>
             </v-toolbar>
-            <v-card-text class="login-card-text">
-              please check back later
+            <v-card-text>
+              <router-link :to="`/login`">Back to Login</router-link>
+              <div class="py-4 font-size-16">
+                Albatross is under maintenance. <br/>
+                If you are a mobile user, please watch the training video below. <br/>
+                The site will be available again on Monday, Feb 14th.
+              </div>
+              <div class="text-center one-hunned">
+                <video
+                  v-for="a in attachments"
+                  class="maintenance-c"
+                  :key="a.presignedUrl"
+                  width="450"
+                  height="400"
+                  controls
+                >
+                  <source
+                    :src="a.presignedUrl"
+                    type="video/mp4"
+                  >
+                </video>
+              </div>
             </v-card-text>
           </v-card>
         </v-col>
@@ -20,18 +40,41 @@
 
 <script>
   import constants from '@/helpers/constants'
-  import {handleHidingGlobalLoader, postRequest, getSnackbar} from '@/helpers/helpers'
+  import {handleHidingGlobalLoader, postRequest, getSnackbar, getRequest} from '@/helpers/helpers'
   import {AppMutations} from '@/stores/AppStore'
+  import axios from 'axios'
 
   export default {
     name: 'SiteUnderMaintenance',
     data () {
       return {
         snackbar: {},
+        attachments: []
       }
     },
+    created () {
+      this.getMaintenanceAttachments()
+    },
     methods: {
-
+      async getMaintenanceAttachments () {
+        try {
+          const {data} = await axios.get(`${constants.VUE_APP_BASE_API}/public/maintenanceAttachments`)
+          this.attachments = data
+        } catch(e) {
+          console.error('*** ERROR ***', e)
+          this.snackbar = getSnackbar('ERROR', 'Error Retrieving Files')
+          this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
+          this.$store.commit(AppMutations.SET_LOADING, false)
+        }
+      },
     }
   }
 </script>
+
+
+<style scoped lang="scss">
+.maintenance-content {
+
+}
+
+</style>
