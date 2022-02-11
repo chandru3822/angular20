4ba6176b73a784b046bdff3e8af04f120e3c1d66
Scott Keller <kellersk88@gmail.com>
2022-02-11 09:18:40 -0700

    status changes for events

--- src/main/resources/db/migration/procedures/flow/R__migrate_event_status.sql
+++ src/main/resources/db/migration/procedures/flow/R__migrate_event_status.sql
@@ -1,4 +1,5 @@
-drop function if exists flow.migrate_event_status(integer,  integer,  timestamp,  timestamp,  integer,  integer,  integer,  integer);
+drop function if exists flow.migrate_event_status(integer, integer, timestamp, timestamp, integer, integer, integer,
+                                                  integer);
 CREATE OR REPLACE function flow.migrate_event_status(p_project_process_step_id integer,
                                                      p_project_id integer,
                                                      p_start_time timestamp,
@@ -39,6 +40,7 @@ declare
   v_complete_timestamp4                   timestamp;
   v_process_step_id                       integer;
   v_online_timestamp_value                timestamp;
+  v_main                                  boolean;
 BEGIN
   v_online_timestamp_value = null;
   select process_step_id
@@ -85,8 +87,9 @@ BEGIN
 
   select psst.id,
          coalesce(pps.cancelled_date, pps.date_modified),
-         coalesce(pps.process_step_complete_date, pps.date_modified)
-  into v_company_status_id1,v_cancel_timestamp,v_complete_timestamp1
+         coalesce(pps.process_step_complete_date, pps.date_modified),
+         pps.main
+  into v_company_status_id1,v_cancel_timestamp,v_complete_timestamp1,v_main
   from flow.project_process_step pps
          inner join flow.company_process_step_status_type cpsst on pps.company_process_step_status_type_id = cpsst.id
          inner join flow.process_step_status_type psst on cpsst.process_step_status_type_id = psst.id
@@ -127,34 +130,64 @@ BEGIN
   end if;
 
 
-  --   raise notice 'second child=%',v_company_status_id2;
---   raise notice 'third child=%',v_company_status_id3;
---   raise notice 'fourth child=%',v_company_status_id4;
   if v_online_timestamp_value is not null and v_online_timestamp_value < now() then
     v_process_event_status_id = v_company_event_status_type_id_complete;
+    v_return_complete_timestamp =
+      least(v_complete_timestamp1, v_complete_timestamp2, v_complete_timestamp3, v_complete_timestamp4);
   elsif v_online_timestamp_value is not null and v_online_timestamp_value > now() then
     v_process_event_status_id = v_company_event_status_type_id_pending;
-  elsif v_company_status_id1 = 3 then
-    v_process_event_status_id = v_company_event_status_type_id_NCO;
-    v_return_cancel_timestamp = v_cancel_timestamp;
-    v_return_complete_timestamp = null;
-  elsif p_start_time is null and p_end_time is null then
-    v_process_event_status_id = v_company_event_status_type_id_RTS;
-  elsif p_start_time > now() then
-    v_process_event_status_id = v_company_event_status_type_id_pending;
-  elsif p_start_time is not null and p_end_time is not null and
-        (p_end_time < now() or now() between p_start_time and p_end_time) then
-    if v_company_status_id1 = 1 or (v_company_status_id2 is not null and v_company_status_id2 = 1) or
+  elsif v_company_status_id1 = 1 then
+    if p_start_time > now() or now() between p_start_time and p_end_time then
+      v_process_event_status_id = v_company_event_status_type_id_pending;
+    elsif ((p_end_time < now()) or (p_start_time < now() and p_end_time is null)) then
+      v_process_event_status_id = v_company_event_status_type_id_NV;
+    end if;
+  elsif v_company_status_id1 = 2 and v_main is true then
+    if (v_company_status_id2 is not null and v_company_status_id2 = 1) or
        (v_company_status_id3 is not null and v_company_status_id3 = 1) or
        (v_company_status_id4 is not null and v_company_status_id4 = 1) then
-      v_process_event_status_id = v_company_event_status_type_id_NV;
+      if p_start_time > now() or now() between p_start_time and p_end_time then
+        v_process_event_status_id = v_company_event_status_type_id_pending;
+        update flow.project_process_step
+        set company_process_step_status_type_id = 76
+        where id = p_project_process_step_id;
+      else
+        v_process_event_status_id = v_company_event_status_type_id_NV;
+        update flow.project_process_step
+        set company_process_step_status_type_id = 76
+        where id = p_project_process_step_id;
+      end if;
+    elsif (v_company_status_id2 is not null and v_company_status_id2 = 3) or
+          (v_company_status_id3 is not null and v_company_status_id3 = 3) or
+          (v_company_status_id4 is not null and v_company_status_id4 = 3) then
+      v_process_event_status_id = v_company_event_status_type_id_NCO;
+      v_return_cancel_timestamp = v_cancel_timestamp;
     else
       v_process_event_status_id = v_company_event_status_type_id_complete;
-      v_return_cancel_timestamp = null;
       v_return_complete_timestamp =
-        least(v_complete_timestamp1, v_complete_timestamp2, v_complete_timestamp3, v_complete_timestamp4);
+        greatest(v_complete_timestamp1, v_complete_timestamp2, v_complete_timestamp3, v_complete_timestamp4);
     end if;
+  elsif v_company_status_id1 = 2 and v_main is false then
+    if (v_company_status_id2 is not null and v_company_status_id2 = 3) or
+       (v_company_status_id3 is not null and v_company_status_id3 = 3) or
+       (v_company_status_id4 is not null and v_company_status_id4 = 3) then
+      v_process_event_status_id = v_company_event_status_type_id_c;
+      v_return_cancel_timestamp = v_cancel_timestamp;
+    elsif
+        (v_company_status_id2 is null or (v_company_status_id2 is not null and v_company_status_id2 = 2)) and
+        (v_company_status_id3 is null or (v_company_status_id3 is not null and v_company_status_id3 = 2)) and
+        (v_company_status_id4 is null or (v_company_status_id4 is not null and v_company_status_id4 = 2)) then
+      v_process_event_status_id = v_company_event_status_type_id_complete;
+      v_return_complete_timestamp =
+        greatest(v_complete_timestamp1, v_complete_timestamp2, v_complete_timestamp3, v_complete_timestamp4);
+    else
+      v_process_event_status_id = v_company_event_status_type_id_pending;
+    end if;
+  else
+    v_process_event_status_id = v_company_event_status_type_id_nco;
+    v_return_cancel_timestamp = v_cancel_timestamp;
   end if;
+
   return query select v_process_event_status_id,
                       v_return_cancel_timestamp,
                       v_return_complete_timestamp,
@@ -165,4 +198,5 @@ end
 $$
   LANGUAGE plpgsql VOLATILE
                    COST 100;
+
 
