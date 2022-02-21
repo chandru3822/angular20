e11c30a8dbbc4b4527f63bb88d4406cdda5ff291
Randa Nunn <randanunn@gmail.com>
2022-02-21 14:40:41 -0700

    event user position endpoint for mobile

--- src/main/java/com/albatross/api/v1/flow/services/ProcessStepEventService.java
+++ src/main/java/com/albatross/api/v1/flow/services/ProcessStepEventService.java
@@ -11,6 +11,7 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.BeanWrapper;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.SingleColumnRowMapper;
 import org.springframework.stereotype.Service;
 
 import java.util.HashMap;
@@ -87,6 +88,19 @@ public class ProcessStepEventService {
       sqlCache.update("processStepEvent.updateDisplayOrder", params);
     }
   }
+
+  public Boolean userCanEditStartTime(Long processStepEventId) {
+    User currentUser = securityService.getCurrentUser();
+    HashMap<String, Object> params = new HashMap<>();
+    params.put("processStepEventId", processStepEventId);
+
+    List<Long> whiteListedPositionIds = sqlCache.query("processStepEvent.getStartTimeWhitelistedPositionIds", params, new SingleColumnRowMapper<>(Long.class));
+
+    Boolean userHasPosition = securityService.userHasPosition(currentUser.getHighestCompanyId(), whiteListedPositionIds, currentUser.getUserPositions());
+
+    return userHasPosition;
+  }
+
 
   public void deleteEventFromStep(Long id) {
     User currentUser = securityService.getCurrentUser();
