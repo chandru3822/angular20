632a5c86fb310fcbbdfc3886f4fee9e547d9d2ec
turekj <turekj@ldschurch.org>
2022-02-28 20:48:47 -0600

    Use global delete dialog in one place in EventComponents.vue
    I can't add an event status so I can't test that the other dialog works

--- client/src/views/flow/settings/event/EventComponents.vue
+++ client/src/views/flow/settings/event/EventComponents.vue
@@ -57,6 +57,8 @@
                 <td class="text-left">{{ item.rootEventStatusType }}</td>
                 <td class="text-right">
                   <div class="flex-display">
+<!--                    todo: I can't add an event status so I can't test that this dialog works-->
+<!--                    <confirm-delete-dialog :item-to-delete="item.eventStatusType" @confirm-delete="deleteStatusTypeFromEvent(item)"></confirm-delete-dialog>-->
                     <v-dialog
                       v-if="userCanEdit"
                       v-model="item.deleteConfirm"
@@ -138,44 +140,7 @@
                 <v-list-item-content>
                   {{a.attachmentType}}
                 </v-list-item-content>
-                <v-dialog
-                  v-if="userCanEdit"
-                  v-model="a.deleteConfirm"
-                  width="500">
-                  <template v-slot:activator="{ on }">
-                    <v-list-item-action class="clickable" v-on="on">
-                      <v-icon>delete</v-icon>
-                    </v-list-item-action>
-                  </template>
-                  <v-card>
-                    <v-card-title
-                      class="text-h5 grey lighten-2"
-                      primary-title
-                    >
-                      Confirm
-                    </v-card-title>
-
-                    <v-card-text>
-                      Are you sure you want to delete this attachment type: <strong>{{ a.attachmentType }}</strong>?
-                    </v-card-text>
-
-                    <v-divider></v-divider>
-
-                    <v-card-actions>
-                      <v-spacer></v-spacer>
-                      <v-btn
-                        @click="a.deleteConfirm = false">
-                        No
-                      </v-btn>
-                      <v-btn
-                        color="primaryCustom"
-                        text
-                        @click="[a.archived = true, deleteAttachmentType(a.id)]">
-                        Yes
-                      </v-btn>
-                    </v-card-actions>
-                  </v-card>
-                </v-dialog>
+                <confirm-delete-dialog label="this attachment type" :item-to-delete="a.attachmentType" @confirm-delete="[a.archived = true, deleteAttachmentType(a.id)]"></confirm-delete-dialog>
               </v-list-item>
             </v-list>
           </draggable>
@@ -192,11 +157,13 @@ import {getAvailableForEvent} from '@/services/eventStatusTypeService'
 import {deleteRequest, getRequest, getSnackbar, postRequest, putRequest} from "@/helpers/helpers";
 import Vue2Filters from "vue2-filters"
 import orderBy from "lodash.orderby"
+import ConfirmDeleteDialog from "@/ConfirmDeleteDialog";
 
 export default {
   name: 'EventComponents',
   mixins: [Vue2Filters.mixin],
   components: {
+    ConfirmDeleteDialog,
     draggable
   },
   data () {
