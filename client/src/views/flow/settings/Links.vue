0676f63454790a665dc44f46cb93845706180766
turekj <turekj@ldschurch.org>
2022-03-08 15:25:09 -0600

    Update all delete buttons in Custom Components section of settings to the ConfirmDeleteDialog.vue

--- client/src/views/flow/settings/Links.vue
+++ client/src/views/flow/settings/Links.vue
@@ -42,44 +42,12 @@
                 <v-icon v-if="selectedLinkId === a.id" @click="saveLink(a)">save</v-icon>
                 <v-icon v-else @click="selectedLinkId = a.id">edit</v-icon>
               </v-list-item-action>
-              <v-dialog
+              <confirm-delete-dialog
                   v-if="$store.getters.userHasFeatureAccessLevel('SETTINGS', 'DELETE')"
-                  v-model="a.deleteConfirm"
-                  width="500">
-                <template v-slot:activator="{ on }">
-                  <v-list-item-action class="clickable" v-on="on">
-                    <v-icon>delete</v-icon>
-                  </v-list-item-action>
-                </template>
-                <v-card>
-                  <v-card-title
-                      class="text-h5 grey lighten-2"
-                      primary-title
-                  >
-                    Confirm
-                  </v-card-title>
-
-                  <v-card-text>
-                    Are you sure you want to delete this link: <strong>{{ a.link }}</strong>?
-                  </v-card-text>
-
-                  <v-divider></v-divider>
-
-                  <v-card-actions>
-                    <v-spacer></v-spacer>
-                    <v-btn
-                        @click="a.deleteConfirm = false">
-                      No
-                    </v-btn>
-                    <v-btn
-                        color="primaryCustom"
-                        text
-                        @click="[a.archived = true, deleteLink(a.id)]">
-                      Yes
-                    </v-btn>
-                  </v-card-actions>
-                </v-card>
-              </v-dialog>
+                  label="this link: "
+                  :item-to-delete="a.link"
+                  @confirm-delete="[a.archived = true, deleteLink(a.id)]"
+              ></confirm-delete-dialog>
             </v-list-item>
           </v-list>
         </v-container>
@@ -97,9 +65,11 @@
 
   import {handleHidingGlobalLoader, getRequest, deleteRequest, putRequest, postRequest, getSnackbar} from '@/helpers/helpers'
   import constants from '@/helpers/constants'
+  import ConfirmDeleteDialog from "@/ConfirmDeleteDialog";
 
   export default {
     name: 'Links',
+    components: {ConfirmDeleteDialog},
     mixins: [Vue2Filters.mixin],
 
     data () {
