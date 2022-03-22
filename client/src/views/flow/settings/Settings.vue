397b5a0e8dc0ff46d595b3264e1bbcc455f53845
Randa Nunn <randanunn@gmail.com>
2022-03-21 21:04:17 -0600

    fix double scrolling in settings page

--- client/src/views/flow/settings/Settings.vue
+++ client/src/views/flow/settings/Settings.vue
@@ -1,7 +1,7 @@
 <template>
   <v-container class="pt-0">
-    <v-row>
-      <v-col cols="12" md="3" class="text-left pt-0 px-0 left-column">
+    <v-row class="settings-container">
+      <v-col cols="12" md="3" class="text-left pa-0 left-column">
         <v-menu data-app left
                 v-if="constants.IS_MOBILE"
                 offset-y
@@ -281,21 +281,31 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.settings-container {
+  height: calc(100vh - 50px);
+  overflow: hidden;
+}
+
 a {
   text-decoration: none;
 }
 
 .left-menu {
-  height: 100vh;
+  max-height: 100%;
+  height: 100%;
   overflow: auto;
 }
 
 .left-column {
   background-color: #F6F7F8;
+  height: 100%;
+  max-height: 100%;
 }
 
 .main-section {
   background-color: #fff;
+  max-height: 100%;
+  overflow: auto;
 }
 
 .dense-setting-row {
