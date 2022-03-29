d1d618d44f28de59e45fca507a75802d7839d98f
Scott Humes <scotthumes@gmail.com>
2022-03-29 09:46:57 -0600

    HOTFIX: Perform cron autotriggers on startup

--- src/main/java/com/albatross/api/config/ScheduledConfig.java
+++ src/main/java/com/albatross/api/config/ScheduledConfig.java
@@ -75,11 +75,11 @@ public class ScheduledConfig implements SchedulingConfigurer {
     @PostConstruct
     public void init() {
         log.info("*** CRON: cron service enabled ***");
-//      if (initialAutoTriggers) {
-//        log.info("*** CRON: start INITIAL auto triggers ***");
-//        projectProcessStepService.performInitialAutoTriggers();
-//        log.info("*** CRON: end INITIAL auto triggers ***");
-//      }
+        if (autoTriggers) {
+            log.info("*** CRON: start auto triggers ***");
+            projectProcessStepService.performTimeBasedAutoTriggers();
+            log.info("*** CRON: end auto triggers ***");
+        }
     }
 
     //    every  minute
