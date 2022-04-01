5a014bb25eaee27ef4b159e9834e7ccadb185f1c
Randa Nunn <randanunn@gmail.com>
2022-04-01 13:18:50 -0600

    turn off loud genesys error

--- src/main/java/com/albatross/api/v1/company/blueraven/services/GenesysService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/GenesysService.java
@@ -430,10 +430,10 @@ public class GenesysService {
             apiInstance.putOutboundContactlistContact(contactListId, genesysContactId.get(), dc);
           } catch (ApiException ae) {
             JSONObject apiException = new JSONObject(ae.getRawBody());
-            log.error(
-                "GENE: Error updating contactId={}, msg={}",
-                contactId,
-                apiException.getString("message"));
+//            log.error(
+//                "GENE: Error updating contactId={}, msg={}",
+//                contactId,
+//                apiException.getString("message"));
           }
         }
       }
