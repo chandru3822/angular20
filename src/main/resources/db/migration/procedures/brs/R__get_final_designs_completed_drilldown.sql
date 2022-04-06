af3cf460f1bcc7bcbf0bbfa17d172b0748a720cc
Randa Nunn <randanunn@gmail.com>
2022-04-06 13:41:09 -0600

    milestone fixes

--- src/main/resources/db/migration/procedures/brs/R__get_final_designs_completed_drilldown.sql
+++ src/main/resources/db/migration/procedures/brs/R__get_final_designs_completed_drilldown.sql
@@ -1,48 +1,59 @@
 CREATE OR REPLACE FUNCTION brs.get_final_designs_completed_drilldown(p_user_id integer, p_quarter integer)
-	RETURNS SETOF json AS
+  RETURNS SETOF json AS
 $BODY$
 declare
-	v_start_date date;
-	v_end_date date;
-	v_year integer;
+  v_start_date date;
+  v_end_date date;
+  v_year integer;
 BEGIN
-	select extract('year' from now())::integer
-	into v_year;
+  select extract('year' from now())::integer
+  into v_year;
 
-	case when p_quarter = 1 then
-		v_start_date := (v_year || '-01-01')::date;
-		v_end_date := (v_year || '-03-31')::date;
-	when p_quarter = 2 then
-		v_start_date := (v_year || '-04-01')::date;
-		v_end_date := (v_year || '-06-30')::date;
+  case when p_quarter = 1 then
+    v_start_date := (v_year || '-01-01')::date;
+    v_end_date := (v_year || '-03-31')::date;
+  when p_quarter = 2 then
+    v_start_date := (v_year || '-04-01')::date;
+    v_end_date := (v_year || '-06-30')::date;
   when p_quarter = 3 then
     v_start_date := (v_year || '-07-01')::date;
-		v_end_date := (v_year || '-09-30')::date;
+    v_end_date := (v_year || '-09-30')::date;
   else
     v_start_date := (v_year || '-10-01')::date;
-		v_end_date := (v_year || '-12-31')::date;
-	end case;
+    v_end_date := (v_year || '-12-31')::date;
+  end case;
 
   RETURN QUERY select array_to_json(array_agg(row_to_json(sub_rows)))
     from (
-      select concat(c.first_name, ' ', c.last_name) as customer_name,
-             p.id,
+      select pd.contact_name as customer_name,
+             pd.project_id as id,
              pd.source_name,
-             concat(u.first_name, ' ', u.last_name) as owner_name,
+             pd.closer_name as owner_name,
              pd.system_size,
-             pd.final_design_complete_date
-      from flow.project p
-        inner join flow.contact c on c.id = p.contact_id
-        inner join brs.project_details pd on pd.project_id = p.id
-        inner join flow.user u on u.id = pd.closer_user_id
-        inner join flow.user_position up on up.user_id = u.id
+             pd.final_design_complete_date,
+             --requirements for self_gen as defined by BR
+                    -- -- Lead Source = Closer Gen || Events - Closer Gen
+                    -- -- Referral = True
+                    -- -- Referred By = False
+                    -- -- Referral Generation Representative = False
+             case when pd.source in (523, 530) AND
+                       ccfv_referral.boolean_value is true AND
+                       coalesce(ccfv_referred_by.text_value, '') = '' AND
+                       ccfv_ref_gen_rep.int_value is null
+                       then true else false end as self_gen
+      from brs.project_details pd
+        left join flow.contact_custom_field_value ccfv_referral on ccfv_referral.contact_id = pd.contact_id and ccfv_referral.custom_field_group_assignment_id = 19106 -- Referral field - boolean
+        left join flow.contact_custom_field_value ccfv_referred_by on ccfv_referred_by.contact_id = pd.contact_id and ccfv_referred_by.custom_field_group_assignment_id = 997 -- Referral by field - text
+        left join flow.contact_custom_field_value ccfv_ref_gen_rep on ccfv_ref_gen_rep.contact_id = pd.contact_id and ccfv_ref_gen_rep.custom_field_group_assignment_id = 19178 -- Referral Generation Representative field - system list (int)
       where pd.final_design_complete_date is not null
         and pd.final_design_complete_date::date between v_start_date and v_end_date
         and ((pd.cancelled_date is null) or (pd.cancelled_date is not null and pd.cancelled_date::date > v_end_date))
-        and (p.company_project_status_type_id is null or p.company_project_status_type_id != 3)
-        and u.id = p_user_id
+        and (pd.company_project_status_type_id is null or pd.company_project_status_type_id != 3)
+        and pd.closer_user_id = p_user_id
         and pd.company_id = 3
-      group by customer_name, p.id, pd.source_name, owner_name, pd.system_size, pd.final_design_complete_date
+      group by customer_name, pd.project_id, pd.source_name, owner_name, pd.system_size,
+               pd.final_design_complete_date, pd.source, ccfv_referral.boolean_value,
+               ccfv_referred_by.text_value, ccfv_ref_gen_rep.int_value
       order by customer_name
     ) as sub_rows;
 
