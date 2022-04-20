ac9a6c00e952634aa886a70a50b677fd694da252
Randa Nunn <randanunn@gmail.com>
2022-04-20 13:35:41 -0600

    setter dashboard date options for today and yesterday

--- src/main/resources/db/migration/procedures/brs/R__get_setter_office_ranking.sql
+++ src/main/resources/db/migration/procedures/brs/R__get_setter_office_ranking.sql
@@ -56,7 +56,8 @@ BEGIN
                                      and pd2.first_appointment is not null
                                      then pd2.first_appointment
                                  else pd2.closer_appointment_start
-                                 end) at time zone 'UTC') at time zone 'US/Mountain') :: date between ((now() at time zone 'US/Mountain')::date - p_days) and ((now() at time zone 'US/Mountain')::date)
+--                                  end) at time zone 'UTC') at time zone 'US/Mountain') :: date between ((now() at time zone 'US/Mountain')::date - p_days) and ((now() at time zone 'US/Mountain')::date)
+                                    end) at time zone 'UTC') at time zone 'US/Mountain') :: date between ((now() at time zone 'US/Mountain')::date) - p_days and (case when p_days = 1 then ((now() at time zone 'US/Mountain')::date) - p_days else (now() at time zone 'US/Mountain')::date end)
                      and ((pd2.cancelled_date is null) or (pd2.cancelled_date is not null and pd2.cancelled_date > (now() at time zone 'US/Mountain')::date))
                      and o2.id = o.id
                      and pd2.company_id = 3
@@ -83,7 +84,8 @@ BEGIN
                                 and pd.first_appointment_missed is not null
                                 then pd.first_appointment_missed
                             else pd.closer_appointment_start
-                            end) at time zone 'UTC') at time zone 'US/Mountain') :: date between ((now() at time zone 'US/Mountain')::date - p_days) and ((now() at time zone 'US/Mountain')::date)
+--                             end) at time zone 'UTC') at time zone 'US/Mountain') :: date between ((now() at time zone 'US/Mountain')::date - p_days) and ((now() at time zone 'US/Mountain')::date)
+                               end) at time zone 'UTC') at time zone 'US/Mountain') :: date between ((now() at time zone 'US/Mountain')::date) - p_days and (case when p_days = 1 then ((now() at time zone 'US/Mountain')::date) - p_days else (now() at time zone 'US/Mountain')::date end)
                 and (case when pd.first_appointment_pitched is not null
                               then pd.first_appointment_pitched_id in (2,3,1139,1140) --(Pitched, Missed, Pitched - Proposal Not Shown, Pitched - Proposal Shown)
                           when pd.first_appointment_pitched is null
