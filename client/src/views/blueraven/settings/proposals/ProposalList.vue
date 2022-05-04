3d6a2f82f0dd942d0d4c96325697db70aac3b023
Kaleb Scholes <kscholes@gmail.com>
2022-05-04 17:00:38 -0600

    Add link to proposal designer

--- client/src/views/blueraven/settings/proposals/ProposalList.vue
+++ client/src/views/blueraven/settings/proposals/ProposalList.vue
@@ -3,10 +3,14 @@
     <v-toolbar flat class="app-toolbar">
       <v-toolbar-title class="app-title">Proposal Versions</v-toolbar-title>
       <v-spacer/>
+      <v-toolbar-items>
+        <v-btn text :to="'/settings/proposalDesigner'">
+          <span>Designer</span>
+        </v-btn>
+      </v-toolbar-items>
       <v-toolbar-items v-if="canCreateVersion">
         <v-btn text color="primaryButton" @click="create">Create New Version</v-btn>
       </v-toolbar-items>
-
     </v-toolbar>
     <v-divider/>
     <v-container>
