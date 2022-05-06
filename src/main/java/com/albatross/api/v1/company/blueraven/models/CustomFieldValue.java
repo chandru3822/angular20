c58fb4387496e4c2a52182598820deb689aee44c
Kaleb Scholes <kscholes@gmail.com>
2022-05-06 12:22:56 -0600

    Bugfixes for proposals + Display project information + Fix to properly display proposal attachments (images) + Fix to show custom field values that reference flow custom field values

--- src/main/java/com/albatross/api/v1/company/blueraven/models/CustomFieldValue.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/models/CustomFieldValue.java
@@ -2,21 +2,28 @@ package com.albatross.api.v1.company.blueraven.models;
 
 import com.albatross.api.v1.flow.model.ListOfValue;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
 import lombok.NoArgsConstructor;
 
 import java.math.BigDecimal;
 import java.sql.Timestamp;
 import java.util.List;
 
-@EqualsAndHashCode
 @Data
 @NoArgsConstructor
 public class CustomFieldValue {
 
-  private Long id, sourceId, customFieldGroupAssignmentId, customFieldGroupId, ancillaryCustomFieldGroupAssignmentId,
-               customFieldId, fieldOrder, listOfValueId, companyDataTypeId, dataTypeId,
-               companySystemListId;
+  private Long id,
+      sourceId,
+      customFieldGroupAssignmentId,
+      customFieldGroupId,
+      ancillaryCustomFieldGroupAssignmentId,
+      customFieldId,
+      fieldOrder,
+      listOfValueId,
+      companyDataTypeId,
+      dataTypeId,
+      companySystemListId,
+      flowCustomFieldId;
   private Boolean valueWasChanged;
 
   // not sure on these types
@@ -27,7 +34,6 @@ public class CustomFieldValue {
 
   private BigDecimal numericValue;
   private Boolean booleanValue, hasListValues, useParentData;
-
   private Timestamp dateValue, timestampValue;
 
   private List<ListOfValue> listOfValues;
