4e6494cdb7d2296cf69c62ccb639036649c75619
jberns003 <54857272+jberns003@users.noreply.github.com>
2022-05-13 15:43:59 -0400

    HOTFIX: Removed recently added GENE params

--- src/main/java/com/albatross/api/v1/company/blueraven/services/GenesysService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/GenesysService.java
@@ -237,8 +237,6 @@ public class GenesysService {
     contactMap.put("postal_code", contact.getPostalCode() != null ? contact.getPostalCode() : "");
     contactMap.put("email", contact.getEmail() != null ? contact.getEmail() : "");
     contactMap.put("date_created", formatter.format(calendar.getTime()));
-    contactMap.put("priority", "true");
-    contactMap.put("doNotQueue", "false");
 
     addTextelParameters(contactMap, false);
 
@@ -350,8 +348,6 @@ public class GenesysService {
     contactMap.put("postal_code", contact.getPostalCode() != null ? contact.getPostalCode() : "");
     contactMap.put("email", contact.getEmail() != null ? contact.getEmail() : "");
     contactMap.put("date_created", formatter.format(calendar.getTime()));
-    contactMap.put("priority", "true");
-    contactMap.put("doNotQueue", "false");
 
     addTextelParameters(contactMap, true);
 
