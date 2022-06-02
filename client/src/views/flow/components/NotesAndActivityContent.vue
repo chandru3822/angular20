eac2a89a1befe16dc4ed8ed60f6d61fc928a0c1c
turekj <turekj@ldschurch.org>
2022-06-02 14:46:55 -0500

    use generic delete dialog

--- client/src/views/flow/components/NotesAndActivityContent.vue
+++ client/src/views/flow/components/NotesAndActivityContent.vue
@@ -170,43 +170,9 @@
                                @click="[item.oldNote = item.note, item.edit = true]">
                     <v-list-item-title>Edit Note</v-list-item-title>
                   </v-list-item>
-                  <v-dialog
-                      v-if="item.createdById === userId || $store.getters.isFullAdmin"
-                      v-model="item.deleteConfirm"
-                      width="500">
-                    <template #activator="{ on }">
-                      <v-list-item v-on="on">
-                        <v-list-item-title>Delete Note</v-list-item-title>
-                      </v-list-item>
-                    </template>
-                    <v-card>
-                      <v-card-title
-                          class="text-h5 grey lighten-2"
-                          primary-title>
-                        Confirm
-                      </v-card-title>
-
-                      <v-card-text class="pt-4">
-                        Deleting this note will remove all comments. Are you sure you want to delete?
-                      </v-card-text>
-
-                      <v-divider></v-divider>
-
-                      <v-card-actions>
-                        <v-spacer></v-spacer>
-                        <v-btn
-                            @click="item.deleteConfirm = false">
-                          No
-                        </v-btn>
-                        <v-btn
-                            color="primary"
-                            text
-                            @click="deleteNote(item, false)">
-                          Yes
-                        </v-btn>
-                      </v-card-actions>
-                    </v-card>
-                  </v-dialog>
+                  <v-list-item v-if="item.createdById === userId || $store.getters.isFullAdmin" @click="startNoteDelete(item, false)">
+                    <v-list-item-title>Delete Note</v-list-item-title>
+                  </v-list-item>
 
                 </v-list>
               </v-menu>
@@ -323,45 +289,9 @@
                                    @click="[cn.oldNote = cn.note, cn.edit = true]">
                         <v-list-item-title>Edit Comment</v-list-item-title>
                       </v-list-item>
-<!--                      todo: replace with delete dialog component-->
-                      <v-dialog
-                          v-if="cn.createdById === userId || $store.getters.isFullAdmin"
-                          v-model="cn.deleteConfirm"
-                          width="500">
-                        <template #activator="{ on }">
-                          <v-list-item v-on="on">
-                            <v-list-item-title>Delete Comment</v-list-item-title>
-                          </v-list-item>
-                        </template>
-                        <v-card>
-                          <v-card-title
-                              class="text-h5 grey lighten-2"
-                              primary-title>
-                            Confirm
-                          </v-card-title>
-
-                          <v-card-text class="pt-4">
-                            Are you sure you want to delete this comment?
-                          </v-card-text>
-
-                          <v-divider></v-divider>
-
-                          <v-card-actions>
-                            <v-spacer></v-spacer>
-                            <v-btn
-                                @click="cn.deleteConfirm = false">
-                              No
-                            </v-btn>
-                            <v-btn
-                                color="primary"
-                                text
-                                @click="deleteNote(cn, true, item)">
-                              Yes
-                            </v-btn>
-                          </v-card-actions>
-                        </v-card>
-                      </v-dialog>
-
+                      <v-list-item v-if="cn.createdById === userId || $store.getters.isFullAdmin" @click="startNoteDelete(cn, true, item)">
+                        <v-list-item-title>Delete Comment</v-list-item-title>
+                      </v-list-item>
                     </v-list>
                   </v-menu>
 
@@ -371,6 +301,11 @@
           </td>
         </template>
       </v-data-table>
+    <ConfirmDeleteDialogImproved
+        :open-confirm-delete-dialog="showDeleteNoteDialog"
+        @confirm-delete="deleteNote"
+        @closeConfirmDeleteDialog="closeNoteDelete"
+    >{{ deleteDialogBody }}</ConfirmDeleteDialogImproved>
   </div>
 </template>
 
@@ -380,10 +315,11 @@ import {AppMutations} from '@/stores/AppStore'
 import Vue2Filters from "vue2-filters"
 import {Mentionable} from 'vue-mention'
 import DatetimePickerInput from "@/components/DatetimePickerInput"
+import ConfirmDeleteDialogImproved from "@/ConfirmDeleteDialogImproved";
 
 export default {
   name: 'NotesAndActivityContent',
-  components: {Mentionable, DatetimePickerInput},
+  components: {ConfirmDeleteDialogImproved, Mentionable, DatetimePickerInput},
   mixins: [Vue2Filters.mixin],
   props: {
     showNotes: Boolean,
@@ -420,7 +356,11 @@ export default {
         {text: null, value: 'icons', show: true, width: '50px'}
       ],
       expanded: [],
-      users: []
+      users: [],
+      showDeleteNoteDialog: false,
+      noteToDelete:{},
+      parentOfNoteToDelete:{},
+      deleteDialogBody: ""
     }
   },
   computed: {
@@ -432,16 +372,31 @@ export default {
     this.getUsers()
   },
   methods: {
+    startNoteDelete(n, isChildNote, item){
+      this.deleteDialogBody = isChildNote ? "Are you sure you want to delete this comment?" : "Deleting this note will remove all comments. Are you sure you want to delete?"
+      this.showDeleteNoteDialog = true
+      this.noteToDelete = n
+      this.parentOfNoteToDelete = item
+
+    },
+    closeNoteDelete(){
+      this.showDeleteNoteDialog = false
+      this.noteToDelete = null
+      this.parentOfNoteToDelete = null
+    },
     hasUnsavedNotes() {
       return this.dirtyNote
     },
-    async deleteNote(n, isChildNote, item) {
+    async deleteNote() {
+      const n = this.noteToDelete
+      const isChildNote = !!this.parentOfNoteToDelete
+
       try {
         // @randa: Probably should create an object type enum on the frontend that mimics the backend?
         await deleteRequest(`/note/${n.id}`)
         n.archived = true
         if (isChildNote) {
-          item.childNotes = item.childNotes.filter(cn => !cn.archived)
+          this.parentOfNoteToDelete.childNotes = this.parentOfNoteToDelete.childNotes.filter(cn => !cn.archived)
         }
         this.snackbar = getSnackbar('SUCCESS', 'Note Deleted')
         this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
@@ -450,6 +405,7 @@ export default {
         this.snackbar = getSnackbar('ERROR', 'Error Deleting Note')
         this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
       }
+      this.closeNoteDelete()
     },
     async saveNote(n) {
       try {
