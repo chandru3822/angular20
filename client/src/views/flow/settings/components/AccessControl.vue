099b44fb18afdfbb68eec09fd892909b71a5fa01
jkburnett <jessica@7oaksgroup.com>
2022-06-23 06:20:16 -0500

    checkboxes

--- client/src/views/flow/settings/components/AccessControl.vue
+++ client/src/views/flow/settings/components/AccessControl.vue
@@ -1,5 +1,6 @@
 <template>
   <div>
+    Chocolate Milk
     <v-data-table
       :headers="headers"
       :items="companyFeatureList"
@@ -25,23 +26,23 @@
       </template>
 
       <template v-slot:header.data-table-select="{ on, props }">
-        <v-simple-checkbox v-bind="props" :ripple="false" v-on="on" v-if="userCanEdit"></v-simple-checkbox>
+                                      <v-simple-checkbox color="primary" v-bind="props" :ripple="false" v-on="on" v-if="userCanEdit"></v-simple-checkbox>
       </template>
 
       <template #item="{ item, index, isSelected, select }">
         <tr :class="{ 'shaded-row': index % 2 }">
           <td class="text-center">
-            <v-simple-checkbox v-if="userCanEdit" :ripple="false" :value="isSelected" @input="select($event)"></v-simple-checkbox>
+            <v-simple-checkbox color="primary" v-if="userCanEdit" :ripple="false" :value="isSelected" @input="select($event)"></v-simple-checkbox>
           </td>
           <td class="text-left">
             {{ item.featureName }}
           </td>
           <td v-for="acl in item.accessControl">
             <div v-if="acl.usedByFeature">
-              <input type="checkbox" :readonly="!userCanEdit"
+              <input type="checkbox" :readonly="!userCanEdit" color="primary"
                      :disabled="!userCanEdit" v-model="acl.enabled" @input="[acl.dirty = true, item.dirty = true, callback(companyFeatureList)]">
 
-              <v-icon class="ml-2 mb-1" small color="activeBlue"
+              <v-icon class="ml-2 mb-1" small color="primary"
                       v-if="secondaryFeatureAccess.length > 0 && secondaryHasAccess(item, acl)">
                 mdi-alpha-p-box-outline
               </v-icon>
@@ -241,5 +242,9 @@ export default {
   }
 }
 </script>
-
+<style lang="scss" scoped>
+input[type=checkbox] {
+  color: var(--v-primary-base);
+}
+</style>
 
