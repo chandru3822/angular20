ebfb74df59aefcb75255ce350cec562e52b12009
Randa Nunn <randanunn@gmail.com>
2022-07-05 11:51:46 -0600

    fix adding contact

--- client/src/views/flow/contacts/Contact.vue
+++ client/src/views/flow/contacts/Contact.vue
@@ -505,6 +505,7 @@ export default {
     }
   },
   async created() {
+    console.log('routed param',this.$route.params.contactId)
     let requests = [this.getContact(), this.getCustomFieldGroups()]
     await Promise.all(requests).then(async () => {
       this.fieldsLoading = false
