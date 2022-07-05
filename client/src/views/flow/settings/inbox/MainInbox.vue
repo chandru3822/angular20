bbf115c7ac77d5277829574fc94deafb2ff4e0d0
jberns003 <54857272+jberns003@users.noreply.github.com>
2022-07-05 13:56:32 -0400

    SMS Inbox - Navigate to page 1 on project reload

--- client/src/views/flow/settings/inbox/MainInbox.vue
+++ client/src/views/flow/settings/inbox/MainInbox.vue
@@ -373,6 +373,7 @@ export default {
       try {
         this.showLoading(true)
         const { page, itemsPerPage } = this.options
+        this.options.page = 1;
 
         let filterData = {
           ownerUserIds: this.selectedOwnerFilters,
