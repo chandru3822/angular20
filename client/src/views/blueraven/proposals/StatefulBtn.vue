472160510a29a4c2ac684f4cef33107e40f4ef1d
Kaleb Scholes <kscholes@gmail.com>
2022-07-25 12:53:28 -0600

    prop-tool: building out UI for Next Steps

---
+++ client/src/views/blueraven/proposals/StatefulBtn.vue
@@ -0,0 +1,22 @@
+<template>
+  <v-btn
+    :color="successful ? 'green' : 'primary'"
+    class="text-capitalize"
+    :dark="!$attrs.disabled"
+    v-bind="$attrs"
+    v-on="$listeners"
+  >
+    <v-icon v-if="successful">mdi-check-circle-outline</v-icon>
+    <slot></slot>
+  </v-btn>
+</template>
+<script>
+
+export default {
+  props: {
+    successful: {
+      type: Boolean
+    }
+  }
+}
+</script>
