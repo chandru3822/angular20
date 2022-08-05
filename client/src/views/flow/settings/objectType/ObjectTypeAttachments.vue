1108457c5be9c0f4f2114cc068a6e1dd838cdef9
Randa Nunn <randanunn@gmail.com>
2022-08-05 10:41:39 -0600

    fix for ux

--- client/src/views/flow/settings/objectType/ObjectTypeAttachments.vue
+++ client/src/views/flow/settings/objectType/ObjectTypeAttachments.vue
@@ -73,43 +73,10 @@
                         <v-icon>edit</v-icon>
                       </v-btn>
                     </router-link>
-                    <v-dialog
-                      v-if="userCanEdit"
-                      v-model="item.deleteConfirm"
-                      width="500">
-                      <template #activator="{ on }">
-                        <v-btn small text v-on="on">
-                          <v-icon>delete</v-icon>
-                        </v-btn>
-                      </template>
-                      <v-card>
-                        <v-card-title
-                          class="text-h5 grey lighten-2"
-                          primary-title>
-                          Confirm
-                        </v-card-title>
-
-                        <v-card-text class="pt-4">
-                          Are you sure you want to delete this attachment type?
-                        </v-card-text>
-
-                        <v-divider></v-divider>
-
-                        <v-card-actions>
-                          <v-spacer></v-spacer>
-                          <v-btn
-                            @click="item.deleteConfirm = false">
-                            No
-                          </v-btn>
-                          <v-btn
-                            color="primaryCustom"
-                            text
-                            @click="[item.archived = true, deleteTypeFromObject(a.id)]">
-                            Yes
-                          </v-btn>
-                        </v-card-actions>
-                      </v-card>
-                    </v-dialog>
+                    <v-btn v-if="userCanEdit" small text color="primary" class="clickable"
+                           @click="attachmentTypeToDelete=item">
+                      <v-icon>delete</v-icon>
+                    </v-btn>
                   </div>
                 </td>
               </tr>
