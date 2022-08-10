595364f562c634759cd2f8c5684b0507a97240a5
Randa Nunn <randanunn@gmail.com>
2022-08-10 10:37:22 -0600

    new column in company dashboard

--- src/main/resources/db/migration/procedures/brs/dashboards/R__rpt_company_dashboard.sql
+++ src/main/resources/db/migration/procedures/brs/dashboards/R__rpt_company_dashboard.sql
@@ -784,7 +784,7 @@ RETURN QUERY select array_to_json(array_agg(row_to_json(funnel_rows)))
                     union
                     select name,
                            true  as show_targets,
-                           false as has_additional_column,
+                           true as has_additional_column,
                            milestone_type_id,
                            display_order,
                            company_count,
