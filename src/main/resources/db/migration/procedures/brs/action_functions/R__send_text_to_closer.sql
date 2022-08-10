06083ecb1d14ae6f7b5f80c8535fca9b10e4545e
Randa Nunn <randanunn@gmail.com>
2022-08-10 10:41:46 -0600

    2 more texts for closers

--- src/main/resources/db/migration/procedures/brs/action_functions/R__send_text_to_closer.sql
+++ src/main/resources/db/migration/procedures/brs/action_functions/R__send_text_to_closer.sql
@@ -127,6 +127,16 @@ BEGIN
           values(v_closer_user_id,
                  concat('Hi ', v_closer_first_name, ', your follow-up is needed from the proposals team on project: ', v_contact_name,', ', p_project_id, '. Access the open "Follow-up" process step on this account to see the needed information.'),
                  (SELECT md5(random()::text || clock_timestamp()::text)::uuid), v_closer_phone_number, now(), 1, p_current_user_id);
+          -- do the message for id 14 = change order sent to customer
+          insert into flow.sms_queue(user_id, message, message_group, to_phone, created, recipient_type_id, message_sent_by_user_id)
+          values(v_closer_user_id,
+                 concat('A change order for ', v_contact_name, ' has been sent to the customer for review/signature'),
+                 (SELECT md5(random()::text || clock_timestamp()::text)::uuid), v_closer_phone_number, now(), 1, p_current_user_id);
+          -- do the message for id 15 = change order created
+          insert into flow.sms_queue(user_id, message, message_group, to_phone, created, recipient_type_id, message_sent_by_user_id)
+          values(v_closer_user_id,
+                 concat('A change order for ', v_contact_name, ' has been created and will be sent out soon'),
+                 (SELECT md5(random()::text || clock_timestamp()::text)::uuid), v_closer_phone_number, now(), 1, p_current_user_id);
         end if;
     end if;
 
