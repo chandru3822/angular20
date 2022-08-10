53fa3f162d717fa2d5f77f725e977dfd29264d79
Randa Nunn <randanunn@gmail.com>
2022-08-10 10:27:03 -0600

    better variable name

--- src/main/resources/db/migration/procedures/brs/requirement_functions/R__closer_active.sql
+++ src/main/resources/db/migration/procedures/brs/requirement_functions/R__closer_active.sql
@@ -3,18 +3,18 @@ CREATE OR REPLACE FUNCTION brs.closer_active(p_project_id integer)
     returns boolean AS
 $BODY$
 declare
-    v_closer_inactive boolean;
+    v_closer_active boolean;
 BEGIN
-    v_closer_inactive = false;
+    v_closer_active = false;
 
-    select true into v_closer_inactive
+    select true into v_closer_active
     from brs.project_details pd
            inner join flow.company_user_status cus on pd.closer_user_id = cus.user_id
            inner join flow.user_status_type ust on cus.user_status_type_id = ust.id
     where pd.project_id = p_project_id
       and ust.has_access is true;
 
-    return case when v_closer_inactive is null then false else v_closer_inactive end;
+    return case when v_closer_active is null then false else v_closer_active end;
 
 END
 $BODY$
