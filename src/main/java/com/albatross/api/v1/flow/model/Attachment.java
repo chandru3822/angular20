7cf2fab137169da50a395e0790756695c5004583
Randa Nunn <randanunn@gmail.com>
2022-08-10 14:26:52 -0600

    change fn a bit

--- src/main/java/com/albatross/api/v1/flow/model/Attachment.java
+++ src/main/java/com/albatross/api/v1/flow/model/Attachment.java
@@ -9,7 +9,7 @@ import java.util.UUID;
 public class Attachment {
     private Long id, size, sourceId, attachmentTypeId, companyId, projectProcessStepId, projectProcessStepEventId;
     private String filename, fileExtension, contentType, s3Key, url, attachmentType,
-      presignedUrl, uploadedBy, processStepName, publicUrl, originLocation, displayName;
+      presignedUrl, uploadedBy, processStepName, publicUrl, originLocation, originPath, displayName;
     private Date dateCreated, dateModified;
     private UUID uuid;
     private Boolean archived, show, linked;
--- src/main/resources/db/migration/procedures/flow/R__get_attachment_origin.sql
+++ src/main/resources/db/migration/procedures/flow/R__get_attachment_origin.sql
@@ -1,62 +1,70 @@
--- DROP FUNCTION IF EXISTS flow.get_attachment_origin(integer,);
+-- DROP FUNCTION IF EXISTS flow.get_attachment_origin(integer);
 CREATE OR REPLACE FUNCTION flow.get_attachment_origin(p_attachment_id INTEGER)
 
-RETURNS text AS
+  RETURNS TABLE(origin_location text, origin_path text) AS
 
 $BODY$
-DECLARE
-  v_origin text;
 BEGIN
 
-  with origins as (
-    select 'Project' as origin
-    from flow.project_attachment
-    where attachment_id = p_attachment_id
-      and linked is false
-      and archived is false
-    union
-    select concat(ps.process_step_name, ' ', ppsa.project_process_step_id)::text as origin
-    from flow.project_process_step_attachment ppsa
-      inner join flow.project_process_step pps on ppsa.project_process_step_id = pps.id
-      inner join flow.process_step ps on pps.process_step_id = ps.id
-    where ppsa.attachment_id = p_attachment_id
-      and ppsa.linked is false
-      and ppsa.archived is false
-    union
-    select concat(e.event_name,' ',ppse.id)::text as origin
-    from flow.project_process_step_event_attachment ppsea
-       inner join flow.project_process_step_event ppse on ppse.id = ppsea.project_process_step_event_id
-       inner join flow.process_step_event pse on ppse.process_step_event_id = pse.id
-       inner join flow.event e on pse.event_id = e.id
-    where ppsea.attachment_id = p_attachment_id
-      and ppsea.linked is false
-      and ppsea.archived is false
-    union
-    select 'Contact' as origin
-    from flow.contact_attachment
-    where attachment_id = p_attachment_id
-      and linked is false
-      and archived is false
-    union
-    select 'User' as origin
-    from flow.user_attachment
-    where attachment_id = p_attachment_id
-      and linked is false
-      and archived is false
-    union
-    select 'Organization' as origin
-    from flow.org_attachment
-    where attachment_id = p_attachment_id
-      and linked is false
-      and archived is false
+  return query
+    with origins as (
+      select 'Project' as origin,
+             concat('/project/', pa.project_id) as origin_path_url
+      from flow.project_attachment pa
+      where pa.attachment_id = p_attachment_id
+        and pa.linked is false
+        and pa.archived is false
+      union
+      select concat(ps.process_step_name, ' ', ppsa.project_process_step_id)::text as origin,
+             concat('/project/', pps.project_id, '/processStep/', pps.id) as origin_path_url
+      from flow.project_process_step_attachment ppsa
+             inner join flow.project_process_step pps on ppsa.project_process_step_id = pps.id
+             inner join flow.process_step ps on pps.process_step_id = ps.id
+      where ppsa.attachment_id = p_attachment_id
+        and ppsa.linked is false
+        and ppsa.archived is false
+      union
+      select concat(e.event_name,' ',ppse.id)::text as origin,
+             concat('/project/', pps.project_id, '/processStep/', pps.id, '/event/', ppse.id) as origin_path_url
+      from flow.project_process_step_event_attachment ppsea
+             inner join flow.project_process_step_event ppse on ppse.id = ppsea.project_process_step_event_id
+             inner join flow.project_process_step pps on ppse.project_process_step_id = pps.id
+             inner join flow.process_step_event pse on ppse.process_step_event_id = pse.id
+             inner join flow.event e on pse.event_id = e.id
+      where ppsea.attachment_id = p_attachment_id
+        and ppsea.linked is false
+        and ppsea.archived is false
+      union
+      select 'Contact' as origin,
+             concat('/contact/', contact_id) as origin_path_url
+      from flow.contact_attachment
+      where attachment_id = p_attachment_id
+        and linked is false
+        and archived is false
+      union
+      select 'User' as origin,
+             concat('/user/', user_id) as origin_path_url
+      from flow.user_attachment
+      where attachment_id = p_attachment_id
+        and linked is false
+        and archived is false
+      union
+      select 'Organization' as origin,
+             concat('/org/', org_id) as origin_path_url
+      from flow.org_attachment
+      where attachment_id = p_attachment_id
+        and linked is false
+        and archived is false
     )
-    select origin::text into v_origin
-      from origins;
+    select origin::text,
+           origin_path_url::text
+    from origins;
 
-  return v_origin;
+
 
 
 END;
 $BODY$
   LANGUAGE plpgsql VOLATILE
-                   COST 100;
+                   COST 100
+                   ROWS 1000;
