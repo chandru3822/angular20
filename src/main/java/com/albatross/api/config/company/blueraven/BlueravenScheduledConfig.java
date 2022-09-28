02cb445a2d371f19d1f3fb893a91c35487119ba9
Scott Humes <scotthumes@gmail.com>
2022-09-28 16:27:20 -0600

    Marketo: Cron testing

--- src/main/java/com/albatross/api/config/company/blueraven/BlueravenScheduledConfig.java
+++ src/main/java/com/albatross/api/config/company/blueraven/BlueravenScheduledConfig.java
@@ -68,7 +68,7 @@ public class BlueravenScheduledConfig implements SchedulingConfigurer {
   }
 
   // daily at 1:15 mountain time
-   @Scheduled(cron = "0 30 * * * *", zone = "America/Denver")
+   @Scheduled(cron = "0 35 * * * *", zone = "America/Denver")
   public void pushProjectsToMarketo() {
       if (pushProjectsToMarketo) {
           log.info("*** CRON: start pushing projects to Marketo ***");
