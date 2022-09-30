d5c8ac0b98072bb59df72114ca30284a4b41893b
Randa Nunn <randanunn@gmail.com>
2022-09-30 11:37:49 -0600

    fix work queues

--- src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
+++ src/main/java/com/albatross/api/v1/flow/services/SmartlistService.java
@@ -899,7 +899,11 @@ public class SmartlistService {
         location = f.getReferenceColumn();
       } else {
         if (smartlist.getObjectTypeId() == 4) {
-          location = String.format("%s.%s", f.getReferenceTable(), f.getReferenceColumn());
+          if(null != timezone && f.getDataTypeId() == 2) {
+            location = String.format("((%s.%s at time zone \'UTC\') at time zone \'%s\')", f.getReferenceTable(), f.getReferenceColumn(), timezone);
+          } else {
+            location = String.format("%s.%s", f.getReferenceTable(), f.getReferenceColumn());
+          }
         } else {
           if (f.getCustomFieldGroupAssignmentId() != null || f.getProcessStepId() != null || (f.getJoinTable() != null && f.getJoinColumn() != null)) {
             String table;
@@ -926,12 +930,7 @@ public class SmartlistService {
       } else if (f.getDataTypeId() == 1) {
         query.append(String.format("  to_char(%s, 'YYYY-MM-DD') as \"%s\", ", location, f.getName()));
       } else if(f.getDataTypeId() == 2) {
-        if (timezone != null) {
-//          query.append(String.format(" to_char(%s.%s at time zone 'UTC' at time zone '%s', 'MM/DD/YYYY HH:MI am') as \"%s\", ", f.getReferenceTable(), f.getReferenceColumn(), timezone, f.getName()));
-          query.append(String.format("  to_char(%s at time zone 'UTC' at time zone '%s', 'YYYY-MM-DD HH:MI am') as \"%s\", ", location, timezone, f.getName()));
-        } else {
-          query.append(String.format("  to_char(%s, 'YYYY-MM-DD HH:MI am') as \"%s\", ", location, f.getName()));
-        }
+        query.append(String.format("  to_char(%s, 'YYYY-MM-DD HH:MI am') as \"%s\", ", location, f.getName()));
       } else if (f.getDataTypeId() == 7 && f.getSmartlistSystemListId() == null) {
           query.append(String.format("  (select array_to_string(array(select \"name\" from flow.list_of_value where id = any(%s)), ',')) as \"%s\", ", location, f.getName()));
       } else if (f.getDataTypeId() == 9) {
