375a936107df0939c0984a17880ecf6c18acff2b
Scott Humes <scotthumes@gmail.com>
2022-10-06 13:30:32 -0600

    Marketo: Move substantial completion date to project_details

--- src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/BrsProcessStepActionFunctionService.java
@@ -430,11 +430,9 @@ public class BrsProcessStepActionFunctionService {
                 }
 
                 //substantialCompletionDate
-                final String substantialRawValue = paramValues.get(3).getDynamicValue();
-                if (substantialRawValue != null && !substantialRawValue.isBlank()) {
-                    final Long substantialCompletionDateCfgaId = Long.parseLong(substantialRawValue);
-                    results = sqlCache.get("marketo.getPpsFieldValueByCfgaId", Map.of("cfgaId", substantialCompletionDateCfgaId, "projectId", projectId), new ColumnMapRowMapper());
-                    results.ifPresent(r -> lead.put("substantialCompletionDate", r.get("dateValue").toString()));
+                final boolean updateSubstantialCompletionDate = Boolean.parseBoolean(paramValues.get(3).getDynamicValue());
+                if (updateSubstantialCompletionDate) {
+                    lead.put("substantialCompletionDate", project.getSubstantialCompletionDate());
                 }
 
                 //inspectionStartTime
