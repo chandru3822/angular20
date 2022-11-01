eaf334921e84a7dacf9144d4cf2b029daca55722
Randa Nunn <randanunn@gmail.com>
2022-11-01 10:00:37 -0600

    fix perms

--- client/src/router.js
+++ client/src/router.js
@@ -650,7 +650,7 @@ const router = new Router({
               path: 'postalCode/:id',
               meta: {title: 'Albatross - Settings'},
               component: () => {
-                if (store.getters.userHasFeature('SETTINGS')) {
+                if (store.getters.userHasFeature('ROUND_ROBIN')) {
                   return import (/* webpackChunkName: "postalCodes" */ './views/flow/settings/postalCodeZones/PostalCode.vue')
                 } else {
                   return accessDenied()
