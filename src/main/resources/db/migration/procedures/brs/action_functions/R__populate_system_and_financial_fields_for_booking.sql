d0760698399369d9ec51a68434b5bd133b1260d5
Randa Nunn <randanunn@gmail.com>
2022-11-29 15:39:29 -0700

    handle empty string for number field in prop log

--- src/main/resources/db/migration/procedures/brs/action_functions/R__populate_system_and_financial_fields_for_booking.sql
+++ src/main/resources/db/migration/procedures/brs/action_functions/R__populate_system_and_financial_fields_for_booking.sql
@@ -47,7 +47,8 @@ BEGIN
                                             where cf.id = 11786
                                               and cfga.archived is false and cf.archived is false and cfg.archived is false
                                               and cf.company_id = v_company_id
-                                              and cfg.process_step_id = p_process_step_id), plh.nineteen_plus_payments_all_incentives::numeric,
+                                              and cfg.process_step_id = p_process_step_id),
+                                                (case when plh.nineteen_plus_payments_all_incentives = '' then null else plh.nineteen_plus_payments_all_incentives end)::numeric,
                                             (select cfga.id
                                             from flow.custom_field cf
                                                    inner join flow.custom_field_group_assignment cfga on cfga.custom_field_id = cf.id
