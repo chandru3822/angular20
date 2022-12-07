03ac6ac8c4e6052f91d90e569136fa330362a524
Scott Humes <scotthumes@gmail.com>
2022-12-07 11:31:04 -0700

    Don't use time/timezones when saving and viewing all-day appointments

--- client/src/views/flow/settings/availability/Appointments.vue
+++ client/src/views/flow/settings/availability/Appointments.vue
@@ -175,7 +175,7 @@
 
           <template #item="{ item, index }">
             <tr class="clickable" :class="{'shaded-row': index % 2}">
-              <td class="text-left">{{item.startTime | formatDate(item.allDay ? 'timestampAsDate' : 'timestamp')}} - {{item.endTime | formatDate(item.allDay ? 'timestampAsDate' : 'timestamp')}}</td>
+              <td class="text-left">{{item.startTime | formatDate(item.allDay ? 'date' : 'timestamp')}} - {{item.endTime | formatDate(item.allDay ? 'date' : 'timestamp')}}</td>
               <td class="text-left">{{item.title}}</td>
               <td><input type="checkbox" :disabled="true" v-model="item.allDay"></td>
               <td class="text-left">
@@ -213,12 +213,13 @@
   import RRule from '@/components/RRule.vue'
   import DatetimePickerInput from '@/components/DatetimePickerInput.vue'
   import { handleHidingGlobalLoader, deleteRequest, getRequestWithParams, postRequest, getSnackbar} from '@/helpers/helpers'
-  import orderBy from "lodash.orderby"
+  import orderBy from 'lodash.orderby'
   import moment from 'moment-timezone'
-  import constants from "@/helpers/constants"
-  import MultiOptionDialog from "@/components/MultiOptionDialog";
+  import constants from '@/helpers/constants'
+  import MultiOptionDialog from '@/components/MultiOptionDialog'
+  import { DateTime } from 'luxon'
 
-  const { VITE_ENV } =  import.meta.env
+  const { VITE_ENV } = import.meta.env
 
   export default {
     name: 'Appointments',
@@ -356,9 +357,13 @@
           this.saveErrorMsg = '* Appointment End must be after Appointment Start'
         } else {
           if(appt.repeat) {
-            //if it is a repeating appt, then save the current users timezone and offset (required for adjusting DST later)
-            appt.originTimezone = this.timezone
-            appt.originTimezoneOffset = moment.tz(moment.utc(appt.startTime), this.timezone).utcOffset() * 60
+            // All day appointments don't have an attached timezone
+            //@TODO: ask randa if it's ok we only set these for non all-day recurring appointments
+            if (!appt.allDay) {
+              //if it is a repeating appt, then save the current users timezone and offset (required for adjusting DST later)
+              appt.originTimezone = this.timezone
+              appt.originTimezoneOffset = moment.tz(moment.utc(appt.startTime), this.timezone).utcOffset() * 60
+            }
           } else {
             //clear out recurrence fields if not repeat when saved
             appt.recurrence = null
@@ -367,8 +372,8 @@
           try {
             this.$store.commit(AppMutations.SET_LOADING, true)
             if( appt.allDay) {
-              appt.startTime = moment(appt.startTime).startOf('day').utc().format()
-              appt.endTime = moment(appt.endTime).endOf('day').utc().format()
+              appt.startTime = DateTime.fromISO(appt.startTime, {zone: 'utc'}).set({hour: 0, minute: 0, second: 0}).toISO()
+              appt.endTime = DateTime.fromISO(appt.endTime, {zone: 'utc'}).set({hour: 0, minute: 0, second: 0}).toISO()
             }
 
             //if the local date and the utc date are different, set the startTimeOffsetDay to true so the server knows what to do
@@ -378,7 +383,7 @@
               orgId: this.orgId,
               userId: this.userId,
               ...appt,
-              startTimeOffsetDay: !localAndUtcSame,
+              startTimeOffsetDay: !localAndUtcSame && !appt.allDay,
             }
             const {data, status} = await postRequest(`/availability/appointment`, params)
             this.addNew = false
