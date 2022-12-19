99cd0a5f8f096255ef2a5e10f4c86f615acd7e07
turekj <turekj@ldschurch.org>
2022-12-19 15:02:32 -0600

    Process Step Event link change to link icon

--- client/src/views/flow/settings/processStep/ProcessStepEvents.vue
+++ client/src/views/flow/settings/processStep/ProcessStepEvents.vue
@@ -67,7 +67,7 @@
                       <v-icon>drag_handle</v-icon>
                     </v-btn>
                   </td>
-                  <td class="text-left"><a :href="`/settings/event/${item.eventId}/components`">{{item.eventName}}</a></td>
+                  <td class="text-left">{{item.eventName}} <v-icon color="primary" @click="goToPath(`/settings/event/${item.eventId}/components`)">mdi-link</v-icon></td>
                   <td class="text-left">{{item.initialEventStatusType}}</td>
                   <td>
                     <div style="display: flex; justify-content: flex-end">
@@ -264,6 +264,9 @@ export default {
     closeDeleteDialog(){
       this.showDeleteDialog = false
       this.itemToDelete = null
+    },
+    goToPath(path) {
+      this.$router.push({ path: `${path}` })
     }
   }
 
