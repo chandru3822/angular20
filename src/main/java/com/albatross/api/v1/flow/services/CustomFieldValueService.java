03ce79cefc04eb10467bb28fab1f8c61ecfb6145
Randa Nunn <randanunn@gmail.com>
2022-12-21 11:42:51 -0700

    fix some bugs

--- src/main/java/com/albatross/api/v1/flow/services/CustomFieldValueService.java
+++ src/main/java/com/albatross/api/v1/flow/services/CustomFieldValueService.java
@@ -170,15 +170,15 @@ public class CustomFieldValueService {
       }
 
       // this allows us to pass project_id and user_id to custom sql queries
-      if(objectType.equals("project")) {
+      if(objectType.textValue().equals("project")) {
         handleCustomListOfValue(fieldGroups, id, user.getId(), companyId, null);
-      } else if (objectType.equals("process_step")) {
+      } else if (objectType.textValue().equals("process_step")) {
         Long projectId = projectService.getProjectIdByProjectProcessStepId(id);
         handleCustomListOfValue(fieldGroups, projectId, user.getId(), companyId, id);
-      } else if (objectType.equals("event")) {
+      } else if (objectType.textValue().equals("event")) {
         Long projectId = projectService.getProjectIdByProjectProcessStepEventId(id);
         handleCustomListOfValue(fieldGroups, projectId, user.getId(), companyId, null);
-      } else if(!objectType.equals("attachment_type")) {
+      } else if(!objectType.textValue().equals("attachment_type")) {
         //dont do this for attachment types. it has already been handled and this code all sucks ass.
         handleCustomListOfValue(fieldGroups, companyId);
       }
