edc3625f2ae290f0dd84f7fd3546286c3be9e9ea
alexace95 <alex@7oaksgroup.com>
2023-01-05 14:31:45 -0500

    Changing 'Database' to 'Databases' in a flywayfile that will work the first time

---
+++ src/main/resources/db/migration/V1_370__database_feature_name_change.sql
@@ -0,0 +1,2 @@
+update flow.company_feature set feature_name = 'Databases' where feature_name = 'Database';
+update flow.feature set feature_name = 'Databases', feature_code = 'DATABASES' where feature_name = 'Database';
