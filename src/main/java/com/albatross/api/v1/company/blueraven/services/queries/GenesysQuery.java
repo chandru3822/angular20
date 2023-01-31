34ac990ce45103414d827f685a73f28d349ceaae
jberns003 <54857272+jberns003@users.noreply.github.com>
2023-01-31 16:38:24 -0500

    HOTFIX: Add offset to process contacts Day1 function

--- src/main/java/com/albatross/api/v1/company/blueraven/services/queries/GenesysQuery.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/queries/GenesysQuery.java
@@ -369,7 +369,7 @@ public class GenesysQuery {
 
   //language=PostgreSQL
   public final static String getContactIdProcessDay1 = """
-    select contact_id, int_value as id from flow.contact_custom_field_value where custom_field_group_assignment_id=20977 and int_value in (1, 2, 3, 9 ,10)
+    select contact_id as id from flow.contact_custom_field_value where custom_field_group_assignment_id=20977 and int_value in (1, 2, 3, 9 ,10)
                                                                    and contact_id in (
             select id
             from flow.contact
