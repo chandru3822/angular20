fec5de72ebe8b53514c4fe2d96e6fd73bca24d82
turekj <turekj@ldschurch.org>
2023-02-06 14:00:00 -0600

    adjust router container height when displaying an extra banner for masquerading and/or UAT

--- client/src/App.vue
+++ client/src/App.vue
@@ -28,7 +28,7 @@
     </v-toolbar>
     <AppNav v-if="!noNavRoutes.includes($route.name) && !hideHeader" />
     <v-main>
-      <v-container class="router-container">
+      <v-container class="router-container" :class="{'extra-banner': inUAT || userIsMasquerading, 'two-extra-banners': inUAT && userIsMasquerading}">
         <Spinner v-if="$store.state.app.loading" :spinnerColor="'primary'" :size="100"></Spinner>
         <router-view class="router-view" />
       </v-container>
@@ -43,6 +43,7 @@ import { AppMutations } from '@/stores/AppStore'
 import AppNav from '@/components/AppNav'
 import Snackbar from '@/components/Snackbar'
 import Spinner from '@/components/Spinner'
+const { VITE_ENV } =  import.meta.env
 
 export default {
   name: 'App',
@@ -55,6 +56,8 @@ export default {
     return {
       hideHeader: this.$store.state.user.hideHeader || false,
       hideMobileBanner: this.$store.state.user.hideMobileBanner || false,
+      userIsMasquerading: this.$store.state.user?.details?.masqueradingUserId != null,
+      inUAT: VITE_ENV === 'uat',
       noNavRoutes: ['login', 'forgotPassword', 'forgotPasswordReset', 'resetPassword', 'siteUnderMaintenance'],
       showMobileBanner: false,
       dismissMobileToolbar: false
