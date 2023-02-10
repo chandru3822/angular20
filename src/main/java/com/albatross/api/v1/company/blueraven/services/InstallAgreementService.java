8e0bf36e0c50afd2c4ff14f18cc6cbe5770fd872
jberns003 <54857272+jberns003@users.noreply.github.com>
2023-02-10 18:04:05 -0500

    HOTFIX: 25 year 3.99 interest rate update

--- src/main/java/com/albatross/api/v1/company/blueraven/services/InstallAgreementService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/InstallAgreementService.java
@@ -440,6 +440,8 @@ public class InstallAgreementService {
         financeOption = "349";
       } else if (interestRate.equals("0.0398")) {
         financeOption = "flexpay398";
+      } else if (interestRate.equals("0.0399")) {
+        financeOption = "399";
       } else if (interestRate.equals("0.0498")) {
         financeOption = "flexpay498";
       } else if (interestRate.equals("0.0598")) {
