67ddf327b85ac46119cfd65ade82b5348a2dbb0c
turekj <turekj@ldschurch.org>
2023-03-09 12:44:48 -0600

    improve EmailSettings table mobile
    (cherry picked from commit 449f7cad3dca69bbab59c43191e49923f53b3b78)

--- client/src/views/flow/settings/defaults/EmailSettings.vue
+++ client/src/views/flow/settings/defaults/EmailSettings.vue
@@ -40,7 +40,7 @@
             </v-text-field>
           </v-col>
           <v-col class="text-center">
-            <v-checkbox v-model="newEmail.checked" :value="newEmail.isDefault" label="" width="24px"></v-checkbox>
+            <v-checkbox v-model="newEmail.checked" :value="newEmail.isDefault" label="Default" width="24px"></v-checkbox>
           </v-col>
         </v-row>
       </v-form>
@@ -48,13 +48,10 @@
             :headers="headers"
             :items="emailValues"
             :items-per-page="-1"
-            :mobile-breakpoint="0"
             hide-default-footer
-            class="elevation-1 fix-column-width-bug square-card"
+            class="elevation-1 square-card table-striped"
         >
-          <template #item="{ item, index }">
-            <tr :class="{'shaded-row': index % 2}">
-              <td class="text-left">
+          <template #item.name="{ item, index }">
                 <v-text-field text
                               type="text"
                               :ref="`senderName-edit-${item.id}`"
@@ -65,8 +62,8 @@
                 <div v-else>
                   {{item.senderName}}
                 </div>
-              </td>
-              <td class="text-left">
+          </template>
+              <template #item.value="{item, index}" class="text-left">
                 <v-text-field text
                               type="text"
                               :ref="`emailAddress-edit-${item.id}`"
@@ -77,25 +74,23 @@
                 <div v-else style="overflow-wrap: anywhere">
                   {{ item.emailAddress }}
                 </div>
-              </td>
-              <td class="text-center">
+              </template>
+              <template #item.isDefault="{item, index}" class="text-center">
                 <v-icon v-show="item.isDefault" v-if="index !== editIndex" class="centered">mdi-check</v-icon>
-                <v-checkbox v-if="index == editIndex" v-model="item.checked" :value="item.isDefault" :disabled="item.isDefault" label=""></v-checkbox>
-              </td>
-              <td>
-                <v-btn icon color="primary" @click="editIndex = index" v-if="index !== editIndex">
+                <v-checkbox v-if="index == editIndex" v-model="item.checked" :value="item.isDefault" :disabled="item.isDefault" label="Default"></v-checkbox>
+              </template>
+              <template #item.icons="{item, index}">
+                <v-btn icon :large="$vuetify.breakpoint.smAndDown" color="primary" @click="editIndex = index" v-if="index !== editIndex">
                   <v-icon>edit</v-icon>
                 </v-btn>
-                <v-btn small text color="primary" @click="updateEmailAddress(item)" :disabled="!isEditValid(item)" v-if="index === editIndex">
+                <v-btn small icon :large="$vuetify.breakpoint.smAndDown" color="primary" @click="updateEmailAddress(item)" :disabled="!isEditValid(item)" v-if="index === editIndex">
                   <v-icon>save</v-icon>
                 </v-btn>
                 <v-btn small text color="primary" v-if="index === editIndex" @click="clearChanges()">
                   cancel
                 </v-btn>
-                <v-btn :disabled="item.isDefault" small icon color="primary" @click="emailToDelete=item"><v-icon>delete</v-icon></v-btn>
-              </td>
-            </tr>
-          </template>
+                <v-btn :disabled="item.isDefault" small icon :large="$vuetify.breakpoint.smAndDown" color="primary" @click="emailToDelete=item"><v-icon>delete</v-icon></v-btn>
+              </template>
         </v-data-table>
       </v-col>
     <ConfirmationDialog :open-dialog="!!emailToDelete" @confirm="deleteEmailAddress" @close-dialog="emailToDelete=null">
@@ -135,7 +130,7 @@ export default {
       headers: [
         {text: 'Sender Name', value: 'name', show: true},
         {text: 'Email Address', value: 'value', show: true},
-        {text: 'Default', value: 'isDefault', show: true, width:'90px', align:'center'},
+        {text: 'Default', value: 'isDefault', show: true, align:'center'},
         {text: null, value: 'icons', show: true, sortable: false}
       ],
       emailValues: [],
