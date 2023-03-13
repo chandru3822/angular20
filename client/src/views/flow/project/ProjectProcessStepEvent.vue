a2b9c7fbf9e4c4b21f133a64d277bc4d1cffa3de
Scott Humes <scotthumes@gmail.com>
2023-03-13 10:55:35 -0600

    HOTFIX: Undo last hotfix

--- client/src/views/flow/project/ProjectProcessStepEvent.vue
+++ client/src/views/flow/project/ProjectProcessStepEvent.vue
@@ -781,7 +781,7 @@ export default {
       let hidden = getEventDefaultFieldHidden(this.$store, wlp, hiddenFieldValue)
       // if events admin then they can edit any event fields, otherwise idk???
       // if not readonly and the user can manage then ignore event status check
-      return ((!this.userIsAdmin && !this.userCanManage && !hidden) && this?.selectedEvent?.processStepStatusTypeId === 1)
+      return ((!this.userIsAdmin && !this.userCanManage && !hidden) && this?.selectedEvent?.processStepStatusTypeId !== 1)
         || hidden
     },
     populateDirtyCfvs(field) {
