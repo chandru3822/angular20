43c0c89ff51e27b013cd2015bd8ea7ef13f7aa2a
alexace95 <alex@7oaksgroup.com>
2023-03-27 13:50:45 -0400

    Making the 'this quarter' code quicker & more readable

--- client/src/views/blueraven/companyDashboard/CompanyDashboard.vue
+++ client/src/views/blueraven/companyDashboard/CompanyDashboard.vue
@@ -227,6 +227,12 @@
       endOfPeriod () {
          return moment(this.momentStartOfPeriod).clone().add(3 + this.additionalEndWeek, 'weeks').endOf('isoWeek').format('YYYY-MM-DD')
       },
+      startOfQuarter(){
+        return moment().startOf('isoWeek').isoWeek(Math.floor((moment().isoWeek()-1) / 13)*13 + 1).format('YYYY-MM-DD')
+      },
+      endOfQuarter(){
+        return moment().endOf('isoWeek').isoWeek(Math.floor((moment().isoWeek()-1) / 13)*13 + 13).format('YYYY-MM-DD')
+      },
       startOfWeek () {
         return moment().startOf('isoWeek').format('YYYY-MM-DD')
       },
@@ -332,24 +338,8 @@
             this.endDate = moment().format('YYYY-MM-DD')
             break
           case 'This Quarter':
-            //This is January 1st
-            let tempDate = new Date(moment().startOf('year'));
-            let weeksPerQuarter = 13;
-
-            //This sets tempDate to the first Sunday of the year
-            while(tempDate.getDay() != 0){
-              tempDate.setDate(tempDate.getDate());
-            }
-
-            //This finds the last day of the current quarter
-            while(tempDate < new Date()){
-              tempDate.setDate(tempDate.getDate() + (7*weeksPerQuarter));
-            }
-            this.endDate = moment(tempDate).format('YYYY-MM-DD');
-
-            //This sets the start date of the current quarter
-            tempDate.setDate(tempDate.getDate() - (7*weeksPerQuarter) + 1);
-            this.startDate = moment(tempDate).format('YYYY-MM-DD');
+            this.startDate = this.startOfQuarter
+            this.endDate = this.endOfQuarter
             break;
           case 'All Time':
             this.startDate = moment('2000-01-01').format('YYYY-MM-DD')
