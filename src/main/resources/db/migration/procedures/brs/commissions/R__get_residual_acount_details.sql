49667a868bc4bc7b661e353196a8b1756f60c681
Scott Keller <kellersk88@gmail.com>
2023-03-28 12:47:33 -0600

    residuals

--- src/main/resources/db/migration/procedures/brs/commissions/R__get_residual_acount_details.sql
+++ src/main/resources/db/migration/procedures/brs/commissions/R__get_residual_acount_details.sql
@@ -60,8 +60,9 @@ begin
            foo1.clawback,
            foo1.adjustment_override,
            case
-             when foo1.residual_earned is true and foo1.user_id = any(foo1.selected_user_ids) then
-                 coalesce(foo1.potential_residual,0) - coalesce(foo1.clawback,0) + coalesce(foo1.adjustment_override,0)
+             when foo1.residual_earned is true and foo1.user_id = any(foo1.selected_user_ids) and
+                  coalesce(foo1.potential_residual,0) + coalesce(foo1.adjustment_override,0) - coalesce(foo1.clawback,0) > 0 then
+                 coalesce(foo1.potential_residual,0) + coalesce(foo1.adjustment_override,0) - coalesce(foo1.clawback,0)
              when foo1.residual_earned is false and foo1.user_id = any(foo1.selected_user_ids) and coalesce(foo1.adjustment_override,0) > coalesce(foo1.clawback,0)  then
                  coalesce(foo1.adjustment_override,0) - coalesce(foo1.clawback,0)
              else 0.00 end as total
