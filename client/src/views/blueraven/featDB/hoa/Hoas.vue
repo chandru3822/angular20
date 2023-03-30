9cd473fc71d174f80ff36d2da9fb4a74b08fa805
turekj <turekj@ldschurch.org>
2023-03-30 12:47:06 -0500

    Check for duplicates when adding new HOAs to the HOA database

--- client/src/views/blueraven/featDB/hoa/Hoas.vue
+++ client/src/views/blueraven/featDB/hoa/Hoas.vue
@@ -131,7 +131,7 @@
           <v-spacer></v-spacer>
           <v-btn color="primary" text @click="close">Cancel</v-btn>
           <v-btn color="primary" raised @click="newHoaDuplicateCheck" class="white--text"
-                 :disabled="!editedItem.name || !editedItem.companyStateId">
+                 :disabled="!editedItem.name?.trim() || !editedItem.companyStateId">
             {{ btnTxt }}
           </v-btn>
         </v-card-actions>
@@ -148,6 +148,7 @@
           <div class="body-medium"><span class="label-medium">Management Company:</span>
             <div>{{duplicateHoaMatch.managementCompany}}</div>
           </div>
+          <div class="body-medium"><span class="label-medium">Date Created:</span> {{duplicateHoaMatch.dateCreated | formatDate('date')}}</div>
         </v-col>
         <v-col v-if="editedItem">
           <div class="label-large">New Data</div>
@@ -344,18 +345,33 @@ export default {
     },
 
     newHoaDuplicateCheck() {
+      this.editedItem.managementCompany = !!this.editedItem.managementCompanyId ? this.managementCompanies.find(co => co.id === this.editedItem.managementCompanyId)?.managementCompany : this.newManagementCompany
       this.duplicateHoaMatch = this.hoas.find(hoa => {
-        return this.editedItem.name === hoa.name && this.editedItem.companyStateId === hoa.companyStateId && this.editedItem.managementCompanyId === hoa.managementCompanyId
+
+        return this.doNamesMatch(this.editedItem.name, hoa.name) && this.editedItem.companyStateId === hoa.companyStateId && (this.doNamesMatch(this.editedItem.managementCompany, hoa.managementCompany) || !hoa.managementCompany)
       })
       if(this.duplicateHoaMatch){
         this.hoaDialog = false
         //add managementCompany name and state name for display purposes
-        this.editedItem.managementCompany = this.managementCompanies.find(co => co.id === this.editedItem.managementCompanyId)?.managementCompany
         this.editedItem.state = this.states.find(state => state.id === this.editedItem.companyStateId)?.state
         this.duplicateDialog = true
       } else {
         this.saveHoa()
       }
+    },
+
+    doNamesMatch(name1, name2){
+      //step 1: remove all punctuation and whitespaces (we don't care if those match)
+      const name1Clean = this.cleanName(name1)
+      const name2Clean = this.cleanName(name2)
+      //step 2: check if name1 contains name2 or vice versa, if so they match
+      return name2Clean && name1Clean &&
+          ((name2Clean.length > 0 && name1Clean.indexOf(name2Clean) >= 0)
+          || (name1Clean && name1Clean.length > 0 && name2Clean.indexOf(name1Clean) >= 0))
+    },
+
+    cleanName(name){
+      return name && name.length > 0 ? name.replace(/[^\w]/g, '').toLowerCase() : name
     },
 
     async saveHoa() {
