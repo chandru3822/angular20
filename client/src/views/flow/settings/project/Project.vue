6a3324ad7a831abd25b0029e5ec0220b8282b67d
turekj <turekj@ldschurch.org>
2023-04-13 14:06:13 -0500

    Project settings tabs mobile view tweaks

--- client/src/views/flow/settings/project/Project.vue
+++ client/src/views/flow/settings/project/Project.vue
@@ -2,14 +2,14 @@
   <v-container class="custom-field-group-container">
     <v-row>
       <v-col cols="12">
-        <v-toolbar flat class="app-toolbar">
+        <v-toolbar flat class="app-toolbar" id="project-settings-toolbar">
           <h3>Projects</h3>
           <v-spacer></v-spacer>
           <v-toolbar-items :slot="isMobile ? 'extension' : 'default'">
-            <v-tabs class="tabs-bar" v-model="activeTab">
+            <v-tabs class="tabs-bar" v-model="activeTab" id="projects-settings-tabs">
               <v-tab v-for="(tab, index) in tabs" :key="index" :to="tab.path"
                      class="text-capitalize ma-0"
-                     :style="{'margin-left': index === 0 ? '12px !important' : '0'}">
+                     :style="{'margin-left': (index === 0 && $vuetify.breakpoint.smAndDown) ? '12px !important' : '0'}">
                 {{ tab.label }}
               </v-tab>
             </v-tabs>
@@ -87,5 +87,18 @@
 .name-container {
   background-color: var(--v-primary-lighten9) !important;
   border-radius: 5px;
+}
+</style>
+<style lang="scss">
+@media (max-width: 959px) {
+  #projects-settings-tabs > div > div.v-slide-group__wrapper > div {
+    justify-content: center;
+  }
+  #projects-settings-tabs > div > div.v-slide-group__prev.v-slide-group__prev--disabled {
+    display: none;
+  }
+  #project-settings-toolbar > div.v-toolbar__extension {
+    padding-bottom: 0.75rem;
+  }
 }
 </style>
