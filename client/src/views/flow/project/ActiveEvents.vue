20b601d4c5740089f2fe2f5052d585fce42d1163
alexace95 <alex@7oaksgroup.com>
2023-05-04 16:38:34 -0400

    Episode 3: Revenge of the Whitelist Changes

--- client/src/views/flow/project/ActiveEvents.vue
+++ client/src/views/flow/project/ActiveEvents.vue
@@ -97,6 +97,7 @@ export default {
         this.activeEventsLoading = true
         const {data} = await getRequest(`/project/${this.projectId}/activeEvents`, null, [])
         this.events = data
+        console.log(this.events);
       } catch (e) {
         logError(e)
       } finally {
