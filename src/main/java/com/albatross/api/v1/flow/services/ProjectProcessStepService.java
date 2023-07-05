4578565b16b82ee0105b7f62d9c05b36dbce2b69
Kaleb Scholes <kscholes@gmail.com>
2023-07-05 17:37:32 -0600

    Fix issue deserializing PGArray to List<Integer>

--- src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
+++ src/main/java/com/albatross/api/v1/flow/services/ProjectProcessStepService.java
@@ -276,7 +276,7 @@ public class ProjectProcessStepService {
             moreParams.put("objectTypeId", 6); //6 is the event object type
             moreParams.put("cfgaId", event.getCustomFieldDisplayValueGroupAssignmentId());
             moreParams.put("primaryId", event.getId());
-            List<CustomFieldValueDisplay> cfvs = sqlCache.queryBySql(ProjectProcessStepQuery.getOneCustomFieldValue, moreParams, CustomFieldValueDisplay.class);
+            List<CustomFieldValueDisplay> cfvs = sqlCache.queryBySql(ProjectProcessStepQuery.getOneCustomFieldValue, moreParams, new CustomFieldValueDisplayMapper(CustomFieldValueDisplay.class, om));
             event.setCustomFieldDisplayValue(cfvs.get(0));
           }
         }
