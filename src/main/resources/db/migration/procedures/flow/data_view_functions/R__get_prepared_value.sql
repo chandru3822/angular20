26a3ff0aad9a0bccdff26a1ff0a2f1bb82b2540d
Scott Keller <kellersk88@gmail.com>
2023-07-06 17:04:23 -0600

    data view stuff

--- src/main/resources/db/migration/procedures/flow/data_view_functions/R__get_prepared_value.sql
+++ src/main/resources/db/migration/procedures/flow/data_view_functions/R__get_prepared_value.sql
@@ -27,10 +27,8 @@ BEGIN
     case when p_value is null then select 'null' into v_value; else select quote_literal(p_value) into v_value; end case;
     v_value = v_value || '::boolean';
   elsif p_data_type_id = 7 then
-    case when p_value is null or p_value = '{}' then select 'null' into v_value; else select quote_literal(string_agg(lov.name, ', '))
-                                                                                                              from flow.list_of_value lov
-                                                                                                              where lov.id = any (p_value::bigint[])
-                                                                                                              into v_value; end case;
+    case when p_value is null or p_value = '{}' then select 'null' into v_value; else select quote_literal(p_value) into v_value; end case;
+    v_value = v_value || '::bigint[]';
     else
     v_value = v_value || '::text';
   end if;
