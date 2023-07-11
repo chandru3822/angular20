30ae974249bdfcdf5745ce9f19ede2c30e0dda8f
Randa <randa@7oaksgroup.com>
2023-07-11 15:34:09 -0600

    mas debug

--- client/src/views/blueraven/closerDashboard/CloserLeaderboard.vue
+++ client/src/views/blueraven/closerDashboard/CloserLeaderboard.vue
@@ -110,7 +110,7 @@
           handleHidingGlobalLoader(this, status)
         } catch (e) {
           console.error('*** ERROR ***', e)
-          this.snackbar = getSnackbar('ERROR', `Error retrieving bookings: ${e}`)
+          this.snackbar = getSnackbar('ERROR', `Error retrieving bookings: ${e.toString()}`)
           this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
           this.bookingsLoading = false
         }
