8908cf9948e8f9c0de8d3733e76b9624f9e85fd2
jessicakburnett <jessica@7oaksgroup.com>
2023-07-26 15:27:57 -0500

    add note section: darken border, remove extra padding between topic field and link checkbox, align link box with the textbox and topic field
    (cherry picked from commit c9b319b37d82ce7b1cf0195080777da2bb42fad7)

--- client/src/views/flow/components/ActivitySection.vue
+++ client/src/views/flow/components/ActivitySection.vue
@@ -152,6 +152,7 @@
           class="mt-3"
           :items="editedActivity.createdById !== currentUserId && !addActivity && this.previouslySelectedTopics?.length > 0 ? topics.filter(t => {return !this.previouslySelectedTopics.find(pst => pst.id === t.id)}) : topics"
           multiple
+          hide-details
           label="Topics"
           return-object
           item-text="hashtag"
@@ -160,7 +161,6 @@
           v-if="null != $route.params.processStepId
                 || null != $route.params.ppsEventId
                 || editedActivity.linked"
-          class="ml-3"
           dense
           :disabled="editedActivity.createdById !== userId && !userIsAdmin && !addActivity"
           v-model="editedActivity.linked"
@@ -594,6 +594,7 @@ export default {
   margin-left: -20px;
   margin-right: -20px;
   max-width: unset !important;
+  border-color: var(--v-grey-darken1);
 }
 
 .activity-body {
