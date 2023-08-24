926fce166bdfe158015e5ad55846b93e191d27dc
Scott Keller <kellersk88@gmail.com>
2023-08-24 15:37:06 -0600

    proposal changes

--- src/main/resources/db/migration/procedures/brs/props/R__get_rebate_for_standard_low_income.sql
+++ src/main/resources/db/migration/procedures/brs/props/R__get_rebate_for_standard_low_income.sql
@@ -31,6 +31,7 @@ declare
   v_panel_count                   bigint;
   v_total_solar_resource_fraction numeric;
   v_value                         numeric;
+  v_plane_rebate_amount           numeric;
 BEGIN
   if p_system_size > p_system_size_cutoff then
     v_value = 0.00::numeric;
@@ -38,20 +39,24 @@ BEGIN
       loop
         v_total_solar_resource_fraction = (x::jsonb -> 'shading' -> 'total_solar_resource_fraction' -> 'annual');
         --raise notice 'v_total_solar_resource_fraction = %',x::jsonb -> 'shading' -> 'total_solar_resource_fraction' -> 'annual';
-        v_panel_count = (x::jsonb -> 'module' -> 'count')::bigint;
-        --raise notice 'v_count = %',x::jsonb -> 'module' -> 'count';
-        v_value = coalesce(v_value, 0) +
-                  least(p_rebate_cap_dollar_amount, (p_total_system_cost_before_rebates * p_rebate_cap_percentage_of_total),
-                        case
-                          when v_total_solar_resource_fraction > .8 then
-                            v_panel_count * p_panel_wattage * p_rebate_rate end);
+          v_panel_count = (x::jsonb -> 'module' -> 'count')::bigint;
+          --raise notice 'v_count = %',x::jsonb -> 'module' -> 'count';
+        if v_total_solar_resource_fraction > 80 then
+          v_plane_rebate_amount = coalesce(v_plane_rebate_amount,0) + (v_panel_count * p_panel_wattage * p_rebate_rate);
+        end if;
+
       end loop;
+    v_value = least(p_rebate_cap_dollar_amount,
+                    (p_total_system_cost_before_rebates * p_rebate_cap_percentage_of_total),
+                    v_plane_rebate_amount);
+
     if p_number_of_batteries > 0 then
-      v_value = coalesce(v_value,0) + least(p_battery_rebate_cap_amount,(p_cash_price_storage * p_battery_rebate_cap_percent_of_total ),
-                              p_system_size * p_battery_rebate_amount);
+      v_value = coalesce(v_value, 0) +
+                least(p_battery_rebate_cap_amount, (p_cash_price_storage * p_battery_rebate_cap_percent_of_total),
+                      p_system_size * p_battery_rebate_amount);
     end if;
-    end if;
-  return coalesce(v_value,0);
+  end if;
+  return coalesce(v_value, 0);
 
 END
 $BODY$
