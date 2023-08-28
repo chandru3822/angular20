3e473d66e0d7ce00a197ca7d255a12c4772bef9a
Randa <randa@7oaksgroup.com>
2023-08-28 09:14:08 -0600

    fix some tourney stuff

--- src/main/resources/db/migration/procedures/brs/tournaments/R__get_tournament_user_score.sql
+++ src/main/resources/db/migration/procedures/brs/tournaments/R__get_tournament_user_score.sql
@@ -52,10 +52,8 @@ BEGIN
                                and tff.field_code = 'APPOINTMENT_DATE')::date) +
                      ((select count(1)
                        from brs.project_details pd
-                              inner join flow.project p on p.id = pd.project_id
-                              inner join flow.contact c on c.id = p.contact_id
                               left join flow.contact_custom_field_value ccfv
-                                        on ccfv.contact_id = c.id and ccfv.custom_field_group_assignment_id = 19106
+                                        on ccfv.contact_id = pd.contact_id and ccfv.custom_field_group_assignment_id = 19106
                        where pd.closer_user_id = p_user_id
                          and pd.cancelled_date is null
                          and pd.final_design_complete_date between p_start_date and p_end_date
@@ -70,10 +68,8 @@ BEGIN
                                 and tff.field_code = 'APPOINTMENT_DATE')::date) * 4) +
                      ((select count(1)
                        from brs.project_details pd
-                              inner join flow.project p on p.id = pd.project_id
-                              inner join flow.contact c on c.id = p.contact_id
                               left join flow.contact_custom_field_value ccfv
-                                        on ccfv.contact_id = c.id and ccfv.custom_field_group_assignment_id = 19106
+                                        on ccfv.contact_id = pd.contact_id and ccfv.custom_field_group_assignment_id = 19106
                        where pd.final_design_complete_date is not null
                          and pd.cancelled_date is null
                          and pd.closer_user_id = p_user_id
