3c550ee128d1bf25e6a1b1a6af9788fb6a5e8552
Kaleb Scholes <kscholes@gmail.com>
2023-08-28 14:07:38 -0600

    [Proposals] Fix filtering of custom fields

--- src/main/java/com/albatross/api/v1/company/blueraven/controllers/proposal/models/ProposalValueFilter.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/controllers/proposal/models/ProposalValueFilter.java
@@ -1,9 +1,7 @@
 package com.albatross.api.v1.company.blueraven.controllers.proposal.models;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import lombok.Data;
 
-@JsonInclude(JsonInclude.Include.NON_NULL)
 @Data
 public class ProposalValueFilter {
   private Long targetFieldId, targetFlowCustomFieldId, parentFieldId, parentFieldValue;
