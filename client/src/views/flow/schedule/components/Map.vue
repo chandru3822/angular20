5dc203c98158ecf78ee494e5a49f52836099ac04
jessicakburnett <jessica@7oaksgroup.com>
2023-09-01 13:15:37 -0500

    trying something a little different

--- client/src/views/flow/schedule/components/Map.vue
+++ client/src/views/flow/schedule/components/Map.vue
@@ -419,12 +419,17 @@ export default {
       }
     },
     async changeMapLocation() {
-      // Here we're catching 'load' map event
-      await this.asyncActions.flyTo({
-        center: [this.longitude, this.latitude],
+      this.map.jumpTo({
+        center:[this.longitude, this.latitude],
         zoom: this.zoom,
         speed: 2
       })
+      // Here we're catching 'load' map event
+      // await this.asyncActions.flyTo({
+      //   center: [this.longitude, this.latitude],
+      //   zoom: this.zoom,
+      //   speed: 2
+      // })
 
     },
     async onMapLoad(event) {
