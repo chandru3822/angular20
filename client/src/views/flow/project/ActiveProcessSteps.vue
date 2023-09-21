d319863ead4cd4487d54b73cbda07a941056a1ae
jessicakburnett <jessica@7oaksgroup.com>
2023-09-21 16:36:58 -0500

    can click entire section header to expand/collapse in project page left panel

--- client/src/views/flow/project/ActiveProcessSteps.vue
+++ client/src/views/flow/project/ActiveProcessSteps.vue
@@ -1,12 +1,11 @@
 <template>
-  <v-row id="project-details-container" class="">
-    <v-col cols="12" lg="12" class="text-left pt-0"  :class="{'pb-0': !sectionExpanded}">
-      <v-col class="py-0" v-if="$store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')">
-        <v-row>
-          <v-toolbar color="transparent" flat class="project-section-header" height="auto">
+  <v-row id="project-details-container" class="px-3">
+    <v-col cols="12" lg="12" class="text-left py-0 px-0">
+      <v-expansion-panels flat class="py-0" v-if="$store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')">
+        <v-expansion-panel>
+          <v-expansion-panel-header color="transparent" flat class="px-0 project-section-header" height="auto">
             <div class="label-large">Active Process Steps</div>
             <v-spacer></v-spacer>
-            <v-toolbar-items>
               <AddProcessStep
                 v-if="project.processId && $store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'ADD')"
                 class="d-inline-block"
@@ -16,41 +15,25 @@
                 :contact-id="project.contactId"
                 @step-added="getProcessSteps"
               />
-<!--              <v-btn text color="grey darken-1" class="" x-small @click="sectionExpanded = !sectionExpanded">-->
-<!--                <v-icon v-if="sectionExpanded">mdi-chevron-up</v-icon>-->
-<!--                <v-icon v-else>mdi-chevron-down</v-icon>-->
-<!--              </v-btn>-->
-              <div class="clickable d-flex" @click="sectionExpanded = !sectionExpanded">
-                <v-icon v-if="sectionExpanded">mdi-chevron-up</v-icon>
-                <v-icon v-else>mdi-chevron-down</v-icon>
-              </div>
-            </v-toolbar-items>
-          </v-toolbar>
+          </v-expansion-panel-header>
 
-          <v-col cols="12" class="py-0" v-if="sectionExpanded">
+          <v-expansion-panel-content cols="12" class="py-0">
             <SpinnerInline v-if="isProcessStepsLoading" :size="20" color="primary"/>
-
-            <ActiveProjectProcessStepSnippet v-else
+            <ActiveProjectProcessStepSnippet v-else class="px-4"
               :steps="processSteps"
               :projectId="projectId"
               :contactId="project.contactId"/>
-          </v-col>
-        </v-row>
-      </v-col>
-
-
-      <v-fade-transition v-if="sectionExpanded && $store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')">
-        <v-col
-          cols="12"
-          class="text-left pt-0"
-        >
-          <router-link class="albatross-body-3" :to="`/project/${projectId}/processSteps`">View All</router-link>
-
-        </v-col>
-      </v-fade-transition>
-
+            <v-col
+                v-if="$store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')"
+                cols="12"
+                class="text-left pt-0"
+            >
+              <router-link class="albatross-body-3" :to="`/project/${projectId}/processSteps`">View All</router-link>
+            </v-col>
+          </v-expansion-panel-content>
+        </v-expansion-panel>
+      </v-expansion-panels>
     </v-col>
-
   </v-row>
 </template>
 
