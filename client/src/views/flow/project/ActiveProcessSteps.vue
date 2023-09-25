c177dae236b7fdb7693402b3e733c5505c02bae4
jessicakburnett <jessica@7oaksgroup.com>
2023-09-25 15:16:35 -0500

    use SidePanelExpansionPanel in Project left side panel

--- client/src/views/flow/project/ActiveProcessSteps.vue
+++ client/src/views/flow/project/ActiveProcessSteps.vue
@@ -1,39 +1,34 @@
 <template>
-  <v-row id="project-details-container" class="mx-6">
-    <v-col cols="12" lg="12" class="text-left py-0 px-0">
-      <v-expansion-panels flat class="py-0" :value="sectionExpanded" v-if="$store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')">
-        <v-expansion-panel>
-          <v-expansion-panel-header color="transparent" flat class="px-0 project-section-header" height="auto">
-            <div class="label-large">Active Process Steps</div>
-            <v-spacer></v-spacer>
-              <AddProcessStep
-                v-if="project.processId && $store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'ADD')"
-                class="d-inline-block"
-                :admin="$store.getters.isFullAdmin"
-                :project-id="projectId"
-                :process-id="project.processId"
-                :contact-id="project.contactId"
-                @step-added="getProcessSteps"
-              />
-          </v-expansion-panel-header>
-          <v-expansion-panel-content cols="12" class="py-0 pb-4">
-            <SpinnerInline v-if="isProcessStepsLoading" centered :size="20" color="primary"/>
-            <ActiveProjectProcessStepSnippet v-else class="px-4"
-              :steps="processSteps"
-              :projectId="projectId"
-              :contactId="project.contactId"/>
-            <v-col
-                v-if="$store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')"
-                cols="12"
-                class="text-left pt-0"
-            >
-              <router-link class="albatross-body-3" :to="`/project/${projectId}/processSteps`">View All</router-link>
-            </v-col>
-          </v-expansion-panel-content>
-        </v-expansion-panel>
-      </v-expansion-panels>
-    </v-col>
-  </v-row>
+  <SidePanelExpansionPanel v-if="$store.getters.userHasFeatureAccessLevel('PROJECTS', 'ADMIN') || $store.getters.userHasFeatureAccessLevel('PROJECTS', 'DELETE')"
+                           header="Active Process Steps"
+                           :section-expanded="sectionExpanded"
+                           :is-loading="isProcessStepsLoading"
+  >
+    <template v-slot:tool-btn>
+      <AddProcessStep
+          v-if="project.processId && $store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'ADD')"
+          class="d-inline-block"
+          :admin="$store.getters.isFullAdmin"
+          :project-id="projectId"
+          :process-id="project.processId"
+          :contact-id="project.contactId"
+          @step-added="getProcessSteps"
+      />
+    </template>
+    <template v-slot:expanded-content>
+      <ActiveProjectProcessStepSnippet class="px-4"
+                                       :steps="processSteps"
+                                       :projectId="projectId"
+                                       :contactId="project.contactId"/>
+      <v-col
+          v-if="$store.getters.userHasFeatureAccessLevel('PROCESS_STEPS', 'VIEW')"
+          cols="12"
+          class="text-left pt-0"
+      >
+        <router-link class="albatross-body-3" :to="`/project/${projectId}/processSteps`">View All</router-link>
+      </v-col>
+    </template>
+  </SidePanelExpansionPanel>
 </template>
 
 <script>
@@ -44,10 +39,12 @@ import ProjectProcessStepSnippet from '@/views/flow/project/ProjectProcessStepSn
 import SpinnerInline from '@/components/SpinnerInline'
 
 import AddProcessStep from '@/views/flow/components/AddProcessStep'
+import SidePanelExpansionPanel from "../../../components/SidePanelExpansionPanel.vue";
 
 export default {
   name: 'ActiveProcessSteps',
   components: {
+    SidePanelExpansionPanel,
     SpinnerInline,
     ActiveProjectProcessStepSnippet,
     ProjectProcessStepSnippet,
@@ -66,7 +63,7 @@ export default {
     return {
       projectId: parseInt(this.$route.params.projectId),
       processSteps: [],
-      sectionExpanded: this.$route.path.includes('processStep') ? 0 : null,
+      sectionExpanded: this.$route.path.includes('processStep'),
       customFieldGroups: [],
       menuOpen: false,
       userCanEdit: this.$store.getters.userHasFeatureAccessLevel('PROJECTS', 'EDIT'),
