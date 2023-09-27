fbd7bd9ebe1daa772ac24c4eabf398e52c747a51
jessicakburnett <jessica@7oaksgroup.com>
2023-09-27 16:06:16 -0500

    add ThreeColumnLayoutMobile.vue

---
+++ client/src/views/ThreeColumnLayoutMobile.vue
@@ -0,0 +1,52 @@
+<template>
+  <v-container class="pa-0" id="three-column-container">
+  <v-row>
+    <slot name="header">
+      <v-toolbar v-if="!headerHidden" flat class="three-column-header px-4">
+        <v-toolbar-title class="headline-medium d-flex align-center mr-6">
+          <slot name="back-btn"></slot>
+          {{ headerText }}
+        </v-toolbar-title>
+        <slot name="search"></slot>
+        <v-spacer></v-spacer>
+        <v-toolbar-items>
+          <slot name="header-btn">
+          </slot>
+        </v-toolbar-items>
+      </v-toolbar>
+    </slot>
+  </v-row>
+  <v-row class="pa-6">
+    <v-btn small text color="primary" @click="collapseSide('left')">
+      <v-icon>mdi-menu</v-icon>
+    </v-btn>
+  </v-row>
+  </v-container>
+</template>
+
+<script>
+
+export default {
+  name: "ThreeColumnLayoutMobile",
+  props: {
+    headerText: String,
+    headerBtnText: String,
+    headerHidden: Boolean,
+  }
+}
+</script>
+
+<style scoped lang="scss">
+#three-column-container {
+  width: 100%;
+  height: 100%;
+  max-height: 100% !important;
+  padding: 0 !important;
+  overflow: hidden;
+}
+
+.three-column-header {
+  height: 64px;
+  background-color: var(--v-grey-lighten2) !important;
+}
+</style>
