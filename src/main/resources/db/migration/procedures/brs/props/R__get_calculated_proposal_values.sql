405475ff8bad9146a4d496282239279fd2c4da55
Kaleb Scholes <kscholes@gmail.com>
2023-10-11 11:01:20 -0600

    Separated conditions in where clause of proposal_value query

--- src/main/resources/db/migration/procedures/brs/props/R__get_calculated_proposal_values.sql
+++ src/main/resources/db/migration/procedures/brs/props/R__get_calculated_proposal_values.sql
@@ -1342,8 +1342,10 @@ BEGIN
     v_first_year_rebate_cap,v_is_first_year_rebate_cap
   from proposal_value pv
   where object_code = 'PROPOSAL_REBATE'
-    and (jsonb_path_exists(row, '$.fields[*] ? ((@.fieldId == $targetFieldId && @.intValue == $intValue) && (@.intValue == $intValue1 && @.fieldId == $fieldId1))',
-                           jsonb_build_object('targetFieldId', 86, 'intValue', v_state_id,'intValue1',454,'fieldId1',96)));
+    and (jsonb_path_exists(row, '$.fields[*] ? (@.fieldId == $targetFieldId && @.intValue == $intValue)',
+                           jsonb_build_object('targetFieldId', 86, 'intValue', v_state_id)))
+    and (jsonb_path_exists(row, '$.fields[*] ? (@.intValue == $intValue1 && @.fieldId == $fieldId1)',
+                               jsonb_build_object('intValue1',454,'fieldId1',96)));
 
   v_state_rebate_amount = coalesce(v_state_rebate_amount, 0);
   raise notice 'v_state_rebate_amount***************************** = % ',v_state_rebate_amount;
