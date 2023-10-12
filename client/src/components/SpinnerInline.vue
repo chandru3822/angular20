6cf144def742f68a151f4885707d48ddf7411d76
jessicakburnett <jessica@7oaksgroup.com>
2023-10-12 15:07:50 -0500

    halloween
    (cherry picked from commit a1946aec548170e5f33c576a17928695b674a370)

--- client/src/components/SpinnerInline.vue
+++ client/src/components/SpinnerInline.vue
@@ -1,11 +1,12 @@
 <template>
   <v-col class="spinner-container" :class="{'transparent': transparent, 'centered': centered}">
     {{text}}
-    <v-progress-circular
-        indeterminate
-        :size="size"
-        :color="spinnerColor"
-    ></v-progress-circular>
+<!--    <v-progress-circular-->
+<!--        indeterminate-->
+<!--        :size="size"-->
+<!--        :color="spinnerColor"-->
+<!--    ></v-progress-circular>-->
+    <img :src="require('../assets/blueraven/pumpkinCat.gif')"/>
   </v-col>
 </template>
 
--- client/src/helpers/constants.js
+++ client/src/helpers/constants.js
@@ -3,7 +3,7 @@ const { VITE_BASE_API, VITE_ENV, VITE_MAPBOX_ACCESS_TOKEN, VITE_MAPBOX_STYLE} = 
 let constants = {}
 
 constants.NOT_FOUND_404_TEXT = 'FAIL_TO_NOT_FOUND_SCREEN'
-constants.LOCAL_COLOR = 'pink'
+constants.LOCAL_COLOR = 'rgb(254, 90, 14)'
 constants.STAGE_COLOR = 'orange'
 constants.FLUX_COLOR = 'purple'
 constants.UAT_COLOR = 'blue' //this is a light blue color
