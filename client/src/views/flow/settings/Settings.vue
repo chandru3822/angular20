54e2e3153577993f2b5f153a7f9f551054e214b0
jessicakburnett <jessica@7oaksgroup.com>
2023-10-13 14:47:45 -0500

    consolidate Settings list and SettingsMenu list so we only have to update once

--- client/src/views/flow/settings/Settings.vue
+++ client/src/views/flow/settings/Settings.vue
@@ -24,10 +24,10 @@
               </v-btn>
             </v-toolbar>
           </template>
-          <SettingsMenu class="pa-3" :title="title" @closeMenu="menuOpen=false" @updateTitle="setTitle($event)"></SettingsMenu>
+          <SettingsMenu class="pa-3" :title="title" :menu-list="items" :companyObjectItems="companyObjectTypes" @closeMenu="menuOpen=false" @updateTitle="setTitle($event)"></SettingsMenu>
         </v-menu>
         <v-card v-else class=" left-menu square-card d-flex">
-          <SettingsMenu class="px-5 py-2 settings-container" :class="{'hidden': leftCollapsed}"></SettingsMenu>
+          <SettingsMenu class="px-5 py-2 settings-container" :menu-list="items" :class="{'hidden': leftCollapsed}"></SettingsMenu>
           <v-btn small text color="primary" @click="collapseMenu" class="py-6">
             <v-icon>mdi-menu</v-icon>
           </v-btn>
