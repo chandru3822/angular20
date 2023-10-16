fbf0e582f9aab23e0f63164e7ab45e66d62c14ba
jberns003 <54857272+jberns003@users.noreply.github.com>
2023-10-16 11:01:57 -0400

    New contact lead endpoints

--- src/main/java/com/albatross/api/v1/company/blueraven/controllers/ContactLeadController.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/controllers/ContactLeadController.java
@@ -345,4 +345,14 @@ public class ContactLeadController {
   public void updateContactRedVentures(@RequestBody ContactLead contactLead) {
     contactLeadService.saveContactLead(contactLead);
   }
+
+  @PostMapping(value = "/profitise", produces = MediaType.APPLICATION_JSON_VALUE)
+  public void updateContactProfitise(@RequestBody ContactLead contactLead) {
+    contactLeadService.saveContactLead(contactLead);
+  }
+
+  @PostMapping(value = "/rhino", produces = MediaType.APPLICATION_JSON_VALUE)
+  public void updateContactRhino(@RequestBody ContactLead contactLead) {
+    contactLeadService.saveContactLead(contactLead);
+  }
 }
