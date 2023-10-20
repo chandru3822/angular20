272f21493f1a439657c69bad09f91b805dd7d231
jessicakburnett <jessica@7oaksgroup.com>
2023-10-19 20:19:48 -0500

    remove a bunch of stuff that the SidePanelExpansionPanel doesn't need

--- client/src/components/SidePanelExpansionPanel.vue
+++ client/src/components/SidePanelExpansionPanel.vue
@@ -20,20 +20,12 @@
 
 <script>
 
-import {getRequest, logError} from '@/helpers/helpers'
-import ActiveProjectProcessStepSnippet from '@/views/flow/project/ActiveProjectProcessStepSnippet'
-import ProjectProcessStepSnippet from '@/views/flow/project/ProjectProcessStepSnippet'
 import SpinnerInline from '@/components/SpinnerInline'
-
-import AddProcessStep from '@/views/flow/components/AddProcessStep'
 
 export default {
   name: 'SidePanelExpansionPanel',
   components: {
-    SpinnerInline,
-    ActiveProjectProcessStepSnippet,
-    ProjectProcessStepSnippet,
-    AddProcessStep,
+    SpinnerInline
   },
   props: {
     header: String,
@@ -42,94 +34,15 @@ export default {
       default: false
     },
     isLoading: Boolean,
-  },
-  watch: {
-    updateKey: function () {
-      this.getProcessSteps()
-    },
-  },
-  data() {
-    return {
-      projectId: parseInt(this.$route.params.projectId),
-      processSteps: [],
-      customFieldGroups: [],
-      menuOpen: false,
-      userCanEdit: this.$store.getters.userHasFeatureAccessLevel('PROJECTS', 'EDIT'),
-      userHasEventsFeature: this.$store.getters.userHasFeature('EVENTS'),
-      isProcessStepsLoading: false,
-      snackbar: {},
-      stepsSearch: '',
-      isProcessStepsExpanded: false,
-      companyId: this.$store.state.user.details.companyId,
-    }
-  },
-  created() {
-    this.getProcessSteps()
-  },
-  computed: {
-    processStepsByName() {
-      const names = [...new Set(this.processSteps.map(step => step.processStepName))]
-
-      return names.map(processStepName => {
-        return {
-          processStepName,
-          processSteps: this.processSteps.filter(step => step.processStepName === processStepName)
-        }
-      })
-    }
-  },
-  methods: {
-    filteredProcessSteps() {
-      return this.stepsSearch === '' ? this.processStepsByName : this.processStepsByName.filter(psn => psn.processStepName.toLowerCase().includes(this.stepsSearch.toLowerCase()))
-    },
-    getProcessSteps: async function () {
-      try {
-        this.isProcessStepsLoading = true
-        const {data} = await getRequest(`/project/${this.projectId}/upcomingProcessSteps`)
-        this.processSteps = data
-      } catch (e) {
-        logError(e)
-      } finally {
-        this.isProcessStepsLoading = false
-      }
-    },
   }
 }
 </script>
 
 <style lang="scss" scoped>
-#project-details-container {
-  margin-top: -15px;
-  padding-left: 0;
-  padding-right: 0;
-  padding-top: 0;
-}
-
-.project-header {
-  border-bottom: solid 1px #EAEAF4
-}
-
-.project-title {
-  font-size: 20px;
-}
-
-.project-subtitle {
-  font-size: 15px;
-}
-
-.work-type-header {
-  &:not(:first-child) {
-    padding-top: 20px;
-  }
-}
 </style>
 
 <style lang="scss">
 #side-panel-expansion-panel-container div.v-expansion-panel-content__wrap{
   padding:0;
 }
-</style>
-
-<style scoped lang="scss">
-
 </style>
