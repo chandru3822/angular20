ce6cecb2b6f97ba6c155c836c363b4bff023a0ae
Randa <randa@7oaksgroup.com>
2023-10-24 13:16:25 -0600

    dont fail if no owner

--- client/src/views/flow/contacts/Contact.vue
+++ client/src/views/flow/contacts/Contact.vue
@@ -748,7 +748,7 @@ export default {
       this.$store.commit(AppMutations.SET_LOADING, true)
       try {
         //temp contact holds all the changes in case they cancel. use those values
-        this.tempContact.ownerUserPositionId = this.tempContact.owner.userPositionId
+        this.tempContact.ownerUserPositionId = this.tempContact.owner?.userPositionId || null
         const {status} = await postRequest(`/contact`, this.tempContact)
         this.snackbar = getSnackbar('SUCCESS', 'Contact Updated')
         this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
