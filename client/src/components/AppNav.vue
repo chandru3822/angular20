c6371da99758b0f85d213ae432ac274ac13e62ce
jessicakburnett <jessica@7oaksgroup.com>
2023-10-25 15:47:05 -0500

    remove spaces in svg name
    (cherry picked from commit 54d62aafe396a095a43d7b7570975f0efec254ee)

--- client/src/components/AppNav.vue
+++ client/src/components/AppNav.vue
@@ -27,7 +27,7 @@
             <template v-slot:activator="{ on }">
               <v-btn icon v-on="on" :color="selectedCompany.logoPresignedUrl ? 'transparent' : '#bbbbbb'">
                 <img class="header-logo" v-if="selectedCompany.logoPresignedUrl"
-                     :src="require('../assets/blueraven/noun-bat-3435597 1.svg')">
+                     :src="require('client/src/assets/blueraven/batIcon.svg')">
                 <v-icon v-else>mdi-office-building</v-icon>
               </v-btn>
             </template>
