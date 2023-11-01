969c4a0d6ee56d5c52a32414787c1813be8cf1ea
jessicakburnett <jessica@7oaksgroup.com>
2023-10-31 21:22:09 -0500

    Revert "replace icon and gif"
    This reverts commit 5102be64630d2ad9e59cb83c25bef02cdf0677bf.

--- client/src/components/Spinner.vue
+++ client/src/components/Spinner.vue
@@ -5,7 +5,7 @@
 <!--        :size="size"-->
 <!--        :color="spinnerColor"-->
 <!--    ></v-progress-circular>-->
-    <img :src="require('../assets/blueraven/Cauldron.gif')" width="500" height="500"/>
+    <img :src="require('../assets/blueraven/pumpkinCat.gif')"/>
   </v-col>
 </template>
 
