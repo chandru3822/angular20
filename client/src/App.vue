0c49f61d758e579ce7811146cc614153371cb388
jessicakburnett <jessica@7oaksgroup.com>
2023-11-03 15:19:03 -0500

    what if we just put a button in the app nav to link to the mobile app download?

--- client/src/App.vue
+++ client/src/App.vue
@@ -9,23 +9,23 @@
       </v-toolbar-items>
     </v-toolbar>
     <!--    <v-toolbar v-if="showMobileBanner && $route.name !== 'login'">-->
-    <v-toolbar
-      v-if="!dismissMobileToolbar && showMobileBanner && !noNavRoutes.includes($route.name) && $route.path !== '/apps'"
-      class="clickable app-link-header"
-      dense>
-      <v-toolbar-title @click="goToApps" class="body-small primary--text">
-        Go to App Download Page
-        <v-icon small color="primary" class="ml-3">
-          mdi-arrow-right
-        </v-icon>
-      </v-toolbar-title>
-      <v-spacer></v-spacer>
-      <v-toolbar-items>
-        <v-btn x-small text color="primary" @click="dismissMobileToolbar = !dismissMobileToolbar">
-          <v-icon small>close</v-icon>
-        </v-btn>
-      </v-toolbar-items>
-    </v-toolbar>
+<!--    <v-toolbar-->
+<!--      v-if="!dismissMobileToolbar && showMobileBanner && !noNavRoutes.includes($route.name) && $route.path !== '/apps'"-->
+<!--      class="clickable app-link-header"-->
+<!--      dense>-->
+<!--      <v-toolbar-title @click="goToApps" class="body-small primary&#45;&#45;text">-->
+<!--        Go to App Download Page-->
+<!--        <v-icon small color="primary" class="ml-3">-->
+<!--          mdi-arrow-right-->
+<!--        </v-icon>-->
+<!--      </v-toolbar-title>-->
+<!--      <v-spacer></v-spacer>-->
+<!--      <v-toolbar-items>-->
+<!--        <v-btn x-small text color="primary" @click="dismissMobileToolbar = !dismissMobileToolbar">-->
+<!--          <v-icon small>close</v-icon>-->
+<!--        </v-btn>-->
+<!--      </v-toolbar-items>-->
+<!--    </v-toolbar>-->
     <AppNav v-if="!noNavRoutes.includes($route.name) && !hideHeader" />
     <v-main>
       <v-container class="router-container" :class="{'extra-banner': userIsMasquerading}">
