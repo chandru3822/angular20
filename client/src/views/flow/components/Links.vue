e44ee08a848bbd4fe3a246d92178f201c7249923
jessicakburnett <jessica@7oaksgroup.com>
2023-11-08 11:43:39 -0600

    update process step links appearance to take up less space

--- client/src/views/flow/components/Links.vue
+++ client/src/views/flow/components/Links.vue
@@ -5,11 +5,9 @@
       <v-toolbar color="transparent" class="elevation-0">
         <v-toolbar-title>Links</v-toolbar-title>
       </v-toolbar>
-      <v-list class="pa-0">
-        <v-list-item v-for="(l, index) in links" :key="l.id" :class="{'shaded-row': index % 2}">
-          <v-btn text target="_blank" @click="followLink(l.url)">{{l.link}}</v-btn>
-        </v-list-item>
-      </v-list>
+      <v-row v-for="(l, index) in links" class="px-7">
+        <a  :key="l.id" text target="_blank" @click="followLink(l.url)" class="text-capitalize body-medium"><v-icon color="primary" class="pr-1">mdi-link</v-icon>{{l.link}}</a>
+      </v-row>
     </v-col>
   </v-row>
 </v-col>
