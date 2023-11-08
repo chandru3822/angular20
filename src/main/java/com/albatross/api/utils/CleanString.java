dcb7fff6b6902c7608bbffccbd8dad682299866a
Randa <randa@7oaksgroup.com>
2023-11-08 16:44:52 -0700

    chars that dont consume width

--- src/main/java/com/albatross/api/utils/CleanString.java
+++ src/main/java/com/albatross/api/utils/CleanString.java
@@ -25,6 +25,7 @@ public class CleanString {
     cleanFilename = cleanFilename.replaceAll("“", "\"");
     cleanFilename = cleanFilename.replaceAll("”", "\"");
     cleanFilename = cleanFilename.replaceAll("\u2006", " "); //replaces Six-Per-Em Space (cuz we had 25)
+    cleanFilename = cleanFilename.replaceAll("\u200B", " "); //replaces a space that doesn't consume any width (cuz we had 1)
     cleanFilename = cleanFilename.replaceAll("\u202F", " "); //replaces Narrow NBSP cuz WTF
     cleanFilename = cleanFilename.replaceAll("\u00A0", " "); //replaces NBSP
     cleanFilename = cleanFilename.replaceAll("\t", " ");
