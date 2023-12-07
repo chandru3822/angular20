cc21951d128c6a76ce1e93dfc98fa2360732c4d4
jessicakburnett <jessica@7oaksgroup.com>
2023-12-06 18:04:59 -0600

    remove 'Project' from headers on all screen sizes, not just mobile

--- client/src/views/flow/PageOverview.vue
+++ client/src/views/flow/PageOverview.vue
@@ -1,6 +1,6 @@
 <template>
 <v-container class="pa-0 mobile-background">
-  <SidePanelExpansionPanel :header="`${pageName} Overview`" :is-disabled="!isExpandable" :section-expanded="opened">
+  <SidePanelExpansionPanel header="Overview" :is-disabled="!isExpandable" :section-expanded="opened">
     <template v-slot:tool-btn>
       <v-btn
           text small color="primary" class="px-0"
