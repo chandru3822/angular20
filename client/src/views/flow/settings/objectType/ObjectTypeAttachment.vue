77c3ab4d3f4d4546d5ba2994bcc620e4e5968848
jessicakburnett <jessica@7oaksgroup.com>
2023-12-13 18:14:05 -0600

    attachment type table drop down contents better

--- client/src/views/flow/settings/objectType/ObjectTypeAttachment.vue
+++ client/src/views/flow/settings/objectType/ObjectTypeAttachment.vue
@@ -54,7 +54,8 @@
                 hide-default-header
                 :sort-desc="[false]"
                 :sort-by="['groupOrder']"
-                class="elevation-1 attachment-cfg-table square-card table-striped"
+                class="elevation-1 attachment-cfg-table square-card striped"
+                :class="{'table-striped': isMobile}"
               >
                 <template #no-data>
                   No custom field groups assigned
@@ -65,14 +66,15 @@
                 </template>
 
                     <template #item.draggable="{ item, index }">
-                      <td class="draggable-handle-col">
+                      <td class="draggable-handle-col" :class="{'shaded-row': index % 2 && !isMobile}">
                       <v-btn text icon small color="primary" class="handle" v-if="userCanEdit">
                         <v-icon>drag_handle</v-icon>
                       </v-btn>
                       </td>
                     </template>
                     <template #item.groupName="{ item, index }" class="text-left" :class="{'mb-4': $vuetify.breakpoint.xsOnly && item.edit}">
-                      <div v-if="userCanEdit">
+                      <td :class="{'shaded-row': index % 2 && !isMobile}">
+                      <div v-if="userCanEdit" :class="{'shaded-row': index % 2}">
                         <v-text-field text
                                       v-if="item.edit"
                                       v-model="item.groupName">
@@ -86,8 +88,10 @@
                         </a>
                       </div>
                       <span v-else>{{ item.groupName }}</span>
+                      </td>
                     </template>
                     <template #item.icons="{item, index}">
+                      <td :class="{'shaded-row': index % 2 && !isMobile}">
                       <div class="item-icons text-right">
                         <v-btn v-if="userCanAdd" small text color="primary"
                                @click="[addField = !addField, selectedIndex = index, expanded = [item], loadFieldsByParent()]">
@@ -101,10 +105,11 @@
                         </v-btn>
                         <v-btn small color="primary" text @click="cfGroupToDelete = item"><v-icon>delete</v-icon></v-btn>
                       </div>
+                      </td>
                     </template>
 
-                <template #expanded-item="{ headers, item }">
-                  <tr class="pb-2 px-0" :class="{'shaded-row': selectedIndex % 2,'mobile-tr': $vuetify.breakpoint.xsOnly}">
+                <template #expanded-item="{ headers, item, index }">
+                  <td :colspan="headers.length" class="pb-2 px-0" :class="{'shaded-row': selectedIndex % 2,'mobile-tr': $vuetify.breakpoint.xsOnly}">
                     <v-col cols="12" class="pl-3 pr-3 justify" v-if="addField">
                       <h3 class="text-left">Add Ancillary Field</h3>
                       <v-autocomplete v-model="selectedAncillaryField"
@@ -153,7 +158,7 @@
                         </v-list>
                       </draggable>
                     </v-col>
-                  </tr>
+                  </td>
                 </template>
               </v-data-table>
             </v-col>
