d4030b8c94845e2111f8ee11324c868516ec5001
eric.thomson <74467416+ethomson713@users.noreply.github.com>
2024-02-05 16:57:28 -0500

    Added Project Tag History that will allow admins to see when and who adds and removes tags from an account.

--- src/main/java/com/albatross/api/v1/flow/model/ProjectTag.java
+++ src/main/java/com/albatross/api/v1/flow/model/ProjectTag.java
@@ -3,6 +3,8 @@ package com.albatross.api.v1.flow.model;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Date;
+
 /**
  * Created by randanunn on 10/1/19.
  * !Describe Purpose!
@@ -12,7 +14,8 @@ import lombok.Setter;
 public class ProjectTag {
 
   private Long id, projectId, tagId;
-  private String tagName, fontColor, bgColor;
+  private String tagName, fontColor, bgColor, fullNameCreatedBy, fullNameModifiedBy;
+  private Date dateCreated, dateModified;
   private Boolean archived;
 
 }
