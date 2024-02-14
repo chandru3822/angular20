5483767c8fbca75f5e8de167f89a62f9e89ab248
Kaleb Scholes <kscholes@gmail.com>
2024-02-14 08:55:21 -0700

    Hotfix: revert use of virtual threads
    (cherry picked from commit f4eaff6e8934335194c1b3ebb61b7dad203da832)

--- src/main/java/com/albatross/api/config/AsyncConfig.java
+++ src/main/java/com/albatross/api/config/AsyncConfig.java
@@ -7,12 +7,22 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.scheduling.annotation.AsyncConfigurer;
 import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 import org.springframework.web.servlet.config.annotation.AsyncSupportConfigurer;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @EnableAsync
 @Configuration
 public class AsyncConfig implements AsyncConfigurer {
+
+  @Bean(name = "taskExecutor")
+  public ThreadPoolTaskExecutor getAsyncExecutor() {
+    ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+    executor.setCorePoolSize(10);
+    executor.setMaxPoolSize(50);
+    executor.setThreadNamePrefix("AlbaAsync");
+    return executor;
+  }
 
   @Override
   public AsyncUncaughtExceptionHandler getAsyncUncaughtExceptionHandler() {
@@ -25,7 +35,7 @@ public class AsyncConfig implements AsyncConfigurer {
       @Override
       public void configureAsyncSupport(@NotNull AsyncSupportConfigurer configurer) {
         final long oneHourMs = 3600000L;
-        configurer.setDefaultTimeout(oneHourMs);//.setTaskExecutor(getAsyncExecutor());
+        configurer.setDefaultTimeout(oneHourMs).setTaskExecutor(getAsyncExecutor());
         WebMvcConfigurer.super.configureAsyncSupport(configurer);
       }
     };
