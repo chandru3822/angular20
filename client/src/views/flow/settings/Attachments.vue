b8bb304baf1a5e3ad31a3f25ca66f2badd0c4f76
eric.thomson <74467416+ethomson713@users.noreply.github.com>
2024-02-20 10:50:57 -0500

    Attachments.vue Updated to composition API

--- client/src/views/flow/settings/Attachments.vue
+++ client/src/views/flow/settings/Attachments.vue
@@ -40,8 +40,8 @@
           <v-spacer></v-spacer>
           <v-toolbar-items>
             <v-btn text @click="[addNew = !addNew, newType = {}]" color="primary"
-                   v-if="$store.getters.userHasFeatureAccessLevel('SETTINGS', 'ADD')">
-              <v-icon v-if="$vuetify.breakpoint.xsOnly">{{ addNew ? 'close' : 'add' }}</v-icon>
+                   v-if="store.getters.userHasFeatureAccessLevel('SETTINGS', 'ADD')">
+              <v-icon v-if="vuetify.breakpoint.xsOnly">{{ addNew ? 'close' : 'add' }}</v-icon>
               <span v-else>{{ addNew ? 'Cancel' : 'Add New' }}</span>
             </v-btn>
           </v-toolbar-items>
@@ -79,7 +79,7 @@
               <template #item="{ item, index }">
                 <tr :class="{'shaded-row': index % 2}">
                   <td class="text-left clickable" @click="goToType(item.id)">{{ item.attachmentType }}</td>
-                  <td class="text-right" :class="{'d-flex flex-column align-end': $vuetify.breakpoint.xsOnly}">
+                  <td class="text-right" :class="{'d-flex flex-column align-end': vuetify.breakpoint.xsOnly}">
                     <v-btn small text color="primary" @click="goToType(item.id)">
                       <v-icon>edit</v-icon>
                     </v-btn>
@@ -101,136 +101,124 @@
 </template>
 
 
-<script>
-import {AppMutations} from '@/stores/AppStore'
-import Vue2Filters from 'vue2-filters'
-import orderBy from 'lodash.orderby'
+<script setup>
+  import { getCurrentInstance, ref, computed, onMounted} from "vue";
+  import {AppMutations} from '@/stores/AppStore'
+  import Vue2Filters from 'vue2-filters'
+  import orderBy from 'lodash.orderby'
 
-import {
-  handleHidingGlobalLoader,
-  getRequest,
-  deleteRequest,
-  postRequest,
-  getSnackbar
-} from '@/helpers/helpers'
-import constants from '@/helpers/constants'
-import ConfirmationDialog from "@/components/ConfirmationDialog";
+  import {
+    handleHidingGlobalLoader,
+    getRequest,
+    deleteRequest,
+    postRequest,
+    getSnackbar
+  } from '@/helpers/helpers'
+  import constants from '@/helpers/constants'
+  import ConfirmationDialog from "@/components/ConfirmationDialog";
 
-export default {
-  name: 'Attachments',
-  components: {ConfirmationDialog},
-  mixins: [Vue2Filters.mixin],
+  const vueInstance = getCurrentInstance().proxy
+  const snackbar = vueInstance.$snackbar
+  const store = vueInstance.$store
+  const vuetify = vueInstance.$vuetify
 
-  data() {
-    return {
-      snackbar: {},
-      constants,
-      attachmentTypes: [],
-      search: '',
-      addNew: false,
-      newType: {},
-      selectedAttachmentTypeId: null,
-      userId: this.$store.state.user.details.id,
-      companyId: this.$store.state.user.details.companyId,
-      cannotDeleteReasons: {},
-      deleteError: false,
-      headers: [
-        {text: 'Attachment Type', value: 'attachmentType', show: true},
-        {text: '', value: 'icons', show: true},
-      ],
-      footerProps: {
-        'items-per-page-options': [25, 50, 100, 1000],
-        'items-per-page-text': 'Rows per page:'
-      },
-      showDeleteDialog: false,
-      itemToDelete: null
+  const attachmentTypes =  ref([])
+  const search =  ref('')
+  const addNew =  ref(false)
+  const newType =  ref({})
+  const selectedAttachmentTypeId =  ref(null)
+  const userId =  ref(store.state.user.details.id)
+  const companyId =  ref(store.state.user.details.companyId)
+  const cannotDeleteReasons =  ref({})
+  const deleteError =  ref(false)
+  const headers =  ref([
+    {text: 'Attachment Type', value: 'attachmentType', show: true},
+    {text: '', value: 'icons', show: true},
+  ])
+  const footerProps =  ref({
+    'items-per-page-options': [25, 50, 100, 1000],
+    'items-per-page-text': 'Rows per page:'
+  })
+  const showDeleteDialog =  ref(false)
+  const itemToDelete =  ref(null)
+
+  const itemToDeleteAttachmentType = () => {
+    return itemToDelete.value ? itemToDelete.value.attachmentType : ''
+  }
+
+  onMounted(() => {
+    getAttachmentTypes()
+  })
+  const goToType = (typeId) => {
+    router.push({path: `/settings/attachment/${typeId}/customFieldGroups`})
+  }
+  const getAttachmentTypes = async () => {
+    store.commit(AppMutations.SET_LOADING, true)
+    try {
+      const {data, status} = await getRequest(`/attachmentType/types`)
+      attachmentTypes.value = orderBy(data, [a => a.attachmentType.toLowerCase()])
+
+      handleHidingGlobalLoader(vueInstance, status)
+    } catch (e) {
+      console.error('*** ERROR ***', e)
+      snackbar('ERROR', 'Error Retrieving Attachment Types')
+      store.commit(AppMutations.SET_LOADING, false)
     }
-  },
-  computed: {
-    itemToDeleteAttachmentType() {
-      return this.itemToDelete ? this.itemToDelete.attachmentType : ''
+  }
+  const deleteType = async () => {
+    const item = itemToDelete.value
+    store.commit(AppMutations.SET_LOADING, true)
+    try {
+      const {status} = await deleteRequest(`/attachmentType/delete/${item.id}`)
+      item.archived = true
+      snackbar('SUCCESS', 'Successfully Deleted Attachment Type')
+      handleHidingGlobalLoader(vueInstance, status)
+    } catch (e) {
+      console.error('*** ERROR ***', e)
+      if (e.status === 400) {
+        item.deleteConfirm = false
+        deleteError.value = true
+        cannotDeleteReasons.value = e.data
+      }
+      snackbar('ERROR', 'Error Deleting Attachment Type')
+      store.commit(AppMutations.SET_LOADING, false)
     }
-  },
-  async created() {
-    await this.getAttachmentTypes()
-  },
-  methods: {
-    goToType(typeId) {
-      this.$router.push({path: `/settings/attachment/${typeId}/customFieldGroups`})
-    },
-    async getAttachmentTypes() {
-      this.$store.commit(AppMutations.SET_LOADING, true)
-      try {
-        const {data, status} = await getRequest(`/attachmentType/types`)
-        this.attachmentTypes = orderBy(data, [a => a.attachmentType.toLowerCase()])
+    closeDeleteDialog()
+  }
 
-        handleHidingGlobalLoader(this, status)
-      } catch (e) {
-        console.error('*** ERROR ***', e)
-        this.snackbar = getSnackbar('ERROR', 'Error Retrieving Attachment Types')
-        this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
-        this.$store.commit(AppMutations.SET_LOADING, false)
-      }
-    },
-    async deleteType() {
-      const item = this.itemToDelete
-      this.$store.commit(AppMutations.SET_LOADING, true)
-      try {
-        const {status} = await deleteRequest(`/attachmentType/delete/${item.id}`)
-        item.archived = true
-        this.snackbar = getSnackbar('SUCCESS', 'Successfully Deleted Attachment Type')
-        this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
-        handleHidingGlobalLoader(this, status)
-      } catch (e) {
-        console.error('*** ERROR ***', e)
-        if (e.status === 400) {
-          item.deleteConfirm = false
-          this.deleteError = true
-          this.cannotDeleteReasons = e.data
-        }
-        this.snackbar = getSnackbar('ERROR', 'Error Deleting Attachment Type')
-        this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
-        this.$store.commit(AppMutations.SET_LOADING, false)
-      }
-      this.closeDeleteDialog()
-    },
-    async addNewType() {
-      this.$store.commit(AppMutations.SET_LOADING, true)
-      try {
-        this.newType.companyId = this.companyId
-        const {data, status} = await postRequest(`/attachmentType/type`, this.newType, null, [])
+  const addNewType = async () => {
+    store.commit(AppMutations.SET_LOADING, true)
+    try {
+      newType.value.companyId = companyId.value
+      const {data, status} = await postRequest(`/attachmentType/type`, this.newType, null, [])
 
-        this.snackbar = getSnackbar('SUCCESS', 'Action Type Added')
-        this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
+      snackbar('SUCCESS', 'Action Type Added')
 
-        // add it to the records already on the screen
-        this.attachmentTypes.push(data)
-        this.attachmentTypes = orderBy(this.attachmentTypes, [a => a.attachmentType.toLowerCase()])
+      // add it to the records already on the screen
+      attachmentTypes.value.push(data)
+      attachmentTypes.value = orderBy(attachmentTypes.value, [a => a.attachmentType.toLowerCase()])
 
-        // reset the new process fields
-        this.addNew = false
-        this.newType = {}
+      // reset the new process fields
+      addNew.value = false
+      newType.value = {}
 
-        handleHidingGlobalLoader(this, status)
-      } catch (e) {
-        console.error('*** ERROR ***', e)
-        this.snackbar = getSnackbar('ERROR', 'Error Adding Attachment Type')
-        this.$store.commit(AppMutations.SHOW_SNACK, this.snackbar)
-        this.$store.commit(AppMutations.SET_LOADING, false)
-      }
-    },
-    closeDeleteDialog() {
-      this.showDeleteDialog = false
-      this.itemToDelete = null
-    },
-    filterTypes() {
-      return this.attachmentTypes.filter(e => {
-        return !e.archived
-      })
-    },
-  },
+      handleHidingGlobalLoader(vueInstance, status)
+    } catch (e) {
+      console.error('*** ERROR ***', e)
+      snackbar('ERROR', 'Error Adding Attachment Type')
+      store.commit(AppMutations.SET_LOADING, false)
+    }
+  }
+  const closeDeleteDialog = () => {
+    showDeleteDialog.value = false
+    itemToDelete.value = null
+  }
+  const filterTypes = () => {
+    return attachmentTypes.value.filter(e => {
+      return !e.archived
+    })
+  }
 
-}
 </script>
 
 <style lang="scss">
