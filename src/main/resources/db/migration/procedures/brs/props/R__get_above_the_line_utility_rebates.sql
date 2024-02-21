2044d9a48ad26e38a990c4fae9bf7feaee027a93
Scott Keller <kellersk88@gmail.com>
2024-02-21 14:09:45 -0700

    commission details

--- src/main/resources/db/migration/procedures/brs/props/R__get_above_the_line_utility_rebates.sql
+++ src/main/resources/db/migration/procedures/brs/props/R__get_above_the_line_utility_rebates.sql
@@ -2,15 +2,21 @@ drop function if exists brs.get_above_the_line_utility_rebates(p_version_id bigi
                                                                p_aurora_design_summary jsonb, p_system_size numeric,
                                                                p_total_system_cost_before_rebates numeric,
                                                                p_state_id bigint);
+drop function if exists brs.get_above_the_line_utility_rebates(p_version_id bigint, p_utility_company_id bigint,
+                                                               p_aurora_design_summary jsonb, p_system_size numeric,
+                                                               p_total_system_cost_before_rebates numeric,
+                                                               p_state_id bigint,
+                                                               p_storage_capacity numeric);
 CREATE OR REPLACE FUNCTION brs.get_above_the_line_utility_rebates(p_version_id bigint, p_utility_company_id bigint,
-                                                                  p_aurora_design_summary jsonb, p_system_size numeric,
-                                                                  p_total_system_cost_before_rebates numeric,
-                                                                  p_state_id bigint)
+                                                                   p_aurora_design_summary jsonb, p_system_size numeric,
+                                                                   p_total_system_cost_before_rebates numeric,
+                                                                   p_state_id bigint,
+                                                                   p_storage_capacity numeric)
   returns table
           (
             above_the_line_utility_rebate_amount numeric,
-            rebates                      jsonb,
-            eto_rebate_amount    numeric
+            rebates                              jsonb,
+            eto_rebate_amount                    numeric
           )
 AS
 $BODY$
@@ -27,7 +33,9 @@ BEGIN
                   rebate_cap_percent_of_total,
                   minimum_tsrf_for_qualification,
                   rebate,
-                  rebate_id
+                  rebate_id,
+                  odoe_battery_rebate_amount,
+                  odoe_battery_rebate_cap_amount
            from brs.get_proposal_rebates(p_version_id)
            where utility_company_id = p_utility_company_id
              and rebate_type_id = 455
@@ -38,7 +46,7 @@ BEGIN
                    else 1 = 1 end
     loop
       v_utility_rebate_amount = 0::numeric;
---       raise notice 'above the line minimum_tsrf_for_qualification = %',x.minimum_tsrf_for_qualification;
+      --       raise notice 'above the line minimum_tsrf_for_qualification = %',x.minimum_tsrf_for_qualification;
 --       raise notice 'above the line rebate_amount = % ',x.rebate_amount;
 --       raise notice 'above the line rebate_cap_amount = % ',x.rebate_cap_amount;
 --       raise notice 'above the line rebate_cap_percent_of_total = % ',x.rebate_cap_percent_of_total;
@@ -66,14 +74,23 @@ BEGIN
             least(v_utility_rebate_amount, x.rebate_cap_percent_of_total * p_total_system_cost_before_rebates);
         end if;
       end if;
+      if x.odoe_battery_rebate_amount is not null and x.odoe_battery_rebate_amount > 0 then
+        v_utility_rebate_amount = v_utility_rebate_amount +
+                                  least((p_storage_capacity * x.odoe_battery_rebate_amount * 1000)::numeric,
+                                        x.odoe_battery_rebate_cap_amount);
+      end if;
       --raise notice 'above the line v_utility_rebate_amount = %',v_utility_rebate_amount;
-      v_above_the_line_rebate_amount = coalesce(v_above_the_line_rebate_amount,0) + coalesce(v_utility_rebate_amount, 0);
-      v_rebates = COALESCE(v_rebates, '{}'::jsonb) || jsonb_build_object(x.rebate, round(v_utility_rebate_amount,2));
-    if x.rebate_id = 451 then
-      v_eto_rebate_amount = coalesce(v_eto_rebate_amount,0) + coalesce(v_utility_rebate_amount,0);
-    end if;
+      v_above_the_line_rebate_amount =
+        coalesce(v_above_the_line_rebate_amount, 0) + coalesce(v_utility_rebate_amount, 0);
+      v_rebates = COALESCE(v_rebates, '{}'::jsonb) || jsonb_build_object(x.rebate, round(v_utility_rebate_amount, 2));
+      if x.rebate_id = 451 then
+        v_eto_rebate_amount = coalesce(v_eto_rebate_amount, 0) + coalesce(v_utility_rebate_amount, 0);
+      end if;
+
     end loop;
-  return query select coalesce(v_above_the_line_rebate_amount, 0), COALESCE(v_rebates, '{}'::jsonb),v_eto_rebate_amount;
+  return query select coalesce(v_above_the_line_rebate_amount, 0),
+                      COALESCE(v_rebates, '{}'::jsonb),
+                      v_eto_rebate_amount;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
