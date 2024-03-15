9d851cba325fb88b9053a4bd649c8d8ba79d6f91
Randa <randa@7oaksgroup.com>
2024-03-14 22:37:01 -0600

    close enough smartlist v1

--- client/src/views/flow/smartlistv1/SmartlistHome.vue
+++ client/src/views/flow/smartlistv1/SmartlistHome.vue
@@ -2,12 +2,6 @@
 <router-view />
 </template>
 
-<script>
-import SmartLists from './Smartlists'
-export default {
-  name: 'SmartlistHome',
-  components: {
-    SmartLists
-  }
-}
+<script setup>
+
 </script>
