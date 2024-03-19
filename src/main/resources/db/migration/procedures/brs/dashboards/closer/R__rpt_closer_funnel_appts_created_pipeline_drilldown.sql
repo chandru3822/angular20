bdeda30c9939d3097875b4f8ed5414fac4f9822b
Scott Keller <kellersk88@gmail.com>
2024-03-18 19:02:01 -0600

    Carlin and Michael wanted ancillary costs to the log table

--- src/main/resources/db/migration/procedures/brs/dashboards/closer/R__rpt_closer_funnel_appts_created_pipeline_drilldown.sql
+++ src/main/resources/db/migration/procedures/brs/dashboards/closer/R__rpt_closer_funnel_appts_created_pipeline_drilldown.sql
@@ -2,10 +2,12 @@ drop function if exists brs.rpt_closer_funnel_appts_created_pipeline_drilldown(p
                                                                                p_funnel_id bigint,
                                                                                p_source_ids bigint[],
                                                                                p_run_by_id bigint);
+drop function if exists brs.rpt_closer_funnel_appts_created_pipeline_drilldown(p_start_date date, p_end_date date,
+                                                                               p_funnel_id bigint,
+                                                                               p_source_ids bigint[]);
 CREATE OR REPLACE FUNCTION brs.rpt_closer_funnel_appts_created_pipeline_drilldown(p_start_date date, p_end_date date,
                                                                                   p_funnel_id bigint,
-                                                                                  p_source_ids bigint[],
-                                                                                  p_run_by_id bigint)
+                                                                                  p_source_ids bigint[])
   RETURNS SETOF json
   LANGUAGE plpgsql
 AS
@@ -21,33 +23,60 @@ BEGIN
   from brs.funnel f
   where f.id = p_funnel_id;
 
-  RETURN QUERY select array_to_json(array_agg(row_to_json(funnel_rows)))
-               from (
-                      select pd.closer_name                 as owner_name,
-                             o.org_name                        office,
-                             pd.project_state_abbreviation     state,
-                             pd.company_project_status_type as status_type,
-                             pd.contact_name                   customer_name,
-                             pd.contact_id,
-                             pd.project_id,
-                             pd.source_name,
-                             pd.system_size,
-                             pd.primary_financier_name         financier,
-                             pd.closer_appointment_start       appointment_date,
-                             pd.cancelled_date,
-                             pd.project_created_date        as date_created
-                      from brs.project_details pd
-                             inner join flow.user_position up on up.id = pd.closer_user_position_id
-                             inner join flow.org o on o.id = up.org_id
-                      where ((pd.project_created_date at time zone 'UTC') at time zone 'US/Mountain') :: date between p_start_date and p_end_date
-                        and pd.closer_appointment_start is not null
-                        and case
-                              when v_show_no_source then pd.source is null
-                              when v_show_everything then (pd.source is null OR pd.source = any (p_source_ids))
-                              else pd.source = any (p_source_ids) end
-                      order by owner_name, pd.project_created_date
-                    ) as funnel_rows;
 
+  case
+    when p_funnel_id = 34 then RETURN QUERY
+      select array_to_json(array_agg(row_to_json(funnel_rows)))
+      from (select pd.closer_name                 as owner_name,
+                   o.org_name                        office,
+                   s.abbreviation     state,
+                   pd.company_project_status_type as status_type,
+                   concat(c.first_name,' ',c.last_name)                    customer_name,
+                   c.id,
+                   pd.project_id,
+                   pd.source_name,
+                   pd.system_size,
+                   pd.primary_financier_name         financier,
+                   pd.closer_appointment_start       appointment_date,
+                   pd.cancelled_date,
+                   pd.project_created_date        as date_created
+            from flow.contact c
+              left join flow.company_state cs on cs.id = c.company_state_id
+              left join flow.state s on s.id = cs.state_id
+              left join flow.project p on p.contact_id = c.id
+              left join brs.project_details pd on pd.project_id = p.id
+              left join flow.user_position up on up.id = pd.closer_user_position_id
+              left join flow.org o on o.id = up.org_id
+            where ((c.date_created at time zone 'UTC') at time zone
+                   'US/Mountain')::date between p_start_date and p_end_date
+            order by owner_name, c.date_created) as funnel_rows;
+    else return query
+      select array_to_json(array_agg(row_to_json(funnel_rows)))
+      from (select pd.closer_name                 as owner_name,
+                   o.org_name                        office,
+                   pd.project_state_abbreviation     state,
+                   pd.company_project_status_type as status_type,
+                   pd.contact_name                   customer_name,
+                   pd.contact_id,
+                   pd.project_id,
+                   pd.source_name,
+                   pd.system_size,
+                   pd.primary_financier_name         financier,
+                   pd.closer_appointment_start       appointment_date,
+                   pd.cancelled_date,
+                   pd.project_created_date        as date_created
+            from brs.project_details pd
+                   inner join flow.user_position up on up.id = pd.closer_user_position_id
+                   inner join flow.org o on o.id = up.org_id
+            where ((pd.first_time_appointment_created at time zone 'UTC') at time zone 'US/Mountain') :: date between p_start_date and p_end_date
+              and pd.closer_appointment_start is not null
+              and pd.closer_user_id is not null
+              and case
+                    when v_show_no_source then pd.source is null
+                    when v_show_everything then (pd.source is null OR pd.source = any (p_source_ids))
+                    else pd.source = any (p_source_ids) end
+            order by owner_name, pd.project_created_date) as funnel_rows;
 
+    end case;
 END
 $function$
