cd9f24e6d0515d0a79a06d484a6577f27ce7b350
jessicakburnett <jessica@7oaksgroup.com>
2024-03-29 07:20:59 -0500

    add padding to calendar time headers so scrollbar doesn't cover them

--- client/src/views/flow/schedule/components/Calendar.vue
+++ client/src/views/flow/schedule/components/Calendar.vue
@@ -1038,6 +1038,10 @@ const countSelected = computed(() => {
     font-size: 1.25rem;
   }
 }
+#event-calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th:nth-child(1) > div > div > table > thead > tr > th,
+#event-calendar > div.fc-view-harness.fc-view-harness-active > div > table > thead > tr > th > div > div > div > table > tbody > tr.fc-timeline-header-row.fc-timeline-header-row-chrono > th {
+padding-bottom: 8px;
+}
 
 .background-event {
   font-size: 11px;
