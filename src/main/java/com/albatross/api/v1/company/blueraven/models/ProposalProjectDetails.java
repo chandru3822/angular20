f02a67bd788ae217ec010e75e62881b8be0bc0c5
Randa <randa@7oaksgroup.com>
2024-03-31 15:25:18 -0600

    aurora AI/Sales Mode stuff

---
+++ src/main/java/com/albatross/api/v1/company/blueraven/models/ProposalProjectDetails.java
@@ -0,0 +1,14 @@
+package com.albatross.api.v1.company.blueraven.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.Date;
+
+@Data
+@NoArgsConstructor
+public class ProposalProjectDetails extends ProposalProject {
+  //i needed some specific data so i changed from the flow project model to this one
+  private String street1, city, state, postalCode, mobile;
+  private Date closerAppointmentStart;
+}
