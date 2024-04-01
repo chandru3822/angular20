aa7362bd35ed0d139e66eca17d5e7d5da946a8b1
kaylahellbusch <75056244+kaylahellbusch@users.noreply.github.com>
2024-04-01 15:22:32 -0600

    adds more files for endpoint

---
+++ src/main/java/com/albatross/api/v1/company/blueraven/models/FunnelColumn.java
@@ -0,0 +1,22 @@
+package com.albatross.api.v1.company.blueraven.models;
+
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+public class FunnelColumn {
+    private Long id, displayOrder, dataTypeId;
+    private String title, fieldName, dataType;
+
+  public FunnelColumn(Long id, String title, String fieldName, Long displayOrder, Long dataTypeId, String dataType) {
+    this.id = id;
+    this.title = title;
+    this.fieldName = fieldName;
+    this.displayOrder = displayOrder;
+    this.dataTypeId = dataTypeId;
+    this.dataType = dataType;
+  }
+}
