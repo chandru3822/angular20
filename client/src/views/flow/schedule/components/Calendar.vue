07e5543f72929358590aebf0ef6ac94123b87386
jessicakburnett <jessica@7oaksgroup.com>
2024-04-04 16:22:09 -0500

    wrap background title text for improved visibility

--- client/src/views/flow/schedule/components/Calendar.vue
+++ client/src/views/flow/schedule/components/Calendar.vue
@@ -274,7 +274,7 @@
         <template v-slot:eventContent="{event}">
           <v-tooltip bottom :open-on-hover="!$vuetify.breakpoint.smAndDown" :open-on-click="false">
             <template v-slot:activator="{ on, attrs }">
-              <span v-if="event.title !== 'null'" v-bind="attrs" v-on="on" class="event-title body-medium text-no-wrap">{{event.title}}</span>
+              <span v-if="event.title !== 'null'" v-bind="attrs" v-on="on" :class="{'text-no-wrap':event.display !== 'background'}" class="event-title body-medium">{{event.title}}</span>
             </template>
             <span>{{event.title}}</span>
           </v-tooltip>
@@ -978,7 +978,7 @@ const countSelected = computed(() => {
             }
 
             if(d.display === 'background'){
-              d.title = d.title + ': ' + moment(d.start).format('h:mm') + '-' + moment(d.end).format('h:mm')
+              d.title = d.title + ': ' + getFormattedDate(d.start, 'hh:mm') + '-' + getFormattedDate(d.end)
             }
           })
 
