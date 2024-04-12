a49f1cbb01cb0de4f3adc4af770cda3835485996
jessicakburnett <jessica@7oaksgroup.com>
2024-04-12 09:42:10 -0500

    fix resources dropdowns disabled if at max limit and then close a resource from the calendar

--- client/src/views/flow/schedule/components/Calendar.vue
+++ client/src/views/flow/schedule/components/Calendar.vue
@@ -816,6 +816,7 @@ const closeResource = (resource) => {
     toggleMapPinForResource(resource)
   }
   calendarOptions.value.resources = selectedOrgs.value.concat(selectedUsers.value)
+  limiter()
 }
 const toggleScheduleResource = (resource) =>{
   if(!isAssignedResource(resource)){
