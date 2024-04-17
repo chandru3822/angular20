787f3b22288534b1f121bd90267e07ce25c7dd48
Randa <randa@7oaksgroup.com>
2024-04-17 13:15:09 -0600

    if user is logging out dont do the saved fields thing for now

--- client/src/views/flow/users/User.vue
+++ client/src/views/flow/users/User.vue
@@ -172,7 +172,7 @@ onBeforeRouteLeave(async (to, from, next) => {
     hasDirtyNotes.value = userRouterViewContainer.value?.hasDirtyNotes()
   }
 
-  if (navigationOverride.value || (!hasDirtyFields.value && !hasDirtyNotes.value)) {
+  if (to.path === '/login' || navigationOverride.value || (!hasDirtyFields.value && !hasDirtyNotes.value)) {
     to.params.useSavedFilters = "true"
     //navigationOverride gets set to true if they click "Yes" to continue. if you don't override then it just hits the else again before navigating
     next()
