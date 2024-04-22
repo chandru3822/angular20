a0d5628c5f5bf543911dc5c9a035d50f05e8ec93
Randa <randa@7oaksgroup.com>
2024-04-22 12:04:01 -0600

    ugh

--- client/src/views/blueraven/tournament/Qualifying.vue
+++ client/src/views/blueraven/tournament/Qualifying.vue
@@ -152,7 +152,7 @@ const tournamentUserCount = ref(0)
 const matchesNotGenerated = ref(false)
 const minRowsPerPage = ref(0)
 const footerProps = ref({
-  'items-per-page-options': [25, 50, 100, 250],})
+  'items-per-page-options': [25, 50, 100, 250, 500, 1000],})
 const dataLoading = ref(true)
 const pool = ref({})
 const poolUsers = ref([])
