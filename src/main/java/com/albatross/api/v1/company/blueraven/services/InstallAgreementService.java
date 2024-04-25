4cd5f8681154e50f16f357a04633ea64fbe05980
Scott Humes <scotthumes@gmail.com>
2024-04-25 13:39:40 -0600

    [hotfix] Enable sending SREC disclosure docs

--- src/main/java/com/albatross/api/v1/company/blueraven/services/InstallAgreementService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/InstallAgreementService.java
@@ -100,13 +100,12 @@ public class InstallAgreementService {
   }
 
   public String saveRequest(InstallAgreementRequest request) throws Exception {
-	  //@TODO: Holding off on SREC functionality until we get a production API key
     // first create disclosure doc through SREC
-//    var srecSuccessful = sendDisclosureDoc(request.getProjectId(), request.getProposalNbr());
-//
-//    if (!srecSuccessful) {
-//      throw new RuntimeException("Unable to create disclosure document");
-//    }
+    var srecSuccessful = sendDisclosureDoc(request.getProjectId(), request.getProposalNbr());
+
+    if (!srecSuccessful) {
+      throw new RuntimeException("Unable to create disclosure document");
+    }
 
     final String result = createRequest(request);
     if (result == null || result.trim().isEmpty()) {
