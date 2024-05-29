2e73a915ef9b7ced1cc41123ca2b6a242ff8ce69
Scott Humes <scotthumes@gmail.com>
2024-05-29 14:55:28 -0600

    drop workqueue cycle trigger before dropping dependent function

--- src/main/resources/db/migration/procedures/flow/triggers/R__work_queue_cycle_trigger.sql
+++ src/main/resources/db/migration/procedures/flow/triggers/R__work_queue_cycle_trigger.sql
@@ -1,3 +1,4 @@
+drop trigger if exists create_work_queue_cycle_trg ON flow.project_process_step;
 drop function if exists flow.create_work_queue_cycle();
 CREATE OR REPLACE FUNCTION flow.create_work_queue_cycle()
   RETURNS TRIGGER AS
@@ -169,7 +170,6 @@ END
 $$
   LANGUAGE plpgsql;
 
-drop trigger if exists create_work_queue_cycle_trg ON flow.project_process_step;
 CREATE TRIGGER create_work_queue_cycle_trg
   after INSERT or update of company_process_step_status_type_id
   ON flow.project_process_step
