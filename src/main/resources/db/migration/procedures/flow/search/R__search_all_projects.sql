92582f87667d1737189ac4b7a1a8c4b950413743
eric.thomson <74467416+ethomson713@users.noreply.github.com>
2024-06-03 16:11:46 -0400

    Updating search_all_projects function to drop old table

--- src/main/resources/db/migration/procedures/flow/search/R__search_all_projects.sql
+++ src/main/resources/db/migration/procedures/flow/search/R__search_all_projects.sql
@@ -3,6 +3,13 @@ drop function if exists flow.search_all_projects(p_searchterm character varying,
                                                  p_offset bigint ,
                                                  p_company_project_status_type_id bigint ,
                                                  p_sort_column character varying ,
+                                                 p_sort_direction character varying);
+
+drop function if exists flow.search_all_projects(p_searchterm character varying, p_company_id bigint,
+                                                 p_is_parent boolean, p_limit bigint,
+                                                 p_offset bigint ,
+                                                 p_company_project_status_type_id bigint ,
+                                                 p_sort_column character varying ,
                                                  p_sort_direction character varying,
                                                  p_search_column character varying);
 
