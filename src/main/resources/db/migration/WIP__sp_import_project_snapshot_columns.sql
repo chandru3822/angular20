3054805fc72dea2f471ce2470f5bb12c129b0d7e
Kaleb Scholes <kscholes@gmail.com>
2024-06-05 10:03:55 -0600

    Add functions for calculating diffs for projects imported from SunPower

---
+++ src/main/resources/db/migration/WIP__sp_import_project_snapshot_columns.sql
@@ -0,0 +1,7 @@
+alter table flow.import_sp_project
+  add column if not exists _snapshot jsonb;
+comment on column flow.import_sp_project._snapshot is 'store a copy of the data SunPower is interested in so we can send only updates';
+
+alter table flow.import_sp_project
+  add column if not exists _snapshot_date_updated timestamptz;
+comment on column flow.import_sp_project._snapshot_date_updated is 'last time the columns were updated via a diff';
