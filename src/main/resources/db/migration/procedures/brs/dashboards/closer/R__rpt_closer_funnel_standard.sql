be5c017b72c7f8c30b7a537f8af1acda441f2a28
Randa <randa@7oaksgroup.com>
2024-07-03 12:50:19 -0600

    more data for milestones on dashboard

--- src/main/resources/db/migration/procedures/brs/dashboards/closer/R__rpt_closer_funnel_standard.sql
+++ src/main/resources/db/migration/procedures/brs/dashboards/closer/R__rpt_closer_funnel_standard.sql
@@ -138,6 +138,7 @@ BEGIN
                                f.name,
                                f.display_order,
                                f.show_checked_in_column,
+                               dm.major_milestone,
                                dm.inverse_trend,
                                f.funnel_type_id,
                                (select count(1)
@@ -177,12 +178,13 @@ BEGIN
                                  inner join brs.dashboard_milestone dm on f.dashboard_milestone_id = dm.id
                         where f.archived is false
                           and f.funnel_type_id = 3
-                        group by f.id, f.name, f.display_order, dm.inverse_trend
+                        group by f.id, f.name, f.display_order, dm.inverse_trend, dm.major_milestone
                         union
                         select f.id,
                                f.name,
                                f.display_order,
                                f.show_checked_in_column,
+                               dm.major_milestone,
                                dm.inverse_trend,
                                f.funnel_type_id,
                                (select count(1)
@@ -248,12 +250,13 @@ BEGIN
                         where f.archived is false
                           and f.funnel_type_id = 1
                           and f.id != 14
-                        group by f.id, f.name, f.display_order, dm.inverse_trend
+                        group by f.id, f.name, f.display_order, dm.inverse_trend, dm.major_milestone
                         union
                         select f.id,
                                f.name,
                                f.display_order,
                                f.show_checked_in_column,
+                               dm.major_milestone,
                                dm.inverse_trend,
                                f.funnel_type_id,
                                (select count(1)
@@ -265,7 +268,7 @@ BEGIN
                         where f.archived is false
                           and f.funnel_type_id = 1
                           and f.id = 14
-                        group by f.id, f.name, f.display_order, dm.inverse_trend
+                        group by f.id, f.name, f.display_order, dm.inverse_trend, dm.major_milestone
                         order by 3) as funnel_rows;
   else
     RETURN QUERY select array_to_json(array_agg(row_to_json(funnel_rows)))
@@ -455,6 +458,7 @@ BEGIN
                                foo.name,
                                foo.display_order,
                                foo.show_checked_in_column,
+                               foo.major_milestone,
                                foo.inverse_trend,
                                foo.funnel_type_id,
                                foo.custom_date_range_count,
@@ -471,6 +475,7 @@ BEGIN
                                      f.name,
                                      f.display_order,
                                      f.show_checked_in_column,
+                                     dm.major_milestone,
                                      dm.inverse_trend,
                                      f.funnel_type_id,
                                      (select count(1)
@@ -540,12 +545,13 @@ BEGIN
                                        inner join brs.dashboard_milestone dm on f.dashboard_milestone_id = dm.id
                               where f.archived is false
                                 and f.funnel_type_id = 3
-                              group by f.id, f.name, f.display_order, dm.inverse_trend) as foo
+                              group by f.id, f.name, f.display_order, dm.inverse_trend, dm.major_milestone) as foo
                         union
                         select foo1.id,
                                foo1.name,
                                foo1.display_order,
                                foo1.show_checked_in_column,
+                               foo1.major_milestone,
                                foo1.inverse_trend,
                                foo1.funnel_type_id,
                                foo1.custom_date_range_count,
@@ -561,6 +567,7 @@ BEGIN
                                      f.name,
                                      f.display_order,
                                      f.show_checked_in_column,
+                                     dm.major_milestone,
                                      dm.inverse_trend,
                                      f.funnel_type_id,
                                      (select count(1)
@@ -653,12 +660,13 @@ BEGIN
                               where f.archived is false
                                 and f.funnel_type_id = 1
                                 and f.id != 14
-                              group by f.id, f.name, f.display_order, dm.inverse_trend) as foo1
+                              group by f.id, f.name, f.display_order, dm.inverse_trend, dm.major_milestone) as foo1
                         union
                         select f.id,
                                f.name,
                                f.display_order,
                                f.show_checked_in_column,
+                               dm.major_milestone,
                                dm.inverse_trend,
                                f.funnel_type_id,
                                (select count(1)
@@ -672,7 +680,7 @@ BEGIN
                         where f.archived is false
                           and f.funnel_type_id = 1
                           and f.id = 14
-                        group by f.id, f.name, f.display_order, dm.inverse_trend
+                        group by f.id, f.name, f.display_order, dm.inverse_trend, dm.major_milestone
                         order by 3) as funnel_rows;
   end if;
 
