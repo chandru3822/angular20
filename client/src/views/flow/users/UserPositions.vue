8ac9bfe8ba6ef90701b225f42c63c68ff943b42b
Randa <randa@7oaksgroup.com>
2024-07-10 09:32:20 -0600

    show sales org

--- client/src/views/flow/users/UserPositions.vue
+++ client/src/views/flow/users/UserPositions.vue
@@ -197,7 +197,8 @@
                 <input type="checkbox" v-model="item.primaryFlag" disabled>
               </td>
               <td class="text-left user-column" v-for="(f, index) in filters" :key="index">
-                <a v-if="getOrgIdForFilter(item.hierarchy, f.orgLevelId)" :href="`/org/${getOrgIdForFilter(item.hierarchy, f.orgLevelId)}`">{{getOrgNameForFilter(item.hierarchy, f.orgLevelId)}}</a>
+                <a v-if="f.level === 7 && item.salesOrgName" :href="`/org/${item.salesOrgId}`">*{{ item.salesOrgName}}</a>
+                <a v-else-if="getOrgIdForFilter(item.hierarchy, f.orgLevelId)" :href="`/org/${getOrgIdForFilter(item.hierarchy, f.orgLevelId)}`">{{getOrgNameForFilter(item.hierarchy, f.orgLevelId)}}</a>
                 <span v-else>{{getOrgNameForFilter(item.hierarchy, f.orgLevelId)}}</span>
               </td>
               <td width="150" class="d-flex">
