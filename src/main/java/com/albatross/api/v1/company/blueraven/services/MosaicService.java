8383624c59304d23db9dc8538912e802fd136aaa
jberns003 <54857272+jberns003@users.noreply.github.com>
2024-07-29 13:45:41 -0400

    Installation Agreement: Updates for Mosaic proposal - support Ensemble

--- src/main/java/com/albatross/api/v1/company/blueraven/services/MosaicService.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/MosaicService.java
@@ -16,10 +16,7 @@ import org.springframework.stereotype.Service;
 
 import java.io.InputStream;
 import java.nio.charset.Charset;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Optional;
+import java.util.*;
 
 @Slf4j
 @Service
@@ -191,13 +188,13 @@ public class MosaicService {
       // Approved
       Double propRate = Double.parseDouble(propLogDetail.getInterestRate()) * 100;
       propRate = Math.round(propRate * 100.0) / 100.0;
-      String financeProductId = getFinanceProductId(creditDecision, propRate, propLogDetail.getLoanTerm());
+      String financeProductId = getFinanceProductId(creditDecision, propRate, propLogDetail.getLoanTerm(), isEnsembleProposal(propLogDetail.getState()));
       if (financeProductId.isEmpty()) {
         throw new Exception("No matching Mosaic finance offer found for: " + propLogDetail.getLoanTerm() + " year loan term, "
           + propRate + "% APR");
       }
 
-      String offerId = createOffer(propLogDetail.getLoanAmount(), applicationId, financeProductId);//"cda38149-6365-4888-90af-257d0b31bf96";//
+      String offerId = createOffer(propLogDetail.getLoanAmount(), applicationId, financeProductId);
       shareOffer(offerId);
     }
     else {
@@ -260,7 +257,7 @@ public class MosaicService {
     }
   }
 
-  private String getFinanceProductId(JSONObject creditDecision, Double proposalRate, String proposalTerm) {
+  private String getFinanceProductId(JSONObject creditDecision, Double proposalRate, String proposalTerm, Boolean isEnsembleProposal) {
     int termMonths = Integer.parseInt(proposalTerm) * 12;
     JSONArray financingProducts = creditDecision.getJSONArray("financingProducts");
     for (int i = 0; i < financingProducts.length(); i++) {
@@ -269,11 +266,30 @@ public class MosaicService {
       int totalPeriods = financeProduct.getInt("totalPeriods");
       String financingProductType = financeProduct.getString("financingProductType");
       if (financingProductType.equals("Choice") && rate == proposalRate && totalPeriods == termMonths) {
-        return financeProduct.getString("id");
+        if (!isEnsembleProposal) {
+          return financeProduct.getString("id");
+        }
+        else {
+          // If this is an Ensemble proposal, only return an Ensemble product (based on the product name)
+          String name = financeProduct.getString("name");
+          if (name.contains("Blue Raven Ensemble")) {
+            return financeProduct.getString("id");
+          }
+        }
       }
     }
 
     return "";
+  }
+
+  private Boolean isEnsembleProposal(String state) {
+    final HashSet<String> nonEnsembleStates = new HashSet<>(Arrays.asList("VA", "NC", "KY", "OH", "CO", "SC", "NV"));
+    if (nonEnsembleStates.contains(state)) {
+      return false;
+    }
+    else {
+      return true;
+    }
   }
 
   private String createOffer(String loanAmount, String applicationId, String financeProductId) throws Exception {
