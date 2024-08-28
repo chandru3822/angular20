e5de7c5a9ae0b912b8dca7fabc4c17bdbd4a1e44
Scott Keller <kellersk88@gmail.com>
2024-08-28 16:22:34 -0600

    proposal stuff

--- src/main/resources/db/migration/procedures/brs/dashboards/closer/R__util_closer_rep_selection.sql
+++ src/main/resources/db/migration/procedures/brs/dashboards/closer/R__util_closer_rep_selection.sql
@@ -12,6 +12,7 @@ DECLARE
   v_org_ids              bigint[];
   v_office_orgs          bigint[];
   v_closer_orgs          bigint[];
+v_sales_office_org_ids bigint[];
 BEGIN
   select array_agg(position_id)
   into v_current_position_ids
@@ -41,63 +42,75 @@ BEGIN
     from (SELECT (elem ->> 'office_id') :: bigint as elem
 
           FROM json_array_elements(p_office_ids::JSON) elem) as elem;
+    v_sales_office_org_ids = v_org_ids;
   elsif p_district_ids::text != '[]'::text then
     --raise notice '2';
     select array_agg(elem)
     into v_org_ids
     from (SELECT (elem ->> 'district_id') :: bigint as elem
           FROM json_array_elements(p_district_ids::JSON) elem) as elem;
+    select array_agg(id)
+    into v_sales_office_org_ids
+    from flow.org_hierarchy_filter_down(v_org_ids)
+    where org_type_id=3;
   elsif p_region_ids::text != '[]'::text then
     --raise notice '3';
     select array_agg(elem)
     into v_org_ids
     from (SELECT (elem ->> 'region_id') :: bigint as elem
           FROM json_array_elements(p_region_ids::JSON) elem) as elem;
+    select array_agg(id)
+    into v_sales_office_org_ids
+    from flow.org_hierarchy_filter_down(v_org_ids)
+    where org_type_id=3;
   elsif p_area_ids::text != '[]'::text then
     --raise notice '4';
     select array_agg(elem)
     from (SELECT (elem ->> 'area_id') :: bigint as elem
           into v_org_ids
           FROM json_array_elements(p_area_ids::JSON) elem) as elem;
+    select array_agg(id)
+    into v_sales_office_org_ids
+    from flow.org_hierarchy_filter_down(v_org_ids)
+    where org_type_id=3;
   end if;
   --raise notice 'v_org_ids,%',v_org_ids;
   -- org_level_id of 7 = Office
   case
     when p_area_ids::text = '[]'::text and p_region_ids::text = '[]'::text and
-         p_district_ids::text = '[]'::text and p_office_ids::text = '[]'::text then raise notice 'not here please';
-                                                                                    RETURN QUERY
-                                                                                      select array_to_json(array_agg(row_to_json(sub_rows)))
-                                                                                      from (select distinct user_id, name, active, user_position_id
-                                                                                            from (select upv.user_id                                                      user_id,
-                                                                                                         concat(upv.first_name, ' ', upv.last_name, ' - ', o.org_name) as name,
-                                                                                                         (case
-                                                                                                            when
-                                                                                                                upv.start_date is not null and
-                                                                                                                upv.start_date <=
-                                                                                                                (now() at time zone 'US/Mountain')::date and
-                                                                                                                (upv.end_date is null or
-                                                                                                                 upv.end_date >=
-                                                                                                                 (now() at time zone 'US/Mountain')::date)
-                                                                                                              then true
-                                                                                                            else false
-                                                                                                           end)                                                        as active,
-                                                                                                         upv.user_position_id
-                                                                                                  from flow.user_positions_vw upv
-                                                                                                         inner join flow.user_position up
-                                                                                                                    on upv.user_id =
-                                                                                                                       up.user_id and
-                                                                                                                       up.position_id in
-                                                                                                                       (select unnest(string_to_array(value, ',')::bigint[])
-                                                                                                                        from flow.company_configuration_value
-                                                                                                                        where code = 'CLOSER_POSITION_IDS')
-                                                                                                                      and
-                                                                                                                       up.archived is not true and
-                                                                                                                       up.id =
-                                                                                                                       upv.user_position_id
-                                                                                                         inner join flow.org o on o.id = up.org_id
-                                                                                                  where upv.user_status_type_id != 13 --not expired
-                                                                                                 ) as sub_rows
-                                                                                            order by active desc, name) as sub_rows;
+         p_district_ids::text = '[]'::text and p_office_ids::text = '[]'::text then RETURN QUERY
+      select array_to_json(array_agg(row_to_json(sub_rows)))
+      from (select distinct user_id, name, active, user_position_id
+            from (select upv.user_id                                                      user_id,
+                         concat(upv.first_name, ' ', upv.last_name, ' - ', o.org_name) as name,
+                         (case
+                            when
+                              upv.start_date is not null and
+                              upv.start_date <=
+                              (now() at time zone 'US/Mountain')::date and
+                              (upv.end_date is null or
+                               upv.end_date >=
+                               (now() at time zone 'US/Mountain')::date)
+                              then true
+                            else false
+                           end)                                                        as active,
+                         upv.user_position_id
+                  from flow.user_positions_vw upv
+                         inner join flow.user_position up
+                                    on upv.user_id =
+                                       up.user_id and
+                                       up.position_id in
+                                       (select unnest(string_to_array(value, ',')::bigint[])
+                                        from flow.company_configuration_value
+                                        where code = 'CLOSER_POSITION_IDS')
+                                      and
+                                       up.archived is not true and
+                                       up.id =
+                                       upv.user_position_id
+                         inner join flow.org o on o.id = up.org_id
+                  where upv.user_status_type_id != 13 --not expired
+                 ) as sub_rows
+            order by active desc, name) as sub_rows;
     else case
       when (v_org_level_id < 7) OR (326 = any (v_current_position_ids)) OR
            (2 = any (v_current_position_ids)) then ---- Corporate and Regional
@@ -136,10 +149,10 @@ BEGIN
                       and up.archived is not true and up.id = upv.user_position_id
                            inner join flow.org o on o.id = up.org_id
                     where upv.org_id is not null
-                      and
-                      case when v_org_level_id = 7 and (2 = any (v_current_position_ids)) then
-                       upv.org_id = any (v_office_orgs)
-                      else upv.org_id = any (v_org_ids)end
+                      and case
+                            when v_org_level_id = 7 and (2 = any (v_current_position_ids)) then
+                              (upv.org_id = any (v_office_orgs) or upv.sales_org_id = any (v_sales_office_org_ids))
+                            else (upv.org_id = any (v_org_ids) or upv.sales_org_id = any (v_sales_office_org_ids)) end
                       and upv.archived is not true
                       and upv.user_status_type_id != 13 --not expired
                    ) as users
---
+++ src/main/resources/db/migration/procedures/brs/dashboards/setter/R__rpt_get_upcoming_appointments.sql
@@ -0,0 +1,41 @@
+
+drop function if exists brs.rpt_get_upcoming_appointments(p_user_position_ids bigint[],
+                                                          p_org_ids bigint[]);
+CREATE OR REPLACE FUNCTION brs.rpt_get_upcoming_appointments(p_user_position_ids bigint[],
+                                                             p_org_ids bigint[])
+  RETURNS SETOF json
+  LANGUAGE plpgsql
+AS
+$function$
+declare
+  v_whole_company boolean;
+BEGIN
+
+  select -1 = any (p_user_position_ids) into v_whole_company;
+  return query select array_to_json(array_agg(row_to_json(upcoming_appointments)))
+   from (select pd.setter_name,
+                pd.project_name,
+                pd.project_id,
+                ((pd.prioritized_closer_dashboard_start_time at time zone 'UTC') at time zone 'US/Mountain') as appointment_start_time,
+                pd.closer_name,
+                u.phone_number,
+                ((pd.first_time_appointment_created at time zone 'UTC') at time zone 'US/Mountain') as date_created
+         from brs.project_details pd
+                inner join flow.user_position up on up.id = pd.setter_user_position_id
+                inner join flow.org o on o.id = up.org_id
+                inner join flow.user u on u.id = pd.closer_user_id
+         where case
+                 when v_whole_company is false then
+                   (pd.setter_user_position_id = any (p_user_position_ids)
+                     and (case
+                            when array_length(p_org_ids, 1) > 0
+                              then o.id = any (p_org_ids)
+                            else 1 = 1 end)
+                     )
+                 else true end
+           and ((prioritized_closer_dashboard_start_time at time zone 'UTC') at time zone 'US/Mountain')::date =
+               (now() at time zone 'US/Mountain' + interval '1 day')::date)  as upcoming_appointments;
+
+
+END
+$function$
