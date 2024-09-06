1202b8ed419bd1726d9f579ca3b4ac07cfb86cfe
jberns003 <54857272+jberns003@users.noreply.github.com>
2024-09-06 07:34:38 -0400

    Hotfix - Five9 - Modify PnB queries to check for appointment type

--- src/main/java/com/albatross/api/v1/company/blueraven/services/queries/Five9Query.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/services/queries/Five9Query.java
@@ -112,6 +112,15 @@ public class Five9Query {
                        AND pd.closer_user_id not in (select unnest(string_to_array(value, ',')::bigint[])
                                                          from flow.company_configuration_value
                                                       where code = 'PNB_EXCLUDED_USER_IDS')
+                       AND (select ppscfv.int_value
+                            from flow.project_process_step_custom_field_value ppscfv
+                            where ppscfv.custom_field_group_assignment_id = 26698 -- Appointment Type
+                              and ppscfv.project_process_step_id =
+                                  (select id from flow.project_process_step pps where pps.project_id = pd.project_id
+                                                                                  and pps.process_step_id = 1
+                                                                                  and pps.archived is false
+                                                                                  and pps.main is true)
+                            ) is null -- Appointment Type is null
                        )
     select id
     from results
@@ -148,6 +157,15 @@ public class Five9Query {
                        AND pd.closer_user_id not in (select unnest(string_to_array(value, ',')::bigint[])
                                                          from flow.company_configuration_value
                                                       where code = 'PNB_EXCLUDED_USER_IDS')
+                       AND (select ppscfv.int_value
+                            from flow.project_process_step_custom_field_value ppscfv
+                            where ppscfv.custom_field_group_assignment_id = 26698 -- Appointment Type
+                              and ppscfv.project_process_step_id =
+                                  (select id from flow.project_process_step pps where pps.project_id = pd.project_id
+                                                                                  and pps.process_step_id = 1
+                                                                                  and pps.archived is false
+                                                                                  and pps.main is true)
+                            ) is null -- Appointment Type is null
                        )
     select id
     from results
@@ -184,6 +202,15 @@ public class Five9Query {
                        AND pd.closer_user_id not in (select unnest(string_to_array(value, ',')::bigint[])
                                                          from flow.company_configuration_value
                                                       where code = 'PNB_EXCLUDED_USER_IDS')
+                       AND (select ppscfv.int_value
+                            from flow.project_process_step_custom_field_value ppscfv
+                            where ppscfv.custom_field_group_assignment_id = 26698 -- Appointment Type
+                              and ppscfv.project_process_step_id =
+                                  (select id from flow.project_process_step pps where pps.project_id = pd.project_id
+                                                                                  and pps.process_step_id = 1
+                                                                                  and pps.archived is false
+                                                                                  and pps.main is true)
+                            ) is null -- Appointment Type is null
                        )
     select id
     from results
