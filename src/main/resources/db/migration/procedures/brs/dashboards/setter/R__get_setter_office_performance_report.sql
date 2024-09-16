0079ca07d1a7abd027c06ce886fac603787f4d3e
Scott Keller <kellersk88@gmail.com>
2024-09-16 16:06:56 -0600

    setter dashboard and commissions

--- src/main/resources/db/migration/procedures/brs/dashboards/setter/R__get_setter_office_performance_report.sql
+++ src/main/resources/db/migration/procedures/brs/dashboards/setter/R__get_setter_office_performance_report.sql
@@ -8,26 +8,33 @@ drop function if exists brs.get_setter_mgr_performance_report(p_user_id bigint, 
 drop function if exists brs.get_setter_office_performance_report(p_user_id bigint, p_start_date date, p_end_date date,
                                                                  p_run_by_id bigint);
 drop function if exists brs.get_setter_office_performance_report(p_user_id bigint, p_start_date date,
-                                                                    p_end_date date);
+                                                                 p_end_date date);
 CREATE OR REPLACE FUNCTION brs.get_setter_office_performance_report(p_user_id bigint, p_start_date date,
                                                                     p_end_date date)
-  RETURNS table (
-                  total_appointments bigint,
-                  total_pitches bigint,
-                  pitch_percentage bigint,
-                  to_beat_id bigint,
-                  to_beat_name text,
-                  pitches_to_go bigint,
-                  current_rank text
-                ) AS
+  RETURNS table
+          (
+            total_appointments bigint,
+            total_pitches      bigint,
+            pitch_percentage   bigint,
+            to_beat_id         bigint,
+            to_beat_name       text,
+            pitches_to_go      bigint,
+            current_rank       text
+          )
+AS
 $BODY$
 DECLARE
-  v_office_id           bigint;
+  v_office_id    bigint;
+  v_org_level_id bigint;
+  v_sales_org_id bigint;
 BEGIN
 
-  select coalesce(u.sales_org_id, u.org_id)
-  into v_office_id
+  select coalesce(u.sales_org_id, u.org_id), ol.level, u.sales_org_id
+  into v_office_id,v_org_level_id,v_sales_org_id
   from flow.user_position u
+         inner join flow.org o on o.id = u.org_id
+         inner join flow.org_type ot on ot.id = o.org_type_id
+         inner join flow.org_level ol on ol.id = ot.org_level_id and ol.company_id = 3
   where u.user_id = p_user_id
     and u.position_id in (select unnest(string_to_array(value, ',')::bigint[])
                           from flow.company_configuration_value
@@ -35,59 +42,70 @@ BEGIN
     and u.primary_flag is true
     and u.archived is not true;
 
+  if v_org_level_id < 7 and v_sales_org_id is null then
+    return query
+      SELECT null::bigint     AS total_appointments,
+             null::bigint     AS total_pitches,
+             null::bigint     AS pitch_percentage,
+             null::bigint AS setter_office_to_beat_id,
+             null::text   AS setter_office_to_beat_name,
+             null::bigint     AS pitches_to_go,
+             null::text   AS current_office_rank;
+  else
 
-  return query
-    with data as (
-    select coalesce(rpt.total_appointments, 0) as total_appointments,
-           coalesce(rpt.total_pitches, 0) as total_pitches,
-           (case
-              when rpt.total_appointments = 0 then 0
-              else ((coalesce(rpt.total_pitches, 0)::numeric(10, 2) / rpt.total_appointments) * 100)::bigint
-             end
-             ) as pitch_percentage,
-           sotb.setter_office_to_beat_id,
-           sotb.setter_office_to_beat_name,
-           sotb.pitches_to_go,
-           sotb.current_office_rank
-    from (select (select count(1)::bigint
-                  from brs.project_details pd
-                         inner join flow.user_position up2 on up2.id = pd.setter_user_position_id
-                         inner join flow.org o2 on (o2.id = up2.org_id or o2.id = coalesce(up2.sales_org_id,0::bigint))
-                  and o2.id = v_office_id
-                  where pd.source in (525, 526) --(Setter Gen, Retargeted)
-                    and ((pd.first_time_appointment_created at time zone 'UTC') at time zone
-                         'US/Mountain') :: date between p_start_date and p_end_date
-                    and pd.company_id = 3) as total_appointments,
-                 (select count(1)::bigint
-                  from brs.project_details pd
-                         inner join flow.user_position up2 on up2.id = pd.setter_user_position_id
-                         inner join flow.org o2 on (o2.id = up2.org_id or o2.id = coalesce(up2.sales_org_id,0::bigint))
-                    and o2.id = v_office_id
-                  where pd.source in (525, 526) --(Setter Gen, Retargeted)
-                    and ((prioritized_closer_appointment_outcome_date at time zone 'UTC') at time zone
-                         'US/Mountain') :: date between p_start_date and p_end_date
-                    and pd.prioritized_closer_appointment_outcome in (2, 3, 1139, 1140)
-                    and pd.company_id = 3) as total_pitches) rpt
-           join lateral brs.get_setter_office_to_beat(v_office_id, p_start_date, p_end_date) sotb on true)
-  SELECT
-    d.total_appointments AS total_appointments,
-    d.total_pitches AS total_pitches,
-    d.pitch_percentage AS pitch_percentage,
-    d.setter_office_to_beat_id AS setter_office_to_beat_id,
-    d.setter_office_to_beat_name AS setter_office_to_beat_name,
-    d.pitches_to_go AS pitches_to_go,
-    d.current_office_rank AS current_office_rank
-  FROM data d
-    UNION ALL
-    SELECT 0::bigint AS total_appointments,
-           0::bigint AS total_pitches,
-           0::bigint AS pitch_percentage,
-           null::integer AS setter_office_to_beat_id,
-           'TBD'::text AS setter_office_to_beat_name,
-           0::bigint AS pitches_to_go,
-           'TBD'::text AS current_office_rank
-    WHERE NOT EXISTS (SELECT 1 FROM data);
-
+    return query
+      with data as (select coalesce(rpt.total_appointments, 0) as total_appointments,
+                           coalesce(rpt.total_pitches, 0)      as total_pitches,
+                           (case
+                              when rpt.total_appointments = 0 then 0
+                              else ((coalesce(rpt.total_pitches, 0)::numeric(10, 2) / rpt.total_appointments) * 100)::bigint
+                             end
+                             )                                 as pitch_percentage,
+                           sotb.setter_office_to_beat_id,
+                           sotb.setter_office_to_beat_name,
+                           sotb.pitches_to_go,
+                           sotb.current_office_rank
+                    from (select (select count(1)::bigint
+                                  from brs.project_details pd
+                                         inner join flow.user_position up2 on up2.id = pd.setter_user_position_id
+                                         inner join flow.org o2 on
+                                    (o2.id = up2.org_id or o2.id = coalesce(up2.sales_org_id, 0::bigint))
+                                      and o2.id = v_office_id
+                                  where pd.source in (525, 526) --(Setter Gen, Retargeted)
+                                    and ((pd.first_time_appointment_created at time zone 'UTC') at time zone
+                                         'US/Mountain') :: date between p_start_date and p_end_date
+                                    and pd.company_id = 3) as total_appointments,
+                                 (select count(1)::bigint
+                                  from brs.project_details pd
+                                         inner join flow.user_position up2 on up2.id = pd.setter_user_position_id
+                                         inner join flow.org o2 on
+                                    (o2.id = up2.org_id or o2.id = coalesce(up2.sales_org_id, 0::bigint))
+                                      and o2.id = v_office_id
+                                  where pd.source in (525, 526) --(Setter Gen, Retargeted)
+                                    and ((prioritized_closer_appointment_outcome_date at time zone 'UTC') at time zone
+                                         'US/Mountain') :: date between p_start_date and p_end_date
+                                    and pd.prioritized_closer_appointment_outcome in (2, 3, 1139, 1140)
+                                    and pd.company_id = 3) as total_pitches) rpt
+                           join lateral brs.get_setter_office_to_beat(v_office_id, p_start_date, p_end_date) sotb
+                                on true)
+      SELECT d.total_appointments         AS total_appointments,
+             d.total_pitches              AS total_pitches,
+             d.pitch_percentage           AS pitch_percentage,
+             d.setter_office_to_beat_id   AS setter_office_to_beat_id,
+             d.setter_office_to_beat_name AS setter_office_to_beat_name,
+             d.pitches_to_go              AS pitches_to_go,
+             d.current_office_rank        AS current_office_rank
+      FROM data d
+      UNION ALL
+      SELECT 0::bigint     AS total_appointments,
+             0::bigint     AS total_pitches,
+             0::bigint     AS pitch_percentage,
+             null::bigint AS setter_office_to_beat_id,
+             'TBD'::text   AS setter_office_to_beat_name,
+             0::bigint     AS pitches_to_go,
+             'TBD'::text   AS current_office_rank
+      WHERE NOT EXISTS (SELECT 1 FROM data);
+  end if;
 END
 $BODY$
   LANGUAGE plpgsql VOLATILE
