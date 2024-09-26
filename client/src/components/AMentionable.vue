15e02e3c043b0fef10a055330b21aaf324b34964
Kaleb Scholes <kscholes@gmail.com>
2024-09-26 09:47:49 -0600

    Update npm packages

--- client/src/components/AMentionable.vue
+++ client/src/components/AMentionable.vue
@@ -7,16 +7,16 @@
     ref="el"
     class="mentionable"
     :class="$attrs.class"
-    style="position:relative;"
+    style="position: relative"
   >
-    <slot/>
+    <slot />
     <v-menu
       v-model="menuOpen"
       :close-on-content-click="false"
       :activator="el"
       offset-y
-      :max-height=props.height
-      :max-width=props.width
+      :max-height="props.height"
+      :max-width="props.width"
     >
       <template v-slot:default="menu">
         <v-list v-if="displayedItems.length">
@@ -27,7 +27,9 @@
             @mouseover="selectedIndex = index"
             @mousedown="applyMention(index + 1)"
           >
-            <v-list-item-title>{{ item.label || item.value }}</v-list-item-title>
+            <v-list-item-title>{{
+              item.label || item.value
+            }}</v-list-item-title>
           </v-list-item>
         </v-list>
       </template>
@@ -36,52 +38,58 @@
 </template>
 
 <script setup>
-import { computed, onMounted, onUnmounted, onUpdated, ref, watch, nextTick } from 'vue'
+import {
+  computed,
+  onMounted,
+  onUnmounted,
+  onUpdated,
+  ref,
+  watch,
+  nextTick
+} from 'vue'
 import { defineProps, defineEmits } from 'vue'
-import {preventDefault} from "@fullcalendar/core/internal";
 
 const props = defineProps({
   keys: {
     type: Array,
-    required: true,
+    required: true
   },
   items: {
     type: Array,
-    default: () => [],
+    default: () => []
   },
   omitKey: {
     type: Boolean,
-    default: false,
+    default: false
   },
   filteringDisabled: {
     type: Boolean,
-    default: false,
+    default: false
   },
   insertSpace: {
     type: Boolean,
-    default: false,
+    default: false
   },
   mapInsert: {
     type: Function,
-    default: null,
+    default: null
   },
   limit: {
     type: String,
-    default: "8",
+    default: '8'
   },
   clearOnBackspace: {
     type: Boolean,
-    default: false,
+    default: false
   },
   width: {
     type: String,
-    default: "300",
+    default: '300'
   },
   height: {
     type: String,
-    default: "300",
+    default: '300'
   }
-
 })
 
 const emit = defineEmits(['search', 'open', 'close', 'apply', 'clear'])
@@ -100,7 +108,7 @@ const filteredItems = computed(() => {
     return props.items
   }
   const finalSearchText = searchText.value.trim().toLowerCase()
-  return props.items.filter(item => {
+  return props.items.filter((item) => {
     let text = item.searchText || item.label || ''
     for (const key in item) {
       if (!item.searchText && !item.label) {
@@ -111,23 +119,37 @@ const filteredItems = computed(() => {
   })
 })
 
-const displayedItems = computed(() => filteredItems.value.slice(0, parseInt(props.limit)))
+const displayedItems = computed(() =>
+  filteredItems.value.slice(0, parseInt(props.limit))
+)
 
 watch(searchText, (value, oldValue) => {
   emit('search', value, oldValue)
 })
 
-watch(displayedItems, (newItems) => {
-  selectedIndex.value = 0
-  if (searchText.value) {
-    const match = newItems.find(item => item?.label?.toLowerCase() === searchText.value.trim().toLowerCase() || item?.value?.toLowerCase() === searchText.value.trim().toLowerCase());
-    if (match) {
-      applyMention(newItems.indexOf(match) + 1 );
+watch(
+  displayedItems,
+  (newItems) => {
+    selectedIndex.value = 0
+    if (searchText.value) {
+      const match = newItems.find(
+        (item) =>
+          item?.label?.toLowerCase() ===
+            searchText.value.trim().toLowerCase() ||
+          item?.value?.toLowerCase() === searchText.value.trim().toLowerCase()
+      )
+      if (match) {
+        applyMention(newItems.indexOf(match) + 1)
+      }
     }
-  }
-}, { deep: true })
+  },
+  { deep: true }
+)
 
-const getInput = () => el.value.querySelector('input') || el.value.querySelector('textarea') || el.value.querySelector('[contenteditable="true"]')
+const getInput = () =>
+  el.value.querySelector('input') ||
+  el.value.querySelector('textarea') ||
+  el.value.querySelector('[contenteditable="true"]')
 
 onMounted(() => {
   input = getInput()
@@ -180,15 +202,18 @@ const onBlur = () => {
 
 const onKeyDown = (e) => {
   //the following stops the menu from opening when you're trying to scroll through multi-line text
-  if(!currentKey.value && (e.key === 'ArrowDown' || e.key === 'ArrowUp')){
+  if (!currentKey.value && (e.key === 'ArrowDown' || e.key === 'ArrowUp')) {
     e.stopImmediatePropagation()
   }
   if (currentKey.value) {
     if (e.key === 'ArrowDown') {
-      selectedIndex.value = (selectedIndex.value + 1) % displayedItems.value.length
+      selectedIndex.value =
+        (selectedIndex.value + 1) % displayedItems.value.length
       e.preventDefault()
     } else if (e.key === 'ArrowUp') {
-      selectedIndex.value = (selectedIndex.value - 1 + displayedItems.value.length) % displayedItems.value.length
+      selectedIndex.value =
+        (selectedIndex.value - 1 + displayedItems.value.length) %
+        displayedItems.value.length
       e.preventDefault()
     } else if (e.key === 'Enter' || e.key === 'Tab') {
       applyMention(selectedIndex.value)
@@ -199,8 +224,8 @@ const onKeyDown = (e) => {
     }
   }
   if ((e.metaKey || e.ctrlKey) && e.key === 'Backspace') {
-      closeMenu()
-      emit('clear')
+    closeMenu()
+    emit('clear')
   }
   if (e.key === 'Backspace' && oldKey.value) {
     if (searchText.value === null || searchText.value === '') {
@@ -211,13 +236,15 @@ const onKeyDown = (e) => {
       setCaretPosition(0)
       emit('clear')
       e.preventDefault()
-      oldKey.value = ""
+      oldKey.value = ''
     }
   }
 }
 
 const getSelectionStart = () => {
-  return input.isContentEditable ? window.getSelection().anchorOffset : input.selectionStart
+  return input.isContentEditable
+    ? window.getSelection().anchorOffset
+    : input.selectionStart
 }
 
 const setCaretPosition = (index) => {
@@ -227,7 +254,9 @@ const setCaretPosition = (index) => {
 }
 
 const getValue = () => {
-  return input.isContentEditable ? window.getSelection().anchorNode.textContent : input.value
+  return input.isContentEditable
+    ? window.getSelection().anchorNode.textContent
+    : input.value
 }
 
 const setValue = (value) => {
@@ -259,10 +288,14 @@ const checkKey = () => {
 }
 
 const getLastKeyBeforeCaret = (caretIndex) => {
-  const [keyData] = props.keys.map(key => ({
-    key,
-    keyIndex: getValue().toLowerCase().lastIndexOf(key.toLowerCase(), caretIndex - 1),
-  })).sort((a, b) => b.keyIndex - a.keyIndex)
+  const [keyData] = props.keys
+    .map((key) => ({
+      key,
+      keyIndex: getValue()
+        .toLowerCase()
+        .lastIndexOf(key.toLowerCase(), caretIndex - 1)
+    }))
+    .sort((a, b) => b.keyIndex - a.keyIndex)
   return keyData
 }
 
@@ -299,17 +332,35 @@ const applyMention = (itemIndex) => {
   if (itemIndex === 0) {
     itemIndex = 1
   }
-  const item = displayedItems.value[itemIndex -1]
-  const value = (props.omitKey ? '' : currentKey.value) + String(props.mapInsert ? props.mapInsert(item, currentKey.value) : item.value) + (props.insertSpace ? ' ' : '')
+  const item = displayedItems.value[itemIndex - 1]
+  const value =
+    (props.omitKey ? '' : currentKey.value) +
+    String(
+      props.mapInsert ? props.mapInsert(item, currentKey.value) : item.value
+    ) +
+    (props.insertSpace ? ' ' : '')
   if (input.isContentEditable) {
     const range = window.getSelection().getRangeAt(0)
-    range.setStart(range.startContainer, range.startOffset - currentKey.value.length - (searchText.value ? searchText.value.length : 0))
+    range.setStart(
+      range.startContainer,
+      range.startOffset -
+        currentKey.value.length -
+        (searchText.value ? searchText.value.length : 0)
+    )
     range.deleteContents()
     range.insertNode(document.createTextNode(value))
     range.setStart(range.endContainer, range.endOffset)
     input.dispatchEvent(new Event('input'))
   } else {
-    setValue(replaceText(getValue(), searchText.value, value, currentKeyIndex, currentKey.value))
+    setValue(
+      replaceText(
+        getValue(),
+        searchText.value,
+        value,
+        currentKeyIndex,
+        currentKey.value
+      )
+    )
     setCaretPosition(currentKeyIndex + value.length + 1)
   }
 
@@ -318,7 +369,11 @@ const applyMention = (itemIndex) => {
 }
 
 const replaceText = (text, searchString, newText, index, key) => {
-  return text.slice(0, index) + newText + text.slice(index + searchString.length + key.length + 1, text.length)
+  return (
+    text.slice(0, index) +
+    newText +
+    text.slice(index + searchString.length + key.length + 1, text.length)
+  )
 }
 </script>
 
