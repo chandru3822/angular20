a62510587b3fa4593d305bca2669e591f973ea90
Kaleb Scholes <kscholes@gmail.com>
2024-10-02 13:39:59 -0600

    sentry fixes
    (cherry picked from commit 822739006dd2bdb2853acb2ab57c9d5390ce6e14)

--- client/src/logger.js
+++ client/src/logger.js
@@ -1,6 +1,8 @@
 import * as Sentry from '@sentry/vue'
 import Vue from 'vue'
 
+const filters = []
+
 export default function init(dsn, router) {
   const integrations = [Sentry.replayIntegration()]
 
@@ -23,7 +25,8 @@ export default function init(dsn, router) {
     ],
     // Session Replay
     replaysSessionSampleRate: 0.1, // This sets the sample rate at 10%. You may want to change it to 100% while in development and then sample at a lower rate in production.
-    replaysOnErrorSampleRate: 1.0 // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    replaysOnErrorSampleRate: 1.0, // If you're not already sampling the entire session, change the sample rate to 100% when sampling sessions where errors occur.
+    ignoreErrors: ['NavigationDuplicated']
   })
 }
 
