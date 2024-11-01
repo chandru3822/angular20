50823bd5ba5d571c64c99c1b8192bf84bd9cc506
jessicakburnett <jessica@7oaksgroup.com>
2024-11-01 18:01:53 -0500

    add migration to add the flow.virtual_resource_slot_capacity table

---
+++ src/main/resources/db/migration/V1_522__virtual_scheduling_assistant.sql
@@ -0,0 +1,28 @@
+CREATE TABLE if not exists flow.virtual_resource_slot_capacity
+(
+	id                       serial  NOT NULL,
+	company_id       integer NOT NULL,
+	org_id integer NOT NULL,
+	max_capacity integer not null,
+	start_time time not null,
+	end_time time not null,
+	date_created     timestamp without time zone DEFAULT now(),
+	date_modified   timestamp without time zone,
+	created_by_id    integer      not null,
+	modified_by_id  integer,
+	archived       boolean not null default false,
+	CONSTRAINT flow_virtual_resource_slot_capacity_pk PRIMARY KEY (id),
+    CONSTRAINT flow_vrsc_company_id_fk FOREIGN KEY (company_id)
+	REFERENCES flow.company (id) MATCH SIMPLE
+	                          ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT flow_vrsc_org_id_fk FOREIGN KEY (org_id)
+	REFERENCES flow.org (id) MATCH SIMPLE
+	                          ON UPDATE RESTRICT ON DELETE RESTRICT,
+	CONSTRAINT flow_vrsc_created_by_id_fk FOREIGN KEY (created_by_id)
+	REFERENCES flow.user (id) MATCH SIMPLE
+	                          ON UPDATE NO ACTION ON DELETE NO ACTION,
+	CONSTRAINT flow_vrsc_modified_by_id_fk FOREIGN KEY (modified_by_id)
+	REFERENCES flow.user (id) MATCH SIMPLE
+	                          ON UPDATE NO ACTION ON DELETE NO ACTION
+	);
+
