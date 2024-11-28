a66b03464121db0f05322b9ad6851032dc03d740
jessicakburnett <jessica@7oaksgroup.com>
2024-11-28 10:11:42 -0500

    stupid sql function

--- src/main/resources/db/migration/procedures/brs/R__duplicate_capacity_week.sql
+++ src/main/resources/db/migration/procedures/brs/R__duplicate_capacity_week.sql
@@ -34,24 +34,21 @@ BEGIN
 	insert into flow.virtual_resource_slot_capacity(company_id, org_id, max_capacity, start_time, end_time, date_modified, created_by_id, modified_by_id)
 	select
 		p_company_id, p_org_id,
-		(select vrsc.max_capacity from flow.virtual_resource_slot_capacity vrsc
+		(select vrsc.start_time + interval '1 week', vrsc.end_time + interval '1 week'
+		 from flow.virtual_resource_slot_capacity vrsc
 		 where ((vrsc.start_time between v_previous_week_start_time and v_previous_week_end_time)
 			 OR (vrsc.end_time between v_previous_week_start_time and v_previous_week_end_time))
 		   AND vrsc.org_id = p_org_id
 		   AND vrsc.company_id = p_company_id),
-		p_current_week_start_time,
-		p_current_week_end_time, now(),
+		now(),
 		p_current_user_id,
 		p_current_user_id
-	where exists (select vrsc.max_capacity from flow.virtual_resource_slot_capacity vrsc
-		            where ((vrsc.start_time between v_previous_week_start_time and v_previous_week_end_time)
-			            OR (vrsc.end_time between v_previous_week_start_time and v_previous_week_end_time))
-			          AND vrsc.org_id = p_org_id
-			          AND vrsc.company_id = p_company_id)
 	on conflict (company_id, org_id, start_time, end_time)
 		do update
 		set
 			archived = false,
+			start_time = excluded.start_time,
+			end_time = excluded.end_time,
 			max_capacity = excluded.max_capacity,
 			date_modified = now(),
 			modified_by_id = excluded.modified_by_id;
