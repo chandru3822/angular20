a483b8d4e10c622c5230c1fd647e363f8c2d581e
jessicakburnett <jessica@7oaksgroup.com>
2025-03-20 13:48:39 -0500

    Revert "attempt 4 to fix unsaved changes error happening in stage"
    This reverts commit f9fc7438c78b8b5619b6b38fc67ec825c6e7dffe.

--- src/main/java/com/albatross/api/v1/company/blueraven/controllers/billOfMaterials/query/BillOfMaterialsQuery.java
+++ src/main/java/com/albatross/api/v1/company/blueraven/controllers/billOfMaterials/query/BillOfMaterialsQuery.java
@@ -59,7 +59,6 @@ public class BillOfMaterialsQuery {
                                          ON pmvcfg.parts_master_group_uuid = vv.parts_master_group_uuid
                       WHERE bomp.project_id = :projectId
                         and bomp.archived is false
-                        and bomp.quantity is not null
                       GROUP BY bomp.id
                              , pmvcfg.id
                              , s.name
