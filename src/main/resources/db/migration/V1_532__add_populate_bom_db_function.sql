3fc170f731a19e7a090312f3580344bdb07861e7
jessicakburnett <jessica@7oaksgroup.com>
2025-03-25 15:38:52 -0500

    fix the add populate db function flyway script

--- src/main/resources/db/migration/V1_532__add_populate_bom_db_function.sql
+++ src/main/resources/db/migration/V1_532__add_populate_bom_db_function.sql
@@ -1,4 +1,4 @@
-insert into flow.db_function(function_name, return_data_type_id, db_function_type_id, display_name, description)
+insert into flow.db_function(function_name, return_data_type_id, db_function_type_id, display_name, description, process_step_actionable)
 select 'brs.populate_bom_from_permit_pack', 11, 2, 'Populate BOM from Permit Pack Log', 'Given the project id, and permit pack log number, archives all existing bom parts and adds the bom parts from the permit pack to the project bom'
-	where not exists (select id from flow.db_function where function_name = 'brs.populate_bom_from_permit_pack');
+	where not exists (select id from flow.db_function where function_name = 'brs.populate_bom_from_permit_pack', true);
 ;
