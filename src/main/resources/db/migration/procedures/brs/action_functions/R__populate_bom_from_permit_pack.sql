57b9ee039882805826de9b948793adea0075c62e
jessicakburnett <jessica@7oaksgroup.com>
2025-04-11 15:57:38 -0500

    is it finally fixed?

--- src/main/resources/db/migration/procedures/brs/action_functions/R__populate_bom_from_permit_pack.sql
+++ src/main/resources/db/migration/procedures/brs/action_functions/R__populate_bom_from_permit_pack.sql
@@ -6,13 +6,13 @@ AS
 $function$
 DECLARE
 	permit_pack_log_number INT; --permit pack log number selected using cfgaId 26343
-	permit_pack_id INT; --permit pack id selected using cfgaId 26343
-	log_record RECORD; --used to hold the permit pack row
-	json_item  JSONB; --used to hold a single part json item from the log_record's bom data
-	part_num   TEXT; --used within a loop to hold each part_number
-	part_name  TEXT; --used within a loop to hold each part_name/description
-	quantity   INT; --used within a loop to hold the quantity of each part
-	part_id    INT; --used within a loop to hold each part's part_master_id
+	permit_pack_id         INT; --permit pack id selected using cfgaId 26343
+	log_record             RECORD; --used to hold the permit pack row
+	json_item              JSONB; --used to hold a single part json item from the log_record's bom data
+	part_num               TEXT; --used within a loop to hold each part_number
+	part_name              TEXT; --used within a loop to hold each part_name/description
+	quantity               INT; --used within a loop to hold the quantity of each part
+	part_id                INT; --used within a loop to hold each part's part_master_id
 
 BEGIN
 
@@ -50,22 +50,18 @@ BEGIN
 	END IF;
 
 	-- Archive permit_pack_log_number existing brs.bill_of_materials_parts for the given project_id
-	IF EXISTS (
-		SELECT 1
-		FROM brs.bill_of_materials_parts bomp
-		WHERE bomp.project_id = p_project_id
-	) THEN
+	IF EXISTS (SELECT 1
+	           FROM brs.bill_of_materials_parts bomp
+	           WHERE bomp.project_id = p_project_id) THEN
 		UPDATE brs.bill_of_materials_parts
 		SET archived = TRUE
 		WHERE project_id = p_project_id;
 		RAISE NOTICE 'Archived existing brs.bill_of_materials_parts for project_id = %', p_project_id;
 	END IF;
 
-	IF EXISTS (
-		SELECT 1
-		FROM brs.bill_of_materials_custom_parts bomcp
-		WHERE bomcp.project_id = p_project_id
-	) THEN
+	IF EXISTS (SELECT 1
+	           FROM brs.bill_of_materials_custom_parts bomcp
+	           WHERE bomcp.project_id = p_project_id) THEN
 		UPDATE brs.bill_of_materials_custom_parts
 		SET archived = TRUE
 		WHERE project_id = p_project_id;
@@ -76,11 +72,10 @@ BEGIN
 	-- Use query to get the parts_master_data as a CTE so we can get the parts_master_id
 	drop table if exists parts_master_data_temp;
 	CREATE TEMP TABLE parts_master_data_temp AS
-	SELECT
-		pmvcfg.id AS parts_master_id,
-		vw.parts_master_group_uuid,
-		MAX(jsonb_extract_path_text(vw.value, 'value')) FILTER (WHERE vw.field_id = 728) AS description,
-		MAX(jsonb_extract_path_text(vw.value, 'value')) FILTER (WHERE vw.field_id = 730) AS part_number
+	SELECT pmvcfg.id                                                                        AS parts_master_id,
+	       vw.parts_master_group_uuid,
+	       MAX(jsonb_extract_path_text(vw.value, 'value')) FILTER (WHERE vw.field_id = 728) AS description,
+	       MAX(jsonb_extract_path_text(vw.value, 'value')) FILTER (WHERE vw.field_id = 730) AS part_number
 	FROM brs.parts_master_version_custom_field_value_vw AS vw
 		     INNER JOIN brs.parts_master_version AS pmv
 		                ON pmv.id = vw.parts_master_version_id
@@ -95,70 +90,78 @@ BEGIN
 		SELECT * FROM jsonb_array_elements(log_record.bom::jsonb)
 		LOOP
 			-- Extract part_number, name, and quantity
-			part_num := json_item->>'number';
-			part_name := json_item->>'name';
-			quantity := (json_item->>'quantity')::INT;
+			part_num := json_item ->> 'number';
+			part_name := json_item ->> 'name';
+			quantity := (json_item ->> 'quantity')::INT;
 			RAISE NOTICE 'json_item = %', json_item;
 
-			-- Try matching by part_number first
-			SELECT pmd.parts_master_id INTO part_id
-			FROM parts_master_data_temp pmd
-			WHERE LOWER(pmd.part_number) = LOWER(part_num)
-			LIMIT 1;
-
-			-- If no match, try matching by description
-			IF part_id IS NULL THEN
-				SELECT pmd.parts_master_id INTO part_id
+			IF quantity > 0 THEN
+				-- Try matching by part_number first
+				SELECT pmd.parts_master_id
+				INTO part_id
 				FROM parts_master_data_temp pmd
 				WHERE LOWER(pmd.part_number) = LOWER(part_num)
-				  AND LOWER(pmd.description) ILIKE '%' || LOWER(part_name) || '%'
-				LIMIT 1;
-			END IF;
-
-			-- Insert into brs.bill_of_materials_parts table if match is found
-			IF part_id IS NOT NULL AND quantity > 0 THEN
-				INSERT INTO brs.bill_of_materials_parts (quantity, parts_master_id, project_id, permit_pack_id, date_created, date_modified, created_by_id, modified_by_id)
-				VALUES (quantity, part_id, p_project_id, permit_pack_id, now(), now(), 99999999, 99999999);
-			ELSE
-				--if it's not in the parts_master table, we're going to use the custom_parts table
-				RAISE NOTICE 'No matching part found for part_number = % and name = %', part_num, part_name;
-
-				--try matching on the custom_parts table
-				SELECT cp.id INTO part_id
-				FROM brs.custom_parts cp
-				WHERE LOWER(cp.part_number) = LOWER(part_num)
-				  AND cp.name = part_name
 				LIMIT 1;
 
+				-- If no match, try matching by description
 				IF part_id IS NULL THEN
-					-- if it's also not in the custom_parts table then add it.
-					INSERT INTO brs.custom_parts (name, part_number, date_created, date_modified, created_by_id, modified_by_id)
-					VALUES (part_name, part_num, now(), now(), 99999999, 99999999)
-					RETURNING id into part_id;
-
-					RAISE NOTICE 'Adding part_number = % to the custom_parts table', part_num;
+					SELECT pmd.parts_master_id
+					INTO part_id
+					FROM parts_master_data_temp pmd
+					WHERE LOWER(pmd.part_number) = LOWER(part_num)
+					  AND LOWER(pmd.description) ILIKE '%' || LOWER(part_name) || '%'
+					LIMIT 1;
 				END IF;
 
-				--no matter what, we should have a part_id now from the non_parts_master_part table (but it's usually safer to check)
+				-- Insert into brs.bill_of_materials_parts table if match is found
 				IF part_id IS NOT NULL THEN
+					INSERT INTO brs.bill_of_materials_parts (quantity, parts_master_id, project_id, permit_pack_id,
+					                                         date_created, date_modified, created_by_id, modified_by_id)
+					VALUES (quantity, part_id, p_project_id, permit_pack_id, now(), now(), 99999999, 99999999);
+				ELSE
+					--if it's not in the parts_master table, we're going to use the custom_parts table
+					RAISE NOTICE 'No matching part found for part_number = % and name = %', part_num, part_name;
 
-					--increment the usage on the custom_parts table
-					UPDATE brs.custom_parts
-					SET usage=(SELECT COALESCE(MAX(usage), 0) + 1 FROM brs.custom_parts)
-					WHERE id = part_id;
+					--try matching on the custom_parts table
+					SELECT cp.id
+					INTO part_id
+					FROM brs.custom_parts cp
+					WHERE LOWER(cp.part_number) = LOWER(part_num)
+					  AND cp.name = part_name
+					LIMIT 1;
 
-					--Insert into the bill_of_materials_custom_parts table
-					IF quantity IS NOT NULL THEN
-						INSERT INTO brs.bill_of_materials_custom_parts (quantity, custom_part_id, project_id, permit_pack_id, date_created, date_modified, created_by_id, modified_by_id)
+					IF part_id IS NULL THEN
+						-- if it's also not in the custom_parts table then add it.
+						INSERT INTO brs.custom_parts (name, part_number, date_created, date_modified, created_by_id,
+						                              modified_by_id)
+						VALUES (part_name, part_num, now(), now(), 99999999, 99999999)
+						RETURNING id into part_id;
+
+						RAISE NOTICE 'Adding part_number = % to the custom_parts table', part_num;
+					END IF;
+
+					--no matter what, we should have a part_id now from the non_parts_master_part table (but it's usually safer to check)
+					IF part_id IS NOT NULL THEN
+
+						--increment the usage on the custom_parts table
+						UPDATE brs.custom_parts
+						SET usage=(SELECT COALESCE(MAX(usage), 0) + 1 FROM brs.custom_parts)
+						WHERE id = part_id;
+
+						--Insert into the bill_of_materials_custom_parts table
+						INSERT INTO brs.bill_of_materials_custom_parts (quantity, custom_part_id, project_id,
+						                                                permit_pack_id, date_created, date_modified,
+						                                                created_by_id, modified_by_id)
 						VALUES (quantity, part_id, p_project_id, permit_pack_id, now(), now(), 99999999, 99999999);
 
 					ELSE
 						RAISE NOTICE 'Something went wrong updating the custom_parts table for part_number = %', part_num;
-					END IF;
 
-				END IF;
-			END IF;--end Insert into brs.bill_of_materials_parts table if/else
+					END IF;
+				END IF;--end Insert into brs.bill_of_materials_parts table if/else
+			END IF;
 		END LOOP;
 	drop table if exists parts_master_data_temp;
 END;
+
 $function$
