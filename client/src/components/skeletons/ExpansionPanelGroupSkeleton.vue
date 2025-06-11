cd37566b38299b65e940917c83337953401354b1
Sam Riddle <30331129+sjriddle@users.noreply.github.com>
2025-06-11 13:08:01 -0600

    Hotfix/adder subtotal inclusion (#221)
    * hotfix: handle subtotal calculation error when null review date
    
    * chore: added loading skeleton, fixed dupe api calls, date fix for pricing
    
    * chore: fix repeating behavior on mount, fix click ot open dialog
    
    * chore: proposal skeleton for proposal view
    
    * chore: add document skeleton file component

---
+++ client/src/components/skeletons/ExpansionPanelGroupSkeleton.vue
@@ -0,0 +1,25 @@
+<template>
+  <v-expansion-panels multiple :class="groupClass">
+    <ExpansionPanelSkeleton
+      v-for="(panel, index) in panels"
+      :key="`panel-${index}`"
+      v-bind="panel"
+    />
+  </v-expansion-panels>
+</template>
+
+<script setup>
+import ExpansionPanelSkeleton from './ExpansionPanelSkeleton.vue'
+
+defineProps({
+  panels: {
+    type: Array,
+    required: true,
+    // Expected format: Array of ExpansionPanelSkeleton props
+  },
+  groupClass: {
+    type: String,
+    default: ''
+  }
+})
+</script>
