6063e7f0a1ae25b0db8a2ee9b0634a25126b8cc8
mohamed_asr <mohamedashraf.m@codeboardtech.com>
2025-06-13 19:30:34 +0530

    BUG - Project Statuses Object Categories

--- client/src/views/flow/project/StatusTracker.vue
+++ client/src/views/flow/project/StatusTracker.vue
@@ -5,8 +5,9 @@
       <!--        <SpinnerInline :size="50" :spinner-color="`primary`" :transparent="true" :centered="true"/>-->
       <!--      </div>-->
       <div>
-
-        <div class="headline-small stage-header">
+        
+          <div v-if="milestones.length>0">
+               <div class="headline-small stage-header">
           Current Stage:
           <span :class="{'cancelled-text': cancelled}">{{currentStatus.projectStatusType}}
           </span>
@@ -44,6 +45,12 @@
             </div>
           </div>
         </div>
+          </div>
+       <div v-else>
+         <div class="headline-small stage-header">
+              No milestones configured
+        </div>
+       </div>
       </div>
     </v-col>
   </v-row>
