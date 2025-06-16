8371ddea4223f94c006f2d5637b648d2188f360f
chandru <chandru.v@codeboardtech.com>
2025-06-16 14:45:30 +0530

    flyscript changes

--- src/main/resources/db/migration/V1_549__userslot.sql
+++ src/main/resources/db/migration/V1_549__userslot.sql
@@ -9,6 +9,6 @@ CREATE TABLE IF NOT EXISTS flow.user_slot_schedules (
     modified_by_id INTEGER,
     archived BOOLEAN NOT NULL DEFAULT false,
 
-    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES flow.users(id),
-    CONSTRAINT fk_slot_schedule FOREIGN KEY (slot_schedule_id) REFERENCES flow.slot_schedules(id)
+    CONSTRAINT fk_user FOREIGN KEY (user_id) REFERENCES flow.user(id),
+    CONSTRAINT fk_slot_schedule FOREIGN KEY (slot_schedule_id) REFERENCES flow.resource_slot_schedule(id)
 );
