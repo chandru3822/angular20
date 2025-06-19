8797c2490bce887c93ac5945f2ecb8388c5ac42a
Ethan Hogge <97705628+shuvll@users.noreply.github.com>
2025-06-19 16:21:58 -0600

    user_login flyway file fix.

--- src/main/resources/db/migration/V1_550__user_login.sql
+++ src/main/resources/db/migration/V1_550__user_login.sql
@@ -1,3 +1,4 @@
+DROP TABLE IF EXISTS flow.user_login;
 CREATE TABLE IF NOT EXISTS flow.user_login (
   id BIGSERIAL NOT NULL,
   user_id BIGINT NOT NULL,
@@ -8,10 +9,13 @@ CREATE TABLE IF NOT EXISTS flow.user_login (
   modified_by_id INTEGER,
   date_created TIMESTAMP WITHOUT TIME ZONE DEFAULT now() NOT NULL,
   date_modified TIMESTAMP WITHOUT TIME ZONE,
-  CONSTRAINT fk_user
-   FOREIGN KEY(user_id) REFERENCES flow.user(id) ON DELETE CASCADE,
-  CONSTRAINT fk_created_by
-   FOREIGN KEY(created_by_id) REFERENCES flow.user(id),
-  CONSTRAINT fk_modified_by
-   FOREIGN KEY(modified_by_id) REFERENCES flow.user(id)
-);
+  CONSTRAINT fk_user_id FOREIGN KEY (user_id)
+  REFERENCES flow.user (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_created_by FOREIGN KEY (created_by_id)
+  REFERENCES flow.user (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION,
+  CONSTRAINT fk_modified_by FOREIGN KEY (modified_by_id)
+  REFERENCES flow.user (id)
+    ON UPDATE NO ACTION ON DELETE NO ACTION
+  );
